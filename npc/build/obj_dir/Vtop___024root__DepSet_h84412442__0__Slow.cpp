78f44495952b0763cea16f05afb6516635824029
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 16:33:59 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  16:33:59 up  6:44,  3 users,  load average: 1.36, 1.29, 1.22

--- npc/build/obj_dir/Vtop___024root__DepSet_h84412442__0__Slow.cpp
+++ npc/build/obj_dir/Vtop___024root__DepSet_h84412442__0__Slow.cpp
@@ -12,13 +12,14 @@ VL_ATTR_COLD void Vtop___024root___initial__TOP__0(Vtop___024root* vlSelf) {
     Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
     VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___initial__TOP__0\n"); );
     // Init
-    VlWide<5>/*159:0*/ __Vtemp_hfa1e3f21__0;
+    VlWide<6>/*191:0*/ __Vtemp_ha162e690__0;
     // Body
-    __Vtemp_hfa1e3f21__0[0U] = 0x2e766364U;
-    __Vtemp_hfa1e3f21__0[1U] = 0x64756d70U;
-    __Vtemp_hfa1e3f21__0[2U] = 0x766c745fU;
-    __Vtemp_hfa1e3f21__0[3U] = 0x6f67732fU;
-    __Vtemp_hfa1e3f21__0[4U] = 0x6cU;
-    vlSymsp->_vm_contextp__->dumpfile(VL_CVT_PACK_STR_NW(5, __Vtemp_hfa1e3f21__0));
+    __Vtemp_ha162e690__0[0U] = 0x2e766364U;
+    __Vtemp_ha162e690__0[1U] = 0x64756d70U;
+    __Vtemp_ha162e690__0[2U] = 0x766c745fU;
+    __Vtemp_ha162e690__0[3U] = 0x6f67732fU;
+    __Vtemp_ha162e690__0[4U] = 0x6c642f6cU;
+    __Vtemp_ha162e690__0[5U] = 0x627569U;
+    vlSymsp->_vm_contextp__->dumpfile(VL_CVT_PACK_STR_NW(6, __Vtemp_ha162e690__0));
     vlSymsp->_traceDumpOpen();
 }
