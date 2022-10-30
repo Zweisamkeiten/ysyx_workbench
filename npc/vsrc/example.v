4ec9711355c15ebf44a3dc13490bd46c0b184b7b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 15:23:14 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  15:23:14 up  5:33,  2 users,  load average: 1.06, 1.17, 1.16

--- npc/vsrc/example.v
+++ npc/vsrc/example.v
@@ -1,2 +1,12 @@
-module example();
+module top (
+  input a,
+  input b,
+  output f
+);
+
+  assign f = a ^ b;
+  initial begin
+    $dumpfile("logs/vlt_dump.vcd");
+    $dumpvars();
+  end
 endmodule
