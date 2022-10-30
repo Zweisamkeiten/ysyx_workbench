a04004f5e7e57ba68a13cecd28bd59598b731305
22050710-Einsam <snitxmhm@gmail.com>
2022-10-30 17:13:10 +0800

    feat: connect the dual control switch to NVBoard

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,10 +1,6 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-// Include common routines
-#include <verilated.h>
 // Include model header, generated from Verilating "top.v"
 #include "Vtop.h"
+#include <nvboard.h>
 
 int main(int argc, char *argv[], char **env) {
   // Prevent unused variable warnings
@@ -26,20 +22,18 @@ int main(int argc, char *argv[], char **env) {
   // 构造模型
   Vtop *top = new Vtop{contextp};
 
+  nvboard_bind_pin(&top->a, false, false, 1, SW0);
+  nvboard_bind_pin(&top->b, false, false, 1, SW1);
+  nvboard_bind_pin(&top->f, false, true, 1, LD0);
+  nvboard_init();
+
   // Simulate until $finish
   // 仿真
   while (!contextp->gotFinish()) {
     contextp->timeInc(1); // 1 timeprecision period passes...
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-
     // Evaluate model
     top->eval();
-
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+    nvboard_update();
   }
 
   // Final model cleanup
