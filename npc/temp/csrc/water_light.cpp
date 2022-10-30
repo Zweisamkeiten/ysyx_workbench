a4e08d851fe9aa5d0e64898bc43a62b6b7823968
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 18:47:03 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  18:47:03 up  8:57,  3 users,  load average: 1.08, 1.10, 1.09

---
+++ npc/temp/csrc/water_light.cpp
@@ -0,0 +1,12 @@
+#include <Vlight.h>
+#include <nvboard.h>
+
+static Vlight light;
+
+void nvboard_bind_all_pins(Vlight *light);
+
+static void single_cycle() {}
+
+void reset(int n) {}
+
+int main(int argc, char *argv[]) { return 0; }
