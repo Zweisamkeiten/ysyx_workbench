a4bfcef2b21ed51ac982d9eb53d57bf12fc7b1da
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 21:29:50 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  21:29:50 up 11:40,  3 users,  load average: 1.30, 1.23, 1.20

---
+++ npc/temp/build/obj_dir/Vlight___024root.h
@@ -0,0 +1,35 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vlight.h for the primary calling header
+
+#ifndef VERILATED_VLIGHT___024ROOT_H_
+#define VERILATED_VLIGHT___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class Vlight__Syms;
+
+class Vlight___024root final : public VerilatedModule {
+  public:
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clk,0,0);
+    VL_IN8(rst,0,0);
+    CData/*0:0*/ __Vclklast__TOP__clk;
+    VL_OUT16(led,15,0);
+    IData/*31:0*/ light__DOT__count;
+
+    // INTERNAL VARIABLES
+    Vlight__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vlight___024root(Vlight__Syms* symsp, const char* name);
+    ~Vlight___024root();
+    VL_UNCOPYABLE(Vlight___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard
--- npc/temp/csrc/water_light.cpp
+++ npc/temp/csrc/water_light.cpp
@@ -5,8 +5,28 @@ static Vlight light;
 
 void nvboard_bind_all_pins(Vlight *light);
 
-static void single_cycle() {}
+static void single_cycle() {
+  light.clk = 0;
+  light.eval();
+  light.clk = 1;
+  light.eval();
+}
 
-void reset(int n) {}
+void reset(int n) {
+  light.rst = 1;
+  while (n-- > 0)
+    single_cycle();
+  light.rst = 0;
+}
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  nvboard_bind_all_pins(&light);
+  nvboard_init();
+
+  reset(10);
+
+  while (1) {
+    nvboard_update();
+    single_cycle();
+  }
+}
