0854601e08fa4063ef96e9b7e71bc0226ab29e36
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 21:56:42 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  21:56:42 up 13:37,  1 user,  load average: 1.23, 1.42, 1.35

--- npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
+++ npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
@@ -26,7 +26,7 @@ module alu (a, b, opt, out, of, cf, zf, sf);
                 {cf, out} = a + b;
               end
       3'b001: begin // 减法
-                {cf, out} = a + (({4{1'b1}}^b) + 1);
+                {cf, out} = a + (({4{1'b1}}^b));
               end
       3'b010: begin // 取反
                 {cf, out} = {1'b0, ~a};
@@ -41,12 +41,12 @@ module alu (a, b, opt, out, of, cf, zf, sf);
                 {cf, out} = {1'b0, a ^ b};
               end
       3'b110: begin // 比较大小
-                {cf, out} = a + (({4{1'b1}}^b) + 1);
+                {cf, out} = a + (({4{1'b1}}^b));
                 if (sf ^ overflow) out = 4'b0001;
                 else out = 4'b0000;
               end
       3'b111: begin // 判断相等
-                {cf, out} = a + (({4{1'b1}}^b) + 1);
+                {cf, out} = a + (({4{1'b1}}^b));
                 if (zf == 0) out = 4'b0001;
                 else out = 4'b0000;
               end
