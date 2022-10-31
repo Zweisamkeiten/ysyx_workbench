5c23741b510fb75e8b245d728a08f481ff43c982
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 15:21:53 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  15:21:53 up  7:02,  3 users,  load average: 1.16, 1.05, 1.01

--- npc/digital_cricuit_ex/01_prior_encode83/vsrc/top.v
+++ npc/digital_cricuit_ex/01_prior_encode83/vsrc/top.v
@@ -1,15 +1,21 @@
-module prior_encode83(x, en, y);
+module prior_encode83(x, en, y, ins);
   input [7:0] x;
   input en;
   output reg [2:0] y;
+  output ins;
   
   integer i;
   always @(x or en) begin
+
+    if (x) ins = 1;
+    else ins = 0;
+
     if (en) begin
       y = 0;
       for ( i = 0; i <= 7; i = i + 1)
         if (x[i] == 1) y = i[2:0];
       end
     else y = 0;
+
   end
 endmodule
