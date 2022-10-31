a38047768095a06daafb5fb76915aa6aa1358f67
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 22:55:52 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  22:55:52 up 14:36,  1 user,  load average: 1.05, 1.25, 1.40

--- npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
+++ npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
@@ -3,48 +3,44 @@ module alu (a, b, opt, out, of, cf, zf, sf);
   input [3:0] b;
   input [2:0] opt; // 功能选择端
   output reg [3:0] out; // 结果
-  output reg of, cf, zf, sf; // 溢出, 进位, 零标志, 符号标志
+  output of, zf, sf; // 溢出, 零标志, 符号标志
+  output reg cf; // 进位标志
 
-  reg iszero, overflow;
-
-  MuxKey #(2, 1, 1) i0 (zf, iszero, {
-    1'b0, 1'b0,
-    1'b1, 1'b1
-    });
-
-  reg [3:0] t_no_Cin = {4{1'b1}}^b;
+  wire [4:0] tmp;
+  assign tmp = {1'b0, a} + {1'b0, b};
+  assign sf = tmp[3];
+  assign zf = |tmp ? 1'b0 : 1'b1;
+  assign of = tmp[4];
 
   always @ (a or b or opt) begin
     case (opt)
       3'b000: begin // 加法
-                {cf, out} <= a + b;
+                {cf, out} = a + b;
               end
       3'b001: begin // 减法
-                {cf, out} <= a + t_no_Cin + 1;
+                {cf, out} = a - b;
               end
       3'b010: begin // 取反
-                {cf, out} <= {1'b0, ~a};
+                {cf, out} = {1'b0, ~a};
               end
       3'b011: begin // 与
-                {cf, out} <= {1'b0, a & b};
+                {cf, out} = {1'b0, a & b};
               end
       3'b100: begin // 或
-                {cf, out} <= {1'b0, a & b};
+                {cf, out} = {1'b0, a & b};
               end
       3'b101: begin // 异或
-                {cf, out} <= {1'b0, a ^ b};
+                {cf, out} = {1'b0, a ^ b};
               end
       3'b110: begin // 比较大小
-                if (a < b) out <= 4'b0001;
-                else out <= 4'b0000;
+                if (a > b) out = 4'b0001;
+                else out = 4'b0000;
               end
       3'b111: begin // 判断相等
-                if (a == b) out <= 4'b0001;
-                else out <= 4'b0000;
+                if (a == b) out = 4'b0001;
+                else out = 4'b0000;
               end
-      default: {cf, out} <= 5'b00000;
+      default: {cf, out} = 5'b00000;
     endcase
-    overflow <= (a[3] == t_no_Cin[3]) && (out[3] != a[3]);
-    sf <= out[3];
   end
 endmodule
