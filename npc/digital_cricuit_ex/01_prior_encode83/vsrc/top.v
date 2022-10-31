4a06da7f7533a6e7f0323c885b22043f6f0107dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 15:57:07 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  15:57:07 up  7:37,  2 users,  load average: 1.11, 1.11, 1.14

--- npc/digital_cricuit_ex/01_prior_encode83/vsrc/top.v
+++ npc/digital_cricuit_ex/01_prior_encode83/vsrc/top.v
@@ -1,8 +1,9 @@
-module prior_encode83(x, en, y, ins);
+module prior_encode83(x, en, y, ins, dig);
   input [7:0] x;
   input en;
   output reg [2:0] y;
   output reg ins;
+  output reg [6:0] dig;
   
   integer i;
   always @(x or en) begin
@@ -13,9 +14,32 @@ module prior_encode83(x, en, y, ins);
     if (en) begin
       y = 0;
       for ( i = 0; i <= 7; i = i + 1)
-        if (x[i] == 1) y = i[2:0];
+      begin
+        if (x[i] == 1) begin
+          y = i[2:0];
+          decodeDig seg0(y, dig);
+        end
       end
+    end
     else y = 0;
 
+  end
+endmodule
+
+module decodeDig(x, out);
+  input [2:0] x;
+  output [6:0] out;
+
+  always @(x) begin
+    case (x)
+      3'b000 : out = 7'b0000001; // 0
+      3'b001 : out = 7'b1111001; // 1
+      3'b010 : out = 7'b1100100; // 2
+      3'b011 : out = 7'b0110000; // 3
+      3'b100 : out = 7'b0011001; // 4
+      3'b101 : out = 7'b0010010; // 5
+      3'b110 : out = 7'b0000010; // 6
+      3'b111 : out = 7'b1111000; // 7
+    endcase
   end
 endmodule
