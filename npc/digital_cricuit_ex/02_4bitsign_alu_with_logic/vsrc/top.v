0607562efae469fbbff77a51012d1b08d548cb7e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 22:22:01 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  22:22:01 up 14:02,  3 users,  load average: 1.37, 1.48, 1.45

--- npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
+++ npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
@@ -2,13 +2,16 @@ module alu (a, b, opt, out, of, cf, zf, sf);
   input [3:0] a; // 4 位带符号位补码
   input [3:0] b;
   input [2:0] opt; // 功能选择端
-  output reg [3:0] out; // 结果
+  output [3:0] out; // 结果
   output of, zf, sf; // 溢出, 零标志, 符号标志
   output reg cf; // 进位标志
 
+  reg [3:0] temp;
+  assign out = temp;
+
   wire iszero, overflow;
 
-  assign iszero = |out;
+  assign iszero = |temp;
   MuxKey #(2, 1, 1) i0 (zf, iszero, {
     1'b0, 1'b0,
     1'b1, 1'b1
@@ -16,41 +19,41 @@ module alu (a, b, opt, out, of, cf, zf, sf);
 
   wire [3:0] t_no_Cin;
   assign t_no_Cin = {4{1'b1}}^b;
-  assign overflow = (a[3] == t_no_Cin[3]) && (1'b0 != a[3]);
+  assign overflow = (a[3] == t_no_Cin[3]) && (temp[3] != a[3]);
 
-  assign sf = 1'b0;
+  assign sf = temp[3];
 
   always @ (a or b or opt) begin
     case (opt)
       3'b000: begin // 加法
-                {cf, out} = a + b;
+                {cf, temp} = a + b;
               end
       3'b001: begin // 减法
-                {cf, out} = a + t_no_Cin + 1;
+                {cf, temp} = a + t_no_Cin + 1;
               end
       3'b010: begin // 取反
-                {cf, out} = {1'b0, ~a};
+                {cf, temp} = {1'b0, ~a};
               end
       3'b011: begin // 与
-                {cf, out} = {1'b0, a & b};
+                {cf, temp} = {1'b0, a & b};
               end
       3'b100: begin // 或
-                {cf, out} = {1'b0, a & b};
+                {cf, temp} = {1'b0, a & b};
               end
       3'b101: begin // 异或
-                {cf, out} = {1'b0, a ^ b};
+                {cf, temp} = {1'b0, a ^ b};
               end
       3'b110: begin // 比较大小
-                {cf, out} = a + t_no_Cin + 1;
-                if (sf ^ overflow) out = 4'b0001;
-                else out = 4'b0000;
+                {cf, temp} = a + t_no_Cin + 1;
+                if (sf ^ overflow) temp = 4'b0001;
+                else temp = 4'b0000;
               end
       3'b111: begin // 判断相等
-                {cf, out} = a + t_no_Cin + 1;
-                if (zf == 0) out = 4'b0001;
-                else out = 4'b0000;
+                {cf, temp} = a + t_no_Cin + 1;
+                if (zf == 0) temp = 4'b0001;
+                else temp = 4'b0000;
               end
-      default: {cf, out} = 5'b00000;
+      default: {cf, temp} = 5'b00000;
     endcase
   end
 endmodule
