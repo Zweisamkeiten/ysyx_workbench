460b5979156133bed46835e2d3c198312ccd918f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 09:32:01 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  09:32:01 up 41 min,  2 users,  load average: 1.00, 1.04, 1.01

--- npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
+++ npc/digital_cricuit_ex/02_4bitsign_alu_with_logic/vsrc/top.v
@@ -6,42 +6,42 @@ module alu (a, b, opt, out, of, cf, zf, sf);
   output of, zf, sf; // 溢出, 零标志, 符号标志
   output reg cf; // 进位标志
 
-  wire [4:0] tmp;
-  assign tmp = {1'b0, a} + {1'b0, b};
+  reg [4:0] tmp;
+  assign out = tmp;
   assign sf = tmp[3];
   assign zf = |tmp ? 1'b0 : 1'b1;
-  assign of = tmp[4];
+  assign of = (a[3] == b[3]) && (tmp[3] == a[3]);
 
   always @ (a or b or opt) begin
     cf = 0;
     case (opt)
       3'b000: begin // 加法
-                {cf, out} = a + b;
+                {cf, tmp} = a + b;
               end
       3'b001: begin // 减法
-                {cf, out} = a + {4{1'b0}}^b + 1;
+                {cf, tmp} = a - b;
               end
       3'b010: begin // 取反
-                {cf, out} = {1'b0, ~a};
+                {cf, tmp} = {1'b0, ~a};
               end
       3'b011: begin // 与
-                {cf, out} = {1'b0, a & b};
+                {cf, tmp} = {1'b0, a & b};
               end
       3'b100: begin // 或
-                {cf, out} = {1'b0, a & b};
+                {cf, tmp} = {1'b0, a & b};
               end
       3'b101: begin // 异或
-                {cf, out} = {1'b0, a ^ b};
+                {cf, tmp} = {1'b0, a ^ b};
               end
       3'b110: begin // 比较大小
-                if (a > b) out = 4'b0001;
-                else out = 4'b0000;
+                if (a > b) tmp = 4'b0001;
+                else tmp = 4'b0000;
               end
       3'b111: begin // 判断相等
-                if (a == b) out = 4'b0001;
-                else out = 4'b0000;
+                if (a == b) tmp = 4'b0001;
+                else tmp = 4'b0000;
               end
-      default: {cf, out} = 5'b00000;
+      default: {cf, tmp} = 5'b00000;
     endcase
   end
 endmodule
