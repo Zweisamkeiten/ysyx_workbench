8a9183b4fc3a92657c645f744f61b4536ff13eb1
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 14:42:52 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  14:42:52 up  5:51,  1 user,  load average: 1.44, 1.48, 1.46

--- npc/digital_cricuit_ex/06_random_number_generator/vsrc/top.v
+++ npc/digital_cricuit_ex/06_random_number_generator/vsrc/top.v
@@ -11,7 +11,7 @@ module shift_register_8bit (init, clk, rst, out, seg0, seg1);
   decodeDig s1 (.x (temp[7:4]), .out (seg1));
 
   reg [7:0] count;
-  always @(posedge clk) begin
+  always @(posedge clk or posedge rst) begin
     if (rst) begin count <= 0; temp <= {8{1'b0}}; end
     if (count == 0) temp <= init;
     else temp <= {out[4]^out[3]^out[2]^out[0], out[7:1]};
