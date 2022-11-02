29e3759f1f02346b8b8f0223edd4379dc5cfac11
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-02 15:07:34 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  15:07:34 up  6:11,  3 users,  load average: 1.09, 1.11, 1.18

--- npc/digital_cricuit_ex/09_console/vsrc/top.v
+++ npc/digital_cricuit_ex/09_console/vsrc/top.v
@@ -35,15 +35,24 @@ parameter [7:0] sspase = 8'h29, senter = 8'h5a,
   s5 = 8'h2e, s6 = 8'h36, s7 = 8'h3d, s8 = 8'h3e, s9 = 8'h46;
 
 wire [7:0] scancode, cur_ascii;
-wire [9:0] chars; // 字符数量
+reg [9:0] chars; // 字符数量
 wire [9:0] cur_x; // 0 <= x <= 69
 wire [9:0] cur_y; // 0 <= y <= 30
 reg [7:0] vga_mem [2099:0]; // 30 * 70 = 2100
 
 assign cur_x = chars % 70;
 assign cur_y = chars / 16;
-assign chars = |cur_ascii ? (cur_ascii == 8'b11110000 ? chars - 1 : chars + 1) : chars;
 assign vga_mem[{cur_x[6:0], cur_y[4:0]}] = cur_ascii;
+always @(cur_ascii) begin
+  if (cur_ascii) begin
+    if (cur_ascii == 8'hf0) chars <= chars - 1;
+    else begin
+      if (cur_ascii == 8'd13) chars <= chars + 30 - cur_x;
+      else chars <= chars + 1;
+    end
+  end
+  else chars <= chars;
+end
 
 ps2_keyboard mcur_y_keyboard(
     .clk(clk),
