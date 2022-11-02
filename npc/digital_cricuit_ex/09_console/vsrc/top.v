7b253857cdd7d03942e60da8d6bc69d620206733
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-02 16:42:28 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  16:42:28 up  7:46,  3 users,  load average: 1.45, 1.74, 1.63

--- npc/digital_cricuit_ex/09_console/vsrc/top.v
+++ npc/digital_cricuit_ex/09_console/vsrc/top.v
@@ -40,7 +40,7 @@ wire [9:0] cur_x; // 0 <= x <= 69
 wire [9:0] cur_y; // 0 <= y <= 30
 reg [7:0] vga_mem [2099:0]; // 30 * 70 = 2100
 
-assign cur_x = (chars - 1) % 70;
+assign cur_x = chars % 70;
 assign cur_y = chars / 70;
 assign vga_mem[{cur_x[6:0], cur_y[4:0]}] = cur_ascii != 8'hf0 ? cur_ascii : 8'h00;
 
