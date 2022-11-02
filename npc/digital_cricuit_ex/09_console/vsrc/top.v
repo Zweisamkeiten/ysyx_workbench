ff73bcf3c3c56dac406a91e04dd8dde5866beea5
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-02 15:10:10 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  15:10:10 up  6:14,  3 users,  load average: 1.26, 1.17, 1.19

--- npc/digital_cricuit_ex/09_console/vsrc/top.v
+++ npc/digital_cricuit_ex/09_console/vsrc/top.v
@@ -36,8 +36,8 @@ parameter [7:0] sspase = 8'h29, senter = 8'h5a,
 
 wire [7:0] scancode, cur_ascii;
 wire [9:0] chars; // 字符数量
-reg [9:0] temps;
-assign chars = temps;
+reg [9:0] temp;
+assign chars = temp;
 wire [9:0] cur_x; // 0 <= x <= 69
 wire [9:0] cur_y; // 0 <= y <= 30
 reg [7:0] vga_mem [2099:0]; // 30 * 70 = 2100
