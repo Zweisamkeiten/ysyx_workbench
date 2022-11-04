161b844dcc2f710ad4559b4df737e68b60e674ea
22050710-Einsam <snitxmhm@gmail.com>
2022-11-04 15:13:08 +0800

    feat(pa1): cmd_info r

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -74,6 +74,23 @@ static int cmd_si(char *args) {
   return -1;
 }
 
+static int cmd_info(char *args) {
+  char *sub_cmd = strtok(args, " ");
+  if (sub_cmd != NULL) {
+    if (strcmp(sub_cmd, "r") == 0) {
+      // print the reg state
+      isa_reg_display();
+      printf("%s\t\t%#lx\t%lu\n", "pc", cpu.pc, cpu.pc);
+      return 0;
+    }
+    else if (strcmp(sub_cmd, "w") == 0) {
+      // print the watchpoint state
+    }
+  }
+  Log(ANSI_FMT("ERROR: info [r/w]", ANSI_FG_RED));
+  return -1;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -87,6 +104,7 @@ static struct {
 
   /* TODO: Add more commands */
   { "si", "single step", cmd_si },
+  { "info", "print the program state", cmd_info },
 
 };
 
