fd232ffbccb555fd97d5f95a4865796d47a290b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 21:33:17 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  21:33:17 up 12:56,  2 users,  load average: 1.07, 1.10, 1.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -30,6 +30,7 @@ enum {
   TK_DIVIDE,
   TK_BRACKET_L,
   TK_BRACKET_R,
+  TK_NEGATIVE,
   TK_NOTYPE = 256,
 };
 
@@ -80,7 +81,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[65536] __attribute__((used)) = {};
+static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
