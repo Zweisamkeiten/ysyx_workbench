65e6214f0fda4a0246409c56f5ba45bc7e70508c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 11:04:57 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  11:04:57 up  2:17,  2 users,  load average: 1.02, 1.07, 1.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -204,12 +204,8 @@ int find_main_operator(int p, int q) {
     default:
       if (parentheses_stack == 0 ) {
         if (op_position == p || priority(current_type) <= priority(tokens[op_position].type)) {
-          if (priority(tokens[op_position].type) == priority(current_type)) {
-              switch (current_type) {
-              case TK_NEGATIVE:
-              case TK_DEREFERENCE: break;
-              default: if(is_binary_operator(current_type)) op_position = i;
-              }
+          if (current_type == TK_NEGATIVE && tokens[op_position].type == TK_NEGATIVE) {
+              break;
           }
           else {
             op_position = i;
