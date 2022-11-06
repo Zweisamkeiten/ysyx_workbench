c7d58b78c4093631d3749d9d302ce3f5e7d14986
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 10:32:55 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  10:32:55 up  1:45,  2 users,  load average: 1.14, 1.19, 1.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -34,7 +34,7 @@ static char* rl_gets() {
     line_read = NULL;
   }
 
-  line_read = readline("(nemu) ");
+  line_read = readline(ANSI_FMT("(nemu) ", ANSI_FG_BLUE));
 
   if (line_read && *line_read) {
     add_history(line_read);
