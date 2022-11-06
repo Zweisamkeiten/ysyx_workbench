f96b95e962f6ce21325e810cf574b70150fd3793
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 14:53:10 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  14:53:10 up  6:05,  3 users,  load average: 1.22, 1.20, 1.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -71,7 +71,7 @@ static int cmd_si(char *args) {
     return 0;
   }
 
-  printf(ANSI_FMT("ERROR: si [steps]", ANSI_FG_RED));
+  printf(ANSI_FMT("ERROR: si [steps]\n", ANSI_FG_RED));
   return 0;
 }
 
@@ -88,7 +88,7 @@ static int cmd_info(char *args) {
       // print the watchpoint state
     }
   }
-  printf(ANSI_FMT("ERROR: info <r/w>", ANSI_FG_RED));
+  printf(ANSI_FMT("ERROR: info <r/w>\n", ANSI_FG_RED));
   return 0;
 }
 
@@ -121,7 +121,7 @@ static int cmd_x(char *args) {
     }
   }
 
-  printf(ANSI_FMT("ERROR: x <N> <EXPR>", ANSI_FG_RED));
+  printf(ANSI_FMT("ERROR: x <N> <EXPR>\n", ANSI_FG_RED));
   return 0;
 }
 
