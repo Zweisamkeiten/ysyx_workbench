21fba2f924ac677f4cf421c17e4f4f71d7bfaca4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 09:25:13 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  09:25:13 up 37 min,  2 users,  load average: 1.14, 1.14, 1.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -92,7 +92,7 @@ static int nr_token __attribute__((used))  = 0;
 
 // check the token type is not binary operator
 bool is_binary_operator(int type) {
-  if (TK_PLUS <= type && type <= TK_DIVIDE) {
+  if (TK_EQ <= type && type <= TK_DIVIDE) {
     return true;
   }
   return false;
