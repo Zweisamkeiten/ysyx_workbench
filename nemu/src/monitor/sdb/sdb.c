c0d8a4a4984335be9dfc658bc36ac437d13e0443
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 14:55:28 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  14:55:28 up  6:07,  3 users,  load average: 1.28, 1.22, 1.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -127,20 +127,31 @@ static int cmd_x(char *args) {
 
 static int cmd_p(char *args) {
   char *e= args;
-  // assume the expr is valid
-  bool success = true;
-  word_t result = expr(e, &success);
-  if (success == true) {
-    printf(ANSI_FMT("$expr = ", ANSI_FG_CYAN) ANSI_FMT("0x%016lx\t", ANSI_FG_GREEN) ANSI_FMT("%020lu\n", ANSI_FG_GREEN), result, result);
-  } else {
-    printf(ANSI_FMT("A syntax error in expression.\n", ANSI_FG_RED));
+
+  if (e != NULL) {
+    // assume the expr is valid
+    bool success = true;
+    word_t result = expr(e, &success);
+    if (success == true) {
+      printf(ANSI_FMT("$expr = ", ANSI_FG_CYAN) ANSI_FMT("0x%016lx\t", ANSI_FG_GREEN) ANSI_FMT("%020lu\n", ANSI_FG_GREEN), result, result);
+    } else {
+      printf(ANSI_FMT("A syntax error in expression.\n", ANSI_FG_RED));
+    }
+    return 0;
   }
+
+  printf(ANSI_FMT("ERROR: p <EXPR>\n", ANSI_FG_RED));
   return 0;
 }
 
 static int cmd_w(char *args) {
   char *e= args;
-  set_watchpoint(e);
+
+  if (e != NULL) {
+    set_watchpoint(e);
+    return 0;
+  }
+  printf(ANSI_FMT("ERROR: w <EXPR>\n", ANSI_FG_RED));
   return 0;
 }
 
