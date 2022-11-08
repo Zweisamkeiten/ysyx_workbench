d2cf3a6355fedc3c58eb823dad3cdae60a1e3e94
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-08 10:56:41 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 15 Oct 2022 14:00:51 +0000 x86_64 GNU/Linux  10:56:41 up  2:02,  5 users,  load average: 1.16, 1.24, 1.21

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -19,8 +19,6 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-  0x00100313,
-  0x00331293,
   0x00000297,  // auipc t0,0
   0x0002b823,  // sd  zero,16(t0)
   0x0102b503,  // ld  a0,16(t0)
