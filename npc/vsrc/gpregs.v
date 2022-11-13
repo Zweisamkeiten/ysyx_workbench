6570580b60a8c4944d716f87b0c86d71404926db
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 19:44:16 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  19:44:16 up  9:45,  3 users,  load average: 1.29, 1.22, 1.09

--- npc/vsrc/gpregs.v
+++ npc/vsrc/gpregs.v
@@ -3,7 +3,7 @@ module ysyx_22050710_gpr #(ADDR_WIDTH = 5, DATA_WIDTH = 64) (
   input [ADDR_WIDTH-1:0] i_ra, i_rb, i_waddr,
   input [DATA_WIDTH-1:0] i_wdata,
   input i_wen,
-  output [DATA_WIDTH-1:0] o_busA, o_busB,
+  output [DATA_WIDTH-1:0] o_busA, o_busB
 );
   reg [DATA_WIDTH-1:0] rf [ADDR_WIDTH-1:0];
 
