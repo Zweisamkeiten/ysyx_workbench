acc7377aee85415f6f73aab73454211979b57d0f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 19:47:38 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  19:47:38 up  9:48,  3 users,  load average: 1.19, 1.22, 1.11

--- npc/vsrc/adder.v
+++ npc/vsrc/adder.v
@@ -2,7 +2,6 @@ module ysyx_22050710_adder #(WIDTH = 1) (
   input [WIDTH-1:0] i_x,
   input [WIDTH-1:0] i_y,
   output [WIDTH-1:0] o_result,
-  output o_c
 );
-  assign {o_c, o_result} = i_x + i_y;
+  assign o_result = i_x + i_y;
 endmodule
