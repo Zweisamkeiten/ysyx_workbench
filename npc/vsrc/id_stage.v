f8fefcc45712c498970ee623416758fae8ab584b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 16:51:17 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  16:51:17 up  8:27,  3 users,  load average: 1.31, 1.47, 1.42

--- npc/vsrc/id_stage.v
+++ npc/vsrc/id_stage.v
@@ -26,8 +26,8 @@ module ysyx_22050710_idu (
   assign immB = {{52{i_inst[31]}}, i_inst[7], i_inst[30:25], i_inst[11:8], 1'b0};
   assign immJ = {{44{i_inst[31]}}, i_inst[19:12], i_inst[20], i_inst[30:21], 1'b0};
   
-  wire inst_addi   = |(opcode[6:0] ~^ 7'b0010011) & |(funct3[2:0] ~^ 3'b000);
-  wire inst_ebreak = |(opcode[6:0] ~^ 7'b1110011) & |(funct3[2:0] ~^ 3'b000);
+  wire inst_addi   = (|(opcode[6:0] ~^ 7'b0010011)) & (|(funct3[2:0] ~^ 3'b000));
+  wire inst_ebreak = (|(opcode[6:0] ~^ 7'b1110011)) & (|(funct3[2:0] ~^ 3'b000));
 
   wire inst_type_r = 1'b0;
   wire inst_type_i = |{inst_addi, inst_ebreak};
