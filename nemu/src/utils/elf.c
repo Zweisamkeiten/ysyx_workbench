b5624f54af19d6a023a74bee406f1b701d06fd2f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-17 12:10:49 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  12:10:49 up  3:03,  2 users,  load average: 1.26, 1.27, 1.29

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -1,8 +1,7 @@
-#ifdef CONFIG_FTRACE
 #include <common.h>
 
 // load elf file to mem;
-uint8_t * const elf_mem_p = NULL;
+uint8_t * elf_mem_p = NULL;
 Elf_Ehdr *ehdr = NULL;
 
 void init_elf(const char *elf_file) {
@@ -20,22 +19,20 @@ void init_elf(const char *elf_file) {
   Log("The elf is %s, size = %ld", elf_file, size);
   
   fseek(fp, 0, SEEK_SET);
-  elf_mem_p = malloc(size);
+  elf_mem_p = (uint8_t *)malloc(size);
   fread(elf_mem_p, size, 1, fp);
 
-  ehdr = Elf_Ehdr *elf_mem_p;
+  ehdr = (Elf_Ehdr *)elf_mem_p;
 
   /* * Check to see if the ELF magic (The first 4 bytes) * match up as 0x7f E L
    * F */
   if (elf_mem_p[0] != 0x7f && strcmp((char *)&elf_mem_p[1], "ELF")) {
-    Assert("%s is not an ELF file\n", elf_file);
+    Assert(0, "%s is not an ELF file\n", elf_file);
   }
 
   if (ehdr->e_type != ET_EXEC) {
-    Assert("%s is not an executable\n", elf_file);
+    Assert(0, "%s is not an executable\n", elf_file);
   }
 
   Log("Program Entry point: 0x%lx\n", ehdr->e_entry);
 }
-
-#endif
