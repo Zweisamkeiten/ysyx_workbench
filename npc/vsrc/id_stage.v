61994ab19b5463e86d0b7fe6069b8c78fe864ad7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 22:18:09 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  22:18:09 up 13:27,  7 users,  load average: 1.24, 1.23, 1.23

--- npc/vsrc/id_stage.v
+++ npc/vsrc/id_stage.v
@@ -30,6 +30,7 @@ module ysyx_22050710_idu (
   wire inst_lui    = (opcode[6:0] == 7'b0110111);
   wire inst_auipc  = (opcode[6:0] == 7'b0010111);
   wire inst_jal    = (opcode[6:0] == 7'b1101111);
+  wire inst_jalr   = (opcode[6:0] == 7'b1100111) & (funct3[2:0] == 3'b000);
   wire inst_addi   = (opcode[6:0] == 7'b0010011) & (funct3[2:0] == 3'b000);
   wire inst_ebreak = (opcode[6:0] == 7'b1110011) & (funct3[2:0] == 3'b000);
 
@@ -37,7 +38,7 @@ module ysyx_22050710_idu (
   wire inst_sd     = (opcode[6:0] == 7'b0100011) & (funct3[2:0] == 3'b011);
 
   wire inst_type_r = 1'b0;
-  wire inst_type_i = |{inst_addi, inst_ebreak};
+  wire inst_type_i = |{inst_addi, inst_ebreak, inst_jalr};
   wire inst_type_u = |{inst_lui, inst_auipc};
   wire inst_type_s = |{inst_sd};
   wire inst_type_b = 1'b0;
