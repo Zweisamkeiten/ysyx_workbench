bf3c288c790443c9cf02da767aec3b5fa3cb6ab9
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 15:54:00 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  15:54:00 up  7:02,  6 users,  load average: 1.61, 1.08, 0.66

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -17,20 +17,3 @@
 #include <cpu/cpu.h>
 #include <difftest-def.h>
 #include <memory/paddr.h>
-
-void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
-}
-
-void difftest_exec(uint64_t n) {
-  assert(0);
-}
-
-void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
-
-void difftest_init(int port) {
-  /* Perform ISA dependent initialization. */
-  init_isa();
-}
