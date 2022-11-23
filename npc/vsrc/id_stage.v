5d4824eec12b2824253a714557a164755ef383c2
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 17:42:02 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  17:42:02 up  8:50,  6 users,  load average: 1.38, 1.27, 1.22

--- npc/vsrc/id_stage.v
+++ npc/vsrc/id_stage.v
@@ -27,6 +27,7 @@ module ysyx_22050710_idu (
   assign immJ = {{44{i_inst[31]}}, i_inst[19:12], i_inst[20], i_inst[30:21], 1'b0};
   
   // RV32I and RV64I
+  wire inst_lui    = (opcode[6:0] == 7'b0110111);
   wire inst_auipc  = (opcode[6:0] == 7'b0010111);
   wire inst_addi   = (opcode[6:0] == 7'b0010011) & (funct3[2:0] == 3'b000);
   wire inst_ebreak = (opcode[6:0] == 7'b1110011) & (funct3[2:0] == 3'b000);
@@ -34,7 +35,7 @@ module ysyx_22050710_idu (
 
   wire inst_type_r = 1'b0;
   wire inst_type_i = |{inst_addi, inst_ebreak};
-  wire inst_type_u = |{inst_auipc};
+  wire inst_type_u = |{inst_lui, inst_auipc};
   wire inst_type_s = 1'b0;
   wire inst_type_b = 1'b0;
   wire inst_type_j = 1'b0;
@@ -72,7 +73,7 @@ module ysyx_22050710_idu (
   assign o_ALUBsrc = {o_ALUAsrc, |inst_type[5:0]};
 
   wire alu_plus, alu_ebreak;
-  assign alu_plus = |{inst_auipc, inst_addi};
+  assign alu_plus = |{inst_lui, inst_auipc, inst_addi};
   assign alu_ebreak = inst_ebreak;
 
   MuxKeyWithDefault #(.NR_KEY(2), .KEY_LEN(2), .DATA_LEN(4)) u_mux2 (
