efe3c265ec67a0cdf125770e8b391c42500e9879
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 10:10:16 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  10:10:16 up  1:07,  5 users,  load average: 1.40, 1.28, 1.19

--- npc/csrc/include/debug.h
+++ npc/csrc/include/debug.h
@@ -27,11 +27,7 @@
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
-      MUXDEF(CONFIG_TARGET_AM, printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
-        (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__))); \
-      IFNDEF(CONFIG_TARGET_AM, extern FILE* log_fp; fflush(log_fp)); \
-      extern void assert_fail_msg(); \
-      assert_fail_msg(); \
+      fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__); \
       assert(cond); \
     } \
   } while (0)
