1c73d8f9db08db6565c65878ae4be7f26c46caf3
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 10:53:13 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  10:53:13 up  1:50,  6 users,  load average: 1.36, 1.41, 1.35

---
+++ npc/csrc/src/monitor/sdb/sdb.c
@@ -0,0 +1,20 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+static char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline(ANSI_FMT("(npc) ", ANSI_FG_YELLOW));
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
