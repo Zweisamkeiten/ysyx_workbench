172176f826770c1a650efcc677dd5b5893358d41
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 10:11:48 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  10:11:48 up  1:08,  5 users,  load average: 1.21, 1.24, 1.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -100,6 +100,6 @@ int main(int argc, char **argv, char **env) {
 
   switch (npc_state.state) {
     case NPC_END: printf(ANSI_FMT( "Successful exit.\n", ANSI_FG_GREEN)); break;
-    case NPC_ABORT: printf(ANSI_FMT("Unimplemented inst at PC: 0x%016lx\n", ANSI_FG_RED), pc); exit(1);
+    case NPC_ABORT: printf(ANSI_FMT("Unimplemented inst at PC: 0x%016lx\n", ANSI_FG_RED), *pc); exit(1);
   }
 }
