a30482583e56d68b41bf3b30862248ed650546b3
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 14:14:45 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  14:14:45 up  5:11,  6 users,  load average: 1.35, 1.34, 1.30

--- npc/csrc/src/monitor/sdb/sdb.c
+++ npc/csrc/src/monitor/sdb/sdb.c
@@ -19,3 +19,40 @@ static char* rl_gets() {
 
   return line_read;
 }
+
+static struct {
+  const char *name;
+  const char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+};
+
+#define NR_CMD ARRLEN(cmd_table)
+
+void sdb_mainloop() {
+  for (char *str; (str = rl_gets()) != NULL; ) {
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
+
+    int i;
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) { return; }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  }
+}
