5e30d1383708317473e23f2461884362025e53c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 22:29:56 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  22:29:56 up 13:26,  6 users,  load average: 1.18, 1.27, 1.27

--- npc/csrc/src/isa/init.c
+++ npc/csrc/src/isa/init.c
@@ -18,9 +18,6 @@ static const uint32_t img [] = {
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
-
-  /* The zero register is always 0. */
-  cpu.gpr[0] = 0;
 }
 
 void init_isa() {
