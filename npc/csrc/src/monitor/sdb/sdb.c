de92b0bbe27843820f53d00c6bf973b6042f5de6
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 18:17:43 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:17:43 up  9:14,  6 users,  load average: 1.19, 1.32, 1.35

--- npc/csrc/src/monitor/sdb/sdb.c
+++ npc/csrc/src/monitor/sdb/sdb.c
@@ -1,3 +1,5 @@
+#include <isa.h>
+#include <cpu/cpu.h>
 #include <common.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -20,14 +22,53 @@ static char* rl_gets() {
   return line_read;
 }
 
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+
+static int cmd_q(char *args) {
+  npc_state.state = NPC_QUIT;
+  return -1;
+}
+
+static int cmd_help(char *args);
+
 static struct {
   const char *name;
   const char *description;
   int (*handler) (char *);
 } cmd_table [] = {
+  { "help", "Display information about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
 };
 
 #define NR_CMD ARRLEN(cmd_table)
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
 
 void sdb_mainloop() {
   for (char *str; (str = rl_gets()) != NULL; ) {
