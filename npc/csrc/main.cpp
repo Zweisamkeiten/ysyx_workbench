eecaaecdceebf2bd0cc96d67e73ade22426a08eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 18:12:49 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:12:49 up  9:09,  6 users,  load average: 1.38, 1.41, 1.39

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,18 +1,7 @@
 #include <stdio.h>
-#include <stdarg.h>
 #include <common.h>
 #include <isa.h>
 #include <memory/paddr.h>
-
-static const uint32_t img [] = {
-  0x00100093,
-  0x00200113,
-  0x00300193,
-  0x00400213,
-  0x00500293,
-  0x00600313,
-  0x00100073,
-};
 
 NPCState npc_state = {.state = NPC_STOP};
 
