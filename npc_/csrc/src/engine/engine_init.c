9c0ce98722976b36b91992d9ac9a9da4c7beef38
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 18:50:57 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:50:57 up  5:00,  8 users,  load average: 1.41, 1.46, 1.41

--- npc_/csrc/src/engine/engine_init.c
+++
@@ -1,8 +0,0 @@
-#include <cpu/cpu.h>
-
-void sdb_mainloop();
-
-void engine_start() {
-  /* Receive commands from user. */
-  sdb_mainloop();
-}
--- npc_/csrc/src/utils/rand.c
+++
@@ -1,6 +0,0 @@
-#include <common.h>
-#include <time.h>
-
-void init_rand() {
-  srand(time(0));
-}
