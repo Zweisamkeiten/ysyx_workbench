0bdf2b6e0f594036c12933c0628cabbafa609187
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 23:27:57 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  23:27:57 up  9:37,  6 users,  load average: 1.34, 1.32, 1.34

--- npc/csrc/src/monitor/monitor.c
+++ npc/csrc/src/monitor/monitor.c
@@ -8,7 +8,7 @@ void init_sdb();
 void init_sim();
 
 static void welcome() {
-  printf("Trace: %s", MUXNDEF(CONFIG_TRACE, ANSI_FMT("ON\n", ANSI_FG_GREEN), ANSI_FMT("OFF\n", ANSI_FG_RED)));
+  printf("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON\n", ANSI_FG_GREEN), ANSI_FMT("OFF\n", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, printf("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig\n"));
@@ -19,6 +19,7 @@ static void welcome() {
 
 static long load_img(int argc, char ** argv) {
   if (argc != 2) {
+    printf("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON\n", ANSI_FG_GREEN), ANSI_FMT("OFF\n", ANSI_FG_RED)));
     printf("No image is given. Use the default built-in image.\n");
     return 0;
   }
