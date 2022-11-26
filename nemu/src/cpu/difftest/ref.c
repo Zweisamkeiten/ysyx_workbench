e60389957dc88795648aee16b6ef59bc2e2632ab
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-26 17:59:34 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  17:59:34 up  5:29,  6 users,  load average: 1.54, 1.34, 1.18

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,7 +21,7 @@
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     for (size_t i = 0; i < n; i++) {
-      paddr_write(addr, 1, *((uint8_t *)buf+i));
+      paddr_write(addr+i, 1, *((uint8_t *)buf+i));
     }
   } else {
     assert(0);
