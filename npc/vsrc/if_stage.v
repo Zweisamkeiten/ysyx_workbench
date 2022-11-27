e82f29e9b7b82b34ea03ef6b56e6981a68494ddd
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-27 14:56:22 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  14:56:22 up  6:13,  5 users,  load average: 1.19, 1.27, 1.27

--- npc/vsrc/if_stage.v
+++ npc/vsrc/if_stage.v
@@ -1,5 +1,5 @@
 // ysyx_22050710
-import "DPI-C" function void set_pc_ptr(input logic [63:0] a[]);
+import "DPI-C" function void set_inst_ptr(input logic [31:0] a[]);
 module ysyx_22050710_ifu (
   input i_clk, i_rst,
   input [63:0] i_pc,
