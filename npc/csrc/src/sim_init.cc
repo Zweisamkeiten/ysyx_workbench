c74540d8e1730bc415b8b5332ff84c597a8a73ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-27 16:56:53 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  16:56:53 up  8:13,  7 users,  load average: 1.14, 1.23, 1.34

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -1,10 +1,14 @@
 #include <sim.hpp>
 #include <common.h>
 #include <isa.h>
+extern "C" {
+  #include <memory/paddr.h>
+}
 
 Vtop *top;
 VerilatedContext *contextp = NULL;
 VerilatedVcdC *tfp = NULL;
+uint64_t * npcpc;
 
 void set_state_end() {
   npc_state.state = NPC_END;
@@ -14,21 +18,23 @@ void set_state_abort() {
   npc_state.state = NPC_ABORT;
 }
 
-extern "C" void set_pc_ptr(const svOpenArrayHandle r) {
-  npcpc = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
-}
-
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu.gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
-extern "C" void set_inst_ptr(const svOpenArrayHandle r) {
-  cpu.inst = (uint32_t *)(((VerilatedDpiOpenVar*)r)->datap());
+extern "C" void set_pc_ptr(const svLogicVecVal* a) {
+  npcpc = (uint64_t *)(a);
+}
+
+extern "C" void set_inst_ptr(const svLogicVecVal* a) {
+  cpu.inst = (uint32_t *)(a);
 }
 
 extern "C" void npc_pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   *rdata = paddr_read(raddr, 8);
+  printf("0x%016llx\n", *rdata);
+  printf("npc: 0x%016lx\n", *npcpc);
 }
 
 extern "C" void npc_pmem_write(long long waddr, long long wdata, char wmask) {
@@ -70,7 +76,7 @@ extern "C" void init_sim() {
 
   npc_state.state = NPC_RUNNING;
 
-  cpu.pc = top->o_pc;
+  cpu.pc = *npcpc;
 }
 
 extern "C" void end_sim() {
