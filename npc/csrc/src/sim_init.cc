857303853d50ba5b5677e6d086c3bdc716e1d46b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-27 19:11:29 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  19:11:29 up 10:28,  9 users,  load average: 3.12, 2.93, 2.37

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -22,19 +22,19 @@ extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu.gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
-extern "C" void set_pc_ptr(const svLogicVecVal* a) {
-  npcpc = (uint64_t *)((VerilatedVarProps*)a);
+extern "C" void set_pc_ptr(const svOpenArrayHandle r) {
+  npcpc = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
-extern "C" void set_inst_ptr(const svLogicVecVal* a) {
-  cpu.inst = (uint32_t *)((VerilatedVarProps*)a);
+extern "C" void set_inst_ptr(const svOpenArrayHandle r) {
+  cpu.inst = (uint32_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
 extern "C" void npc_pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   *rdata = paddr_read(raddr, 8);
-  printf("0x%016llx\n", *rdata);
-  printf("npc: 0x%016lx\n", *npcpc);
+  // printf("0x%016llx\n", *rdata);
+  // printf("npc: 0x%016lx\n", *npcpc);
 }
 
 extern "C" void npc_pmem_write(long long waddr, long long wdata, char wmask) {
