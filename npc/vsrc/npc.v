31ade4a6b25dd048d61ecfbbe433c26b9f4d7cb8
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-27 15:41:20 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  15:41:20 up  6:58,  5 users,  load average: 1.35, 1.35, 1.43

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -1,6 +1,6 @@
 // ysyx_22050710
-import "DPI-C" function void set_inst_ptr(input int a);
-import "DPI-C" function void set_pc_ptr(input longint a);
+import "DPI-C" function void set_inst_ptr(input logic [31:0] a);
+import "DPI-C" function void set_pc_ptr(input logic [63:0] a);
 
 module ysyx_22050710_npc (
   input i_clk,
@@ -9,17 +9,17 @@ module ysyx_22050710_npc (
 
   initial begin
     set_pc_ptr(pc);
-    set_inst_ptr(insttemp);
+    set_inst_ptr(inst);
   end
 
-  wire [31:0] insttemp;
+  wire [31:0] inst;
   wire [63:0] pc;
   wire [31:0] unused;
   ysyx_22050710_ifu u_ifu (
     .i_clk(i_clk),
     .i_rst(i_rst),
     .i_pc(pc),
-    .o_inst(insttemp),
+    .o_inst(inst),
     .o_unused(unused)
   );
 
@@ -43,7 +43,7 @@ module ysyx_22050710_npc (
   wire MemtoReg, MemWr;
   wire [2:0] MemOP;
   ysyx_22050710_idu u_idu (
-    .i_inst(insttemp),
+    .i_inst(inst),
     .o_imm(imm),
     .o_ra(ra), .o_rb(rb), .o_rd(rd),
     .o_RegWr(RegWr),
