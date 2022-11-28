2ec2fb904f0754ec5d75161059523eae7d280589
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-28 15:18:56 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  15:18:56 up  6:09,  8 users,  load average: 1.35, 1.33, 1.27

--- npc/vsrc/if_stage.v
+++ npc/vsrc/if_stage.v
@@ -2,13 +2,13 @@
 module ysyx_22050710_ifu (
   input i_clk, i_rst,
   input [63:0] i_pc,
-  output [63:0] o_inst
-  /* output [31:0] o_unused */
+  output [31:0] o_inst,
+  output [31:0] o_unused
 );
 
   wire [63:0] rdata;
-  assign o_inst = rdata[63:0];
-  /* assign o_unused = rdata[63:32]; */
+  assign o_inst = rdata[31:0];
+  assign o_unused = rdata[63:32];
 
   always @(posedge i_clk) begin
     if (!i_rst) begin
--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -1,5 +1,5 @@
 // ysyx_22050710
-import "DPI-C" function void set_inst_ptr(input logic [63:0] a[]);
+import "DPI-C" function void set_inst_ptr(input logic [31:0] a[]);
 import "DPI-C" function void set_pc_ptr(input logic [63:0] a[]);
 
 module ysyx_22050710_npc (
@@ -12,15 +12,15 @@ module ysyx_22050710_npc (
     set_inst_ptr(inst);
   end
 
-  wire [63:0] inst;
+  wire [31:0] inst;
   wire [63:0] pc;
   wire [31:0] unused;
   ysyx_22050710_ifu u_ifu (
     .i_clk(i_clk),
     .i_rst(i_rst),
     .i_pc(pc),
-    .o_inst(inst)
-    /* .o_unused(unused) */
+    .o_inst(inst),
+    .o_unused(unused)
   );
 
   wire [63:0] pc_adder = (PCBsrc ? rs1 : pc) + (PCAsrc ? imm : 64'd4);
@@ -49,7 +49,7 @@ module ysyx_22050710_npc (
   wire MemtoReg, MemWr;
   wire [2:0] MemOP;
   ysyx_22050710_idu u_idu (
-    .i_inst(inst[31:0]),
+    .i_inst(inst),
     .o_imm(imm),
     .o_ra(ra), .o_rb(rb), .o_rd(rd),
     .o_RegWr(RegWr),
