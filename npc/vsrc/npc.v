88ed161411cd1a3326829795e03efe7fc9270e95
22050710-Einsam <snitxmhm@gmail.com>
2022-11-28 20:08:56 +0800

    feat(npc): datamem: dpi-c npc_pmem_read npc_pmem_write to implement the mem opt
        1. delete i_inst o_pc signals at the top module
    
        2. fix trace and cpu-exec because the top module change

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -1,21 +1,34 @@
 // ysyx_22050710
-import "DPI-C" function void set_inst_ptr(input logic [31:0] a[]);
-
 module ysyx_22050710_npc (
   input i_clk,
-  input i_rst,
-  input [31:0] i_inst,
-  output [63:0] o_pc
+  input i_rst
 );
-  initial set_inst_ptr(i_inst);
-  wire [63:0] pc_adder = (PCBsrc ? rs1 : o_pc) + (PCAsrc ? imm : 64'd4);
-  ysyx_22050710_pc u_pc (i_clk, i_rst, .i_load(1'b1), .i_in(pc_adder), o_pc);
+
+  wire [31:0] inst;
+  wire [63:0] pc;
+  wire [31:0] unused;
+  ysyx_22050710_ifu u_ifu (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .i_pc(pc),
+    .o_inst(inst),
+    .o_unused(unused)
+  );
+
+  wire [63:0] pc_adder = (PCBsrc ? rs1 : pc) + (PCAsrc ? imm : 64'd4);
+  ysyx_22050710_pc u_pc (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .i_load(1'b1),
+    .i_in(pc_adder),
+    .o_pc(pc)
+  );
 
   wire [63:0] rs1, rs2, ALUresult;
   ysyx_22050710_gpr #(.ADDR_WIDTH(5), .DATA_WIDTH(64)) u_gprs (
     .i_clk(i_clk),
     .i_ra(ra), .i_rb(rb), .i_waddr(rd),
-    .i_wdata(ALUresult), .i_wen(RegWr),
+    .i_wdata(muxbusW), .i_wen(RegWr),
     .o_busA(rs1), .o_busB(rs2)
   );
 
@@ -25,19 +38,36 @@ module ysyx_22050710_npc (
   wire [1:0] ALUBsrc;
   wire [3:0] ALUctr;
   wire PCAsrc, PCBsrc;
+  wire MemtoReg, MemWr;
+  wire [2:0] MemOP;
   ysyx_22050710_idu u_idu (
-    .i_inst(i_inst),
+    .i_inst(inst),
     .o_imm(imm),
     .o_ra(ra), .o_rb(rb), .o_rd(rd),
     .o_RegWr(RegWr),
     .o_ALUAsrc(ALUAsrc), .o_ALUBsrc(ALUBsrc), .o_ALUctr(ALUctr),
-    .o_PCAsrc(PCAsrc), .o_PCBsrc(PCBsrc)
+    .o_PCAsrc(PCAsrc), .o_PCBsrc(PCBsrc),
+    .o_MemtoReg(MemtoReg), .o_MemWr(MemWr), .o_MemOP(MemOP)
   );
 
   ysyx_22050710_exu u_exu (
     .i_rs1(rs1), .i_rs2(rs2),
-    .i_imm(imm), .i_pc(o_pc),
+    .i_imm(imm), .i_pc(pc),
     .i_ALUAsrc(ALUAsrc), .i_ALUBsrc(ALUBsrc), .i_ALUctr(ALUctr), .o_ALUresult(ALUresult)
   );
+
+  wire [63:0] mem_out_data;
+  ysyx_22050710_datamem u_datamem (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .i_addr(ALUresult),
+    .i_data(rs2),
+    .i_MemOP(MemOP),
+    .i_WrEn(MemWr),
+    .o_data(mem_out_data)
+  );
+
+  wire [63:0] muxbusW;
+  assign muxbusW = MemtoReg ? mem_out_data : ALUresult;
 
 endmodule
