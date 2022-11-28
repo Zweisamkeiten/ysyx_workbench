046792c42d148de5cab61978a5b66460eadecc44
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-28 12:36:51 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  12:36:51 up  3:27,  7 users,  load average: 1.35, 1.29, 1.32

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -9,17 +9,17 @@ module ysyx_22050710_npc (
 
   initial begin
     set_pc_ptr(pc);
-    set_inst_ptr(inst_temp);
+    set_inst_ptr(inst);
   end
 
-  wire [31:0] inst_temp;
+  wire [31:0] inst;
   wire [63:0] pc;
   wire [31:0] unused;
   ysyx_22050710_ifu u_ifu (
     .i_clk(i_clk),
     .i_rst(i_rst),
     .i_pc(pc),
-    .o_inst(inst_temp),
+    .o_inst(inst),
     .o_unused(unused)
   );
 
@@ -49,7 +49,7 @@ module ysyx_22050710_npc (
   wire MemtoReg, MemWr;
   wire [2:0] MemOP;
   ysyx_22050710_idu u_idu (
-    .i_inst(inst_temp),
+    .i_inst(inst),
     .o_imm(imm),
     .o_ra(ra), .o_rb(rb), .o_rd(rd),
     .o_RegWr(RegWr),
