e8518a18246cdad021435c834c3fefc26363bb10
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 20:36:01 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  20:36:01 up 11:26,  6 users,  load average: 1.24, 1.31, 1.29

--- npc/vsrc/data_mem.v
+++ npc/vsrc/data_mem.v
@@ -34,19 +34,19 @@ module ysyx_22050710_datamem (
     })
   );
 
-  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux23333 (
-    .out(o_data),
-    .key(i_MemOP),
-    .lut({
-      3'b000, {{56{rdata[7]}}, rdata[7:0]},
-      3'b001, {{56{1'b0}}, rdata[7:0]},
-      3'b010, {{48{rdata[15]}}, rdata[15:0]},
-      3'b011, {{48{1'b0}}, rdata[15:0]},
-      3'b100, {{32{rdata[31]}}, rdata[31:0]},
-      3'b101, {{32{1'b0}}, rdata[31:0]},
-      3'b110, rdata
-    })
-  );
+  /* MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux23333 ( */
+  /*   .out(o_data), */
+  /*   .key(i_MemOP), */
+  /*   .lut({ */
+  /*     3'b000, {{56{rdata[7]}}, rdata[7:0]}, */
+  /*     3'b001, {{56{1'b0}}, rdata[7:0]}, */
+  /*     3'b010, {{48{rdata[15]}}, rdata[15:0]}, */
+  /*     3'b011, {{48{1'b0}}, rdata[15:0]}, */
+  /*     3'b100, {{32{rdata[31]}}, rdata[31:0]}, */
+  /*     3'b101, {{32{1'b0}}, rdata[31:0]}, */
+  /*     3'b110, rdata */
+  /*   }) */
+  /* ); */
 
   always @(posedge i_clk) begin
     if (!i_rst) begin
@@ -56,15 +56,15 @@ module ysyx_22050710_datamem (
       else begin
         if (i_MemOP != 3'b111) begin
           npc_pmem_read2(raddr, rdata);
-          /* case (i_MemOP) */
-          /*    3'b000: o_data = {{56{rdata[7]}}, rdata[7:0]}; */
-          /*    3'b001: o_data = {{56{1'b0}}, rdata[7:0]}; */
-          /*    3'b010: o_data = {{48{rdata[15]}}, rdata[15:0]}; */
-          /*    3'b011: o_data = {{48{1'b0}}, rdata[15:0]}; */
-          /*    3'b100: o_data = {{32{rdata[31]}}, rdata[31:0]}; */
-          /*    3'b101: o_data = {{32{1'b0}}, rdata[31:0]}; */
-          /*    default: o_data = rdata; */
-          /* endcase */
+          case (i_MemOP)
+             3'b000: o_data = {{56{rdata[7]}}, rdata[7:0]};
+             3'b001: o_data = {{56{1'b0}}, rdata[7:0]};
+             3'b010: o_data = {{48{rdata[15]}}, rdata[15:0]};
+             3'b011: o_data = {{48{1'b0}}, rdata[15:0]};
+             3'b100: o_data = {{32{rdata[31]}}, rdata[31:0]};
+             3'b101: o_data = {{32{1'b0}}, rdata[31:0]};
+             default: o_data = rdata;
+          endcase
         end
       end
     end
