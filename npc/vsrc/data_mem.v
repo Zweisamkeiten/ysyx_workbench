953a91a6a7253c7ab2a1fbf35ebf699511d01811
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 23:51:13 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  23:51:13 up 14:42,  7 users,  load average: 1.37, 1.28, 1.28

--- npc/vsrc/data_mem.v
+++ npc/vsrc/data_mem.v
@@ -36,8 +36,7 @@ module ysyx_22050710_datamem (
 
   always @(posedge i_clk) begin
     if (!i_rst & i_MemOP != 3'b111) npc_pmem_read(raddr, rdata);
-    else rdata = 64'b0;
-    if (i_WrEn & i_MemOP != 3'b111) npc_pmem_write(waddr, wdata, wmask);
+    if (!i_rst & i_WrEn & i_MemOP != 3'b111) npc_pmem_write(waddr, wdata, wmask);
   end
   
 endmodule
