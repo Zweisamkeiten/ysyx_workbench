a11bd964ea476a88e37e7aec99fd4e4329843993
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 14:53:17 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  14:53:17 up  5:44,  6 users,  load average: 1.26, 1.31, 1.27

--- npc/vsrc/data_mem.v
+++ npc/vsrc/data_mem.v
@@ -15,6 +15,7 @@ module ysyx_22050710_datamem (
   wire [63:0] rdata, wdata;
   wire [63:0] raddr, waddr;
   wire [63:0] data_temp;
+  assign data_temp = rdata;
   assign wdata = i_data;
   assign raddr = i_addr;
   assign waddr = i_addr;
@@ -35,19 +36,19 @@ module ysyx_22050710_datamem (
   );
 
   assign o_data = data_temp;
-  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux2 (
-    .out(data_temp),
-    .key(i_MemOP),
-    .lut({
-      3'b000, {{56{rdata[7]}}, rdata[7:0]},
-      3'b001, {{56{1'b0}}, rdata[7:0]},
-      3'b010, {{48{rdata[15]}}, rdata[15:0]},
-      3'b011, {{48{1'b0}}, rdata[15:0]},
-      3'b100, {{32{rdata[31]}}, rdata[31:0]},
-      3'b101, {{32{1'b0}}, rdata[31:0]},
-      3'b110, rdata[63:0]
-    })
-  );
+  /* MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux2 ( */
+  /*   .out(data_temp), */
+  /*   .key(i_MemOP), */
+  /*   .lut({ */
+  /*     3'b000, {{56{rdata[7]}}, rdata[7:0]}, */
+  /*     3'b001, {{56{1'b0}}, rdata[7:0]}, */
+  /*     3'b010, {{48{rdata[15]}}, rdata[15:0]}, */
+  /*     3'b011, {{48{1'b0}}, rdata[15:0]}, */
+  /*     3'b100, {{32{rdata[31]}}, rdata[31:0]}, */
+  /*     3'b101, {{32{1'b0}}, rdata[31:0]}, */
+  /*     3'b110, rdata[63:0] */
+  /*   }) */
+  /* ); */
 
   always @(posedge i_clk) begin
     if (!i_rst) begin
