daa919d032d280b2d00c9ff53242cdd1cf60baa2
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 15:52:29 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  15:52:29 up  6:43,  6 users,  load average: 1.52, 1.22, 1.21

--- npc/vsrc/data_mem.v
+++ npc/vsrc/data_mem.v
@@ -33,20 +33,6 @@ module ysyx_22050710_datamem (
     })
   );
 
-  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux23333 (
-    .out(o_data),
-    .key(i_MemOP),
-    .lut({
-      3'b000, {{56{rdata[7]}}, rdata[7:0]},
-      3'b001, {{56{1'b0}}, rdata[7:0]},
-      3'b010, {{48{rdata[15]}}, rdata[15:0]},
-      3'b011, {{48{1'b0}}, rdata[15:0]},
-      3'b100, {{32{rdata[31]}}, rdata[31:0]},
-      3'b101, {{32{1'b0}}, rdata[31:0]},
-      3'b110, rdata[63:0]
-    })
-  );
-
   always @(posedge i_clk) begin
     if (!i_rst) begin
       if (i_WrEn) begin
