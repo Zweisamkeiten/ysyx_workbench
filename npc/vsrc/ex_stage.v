2ea75813e84ca7c157cb2fd5e899c70a76df3708
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-30 13:10:34 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  13:10:34 up  4:18,  6 users,  load average: 1.70, 1.36, 1.16

--- npc/vsrc/ex_stage.v
+++ npc/vsrc/ex_stage.v
@@ -119,5 +119,6 @@ module ysyx_22050710_exu (
   always @(i_ALUctr) begin
     if (i_ALUctr == 4'b1111) set_state_abort(); // invalid inst
     if (i_ALUctr == 4'b1110) set_state_end(); // ebreak
+    $display(i_ALUctr);
   end
 endmodule
