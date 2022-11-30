0c3fade1fe15e1fb41554bbdd6959062803b4761
22050710-Einsam <snitxmhm@gmail.com>
2022-11-30 11:11:59 +0800

    feat(npc: id_stage.v ex_stage.v): inst remw

--- npc/vsrc/id_stage.v
+++ npc/vsrc/id_stage.v
@@ -51,6 +51,10 @@ module ysyx_22050710_idu (
   wire inst_addiw  = (opcode[6:0] == 7'b0011011) & (funct3[2:0] == 3'b000);
   wire inst_addw   = (opcode[6:0] == 7'b0111011) & (funct3[2:0] == 3'b000) & (funct7[6:0] == 7'b0000000);
 
+  // RV64M
+  wire inst_remw   = (opcode[6:0] == 7'b0111011) & (funct3[2:0] == 3'b110) & (funct7[6:0] == 7'b0000001);
+
+  wire inst_type_r = |{inst_add, inst_sub, inst_addw, inst_remw};
   wire inst_type_i = |{inst_jalr, inst_lw, inst_addi, inst_sltiu, inst_addiw, inst_ld, inst_ebreak};
   wire inst_type_u = |{inst_lui, inst_auipc};
   wire inst_type_s = |{inst_sw, inst_sd};
@@ -66,7 +70,7 @@ module ysyx_22050710_idu (
   wire inst_store = |{inst_sw, inst_sd};
 
   // 是否需要对操作数进行32位截断
-  assign o_word_cut = |{inst_addiw, inst_addw};
+  assign o_word_cut = |{inst_addiw, inst_addw, inst_remw};
 
   MuxKey #(.NR_KEY(6), .KEY_LEN(6), .DATA_LEN(3)) u_mux0 (
     .out(extop),
@@ -135,18 +139,20 @@ module ysyx_22050710_idu (
   wire alu_plus    = |{inst_auipc, inst_jal, inst_jalr, inst_addi, inst_add, inst_load, inst_store, inst_addiw, inst_addw};
   wire alu_sub     = |{inst_type_b, inst_sub};
   wire alu_sltu    = |{inst_sltiu};
+  wire alu_singed_rem     = |{inst_remw};
   wire alu_ebreak  = inst_ebreak;
 
-  MuxKeyWithDefault #(.NR_KEY(5), .KEY_LEN(5), .DATA_LEN(4)) u_mux3 (
+  MuxKeyWithDefault #(.NR_KEY(6), .KEY_LEN(6), .DATA_LEN(4)) u_mux3 (
     .out(o_ALUctr),
-    .key({alu_copyimm, alu_plus, alu_sub, alu_sltu, alu_ebreak}),
+    .key({alu_copyimm, alu_plus, alu_sub, alu_sltu, alu_singed_rem, alu_ebreak}),
     .default_out(4'b1111),
     .lut({
-      5'b10000, 4'b0011,
-      5'b01000, 4'b0000,
-      5'b00100, 4'b1000,
-      5'b00010, 4'b1010,
-      5'b00001, 4'b1110
+      6'b100000, 4'b0011,
+      6'b010000, 4'b0000,
+      6'b001000, 4'b1000,
+      6'b000100, 4'b1010,
+      6'b000010, 4'b1101,
+      6'b000001, 4'b1110
     })
   );
 
