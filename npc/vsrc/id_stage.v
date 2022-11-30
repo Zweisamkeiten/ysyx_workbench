ff7ef008464047ffa36e5449cef0dfb0f1bafa8e
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-30 11:26:13 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  11:26:13 up  2:33,  6 users,  load average: 1.33, 1.57, 1.48

--- npc/vsrc/id_stage.v
+++ npc/vsrc/id_stage.v
@@ -38,6 +38,7 @@ module ysyx_22050710_idu (
   wire inst_bltu   = (opcode[6:0] == 7'b1100011) & (funct3[2:0] == 3'b100);
   wire inst_bgeu   = (opcode[6:0] == 7'b1100011) & (funct3[2:0] == 3'b100);
   wire inst_lw     = (opcode[6:0] == 7'b0000011) & (funct3[2:0] == 3'b010);
+  wire inst_lbu    = (opcode[6:0] == 7'b0000011) & (funct3[2:0] == 3'b100);
   wire inst_sw     = (opcode[6:0] == 7'b0100011) & (funct3[2:0] == 3'b010);
   wire inst_addi   = (opcode[6:0] == 7'b0010011) & (funct3[2:0] == 3'b000);
   wire inst_sltiu  = (opcode[6:0] == 7'b0010011) & (funct3[2:0] == 3'b011);
@@ -56,7 +57,7 @@ module ysyx_22050710_idu (
   wire inst_remw   = (opcode[6:0] == 7'b0111011) & (funct3[2:0] == 3'b110) & (funct7[6:0] == 7'b0000001);
 
   wire inst_type_r = |{inst_add, inst_sub, inst_addw, inst_remw};
-  wire inst_type_i = |{inst_jalr, inst_lw, inst_addi, inst_sltiu, inst_addiw, inst_ld, inst_slli, inst_ebreak};
+  wire inst_type_i = |{inst_jalr, inst_lw, inst_lbu, inst_addi, inst_sltiu, inst_addiw, inst_ld, inst_slli, inst_ebreak};
   wire inst_type_u = |{inst_lui, inst_auipc};
   wire inst_type_s = |{inst_sw, inst_sd};
   wire inst_type_b = |{inst_beq, inst_bne, inst_bltu, inst_bgeu};
@@ -66,7 +67,7 @@ module ysyx_22050710_idu (
   wire [5:0] inst_type = {inst_type_r, inst_type_i, inst_type_u, inst_type_s, inst_type_b, inst_type_j};
 
   // Load类指令
-  wire inst_load  = |{inst_lw, inst_ld};
+  wire inst_load  = |{inst_lw, inst_lbu, inst_ld};
   // Store类指令
   wire inst_store = |{inst_sw, inst_sd};
 
@@ -116,7 +117,7 @@ module ysyx_22050710_idu (
   wire signed_halfword    = |{1'b0};
   wire signed_word        = |{inst_lw, inst_sw};
   wire signed_doubleword  = |{inst_ld, inst_sd};
-  wire unsigned_byte      = |{1'b0};
+  wire unsigned_byte      = |{inst_lbu};
   wire unsigned_halfword  = |{1'b0};
   wire unsigned_word      = |{1'b0};
  
