3cda9343e05d3dd2f13d083edb26eb74dd2d1b23
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-30 14:08:33 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  14:08:33 up  5:16,  6 users,  load average: 1.39, 1.55, 1.51

--- npc/vsrc/ex_stage.v
+++ npc/vsrc/ex_stage.v
@@ -86,6 +86,9 @@ module ysyx_22050710_exu (
   // signed rem
   wire signed [63:0] signed_rem_result = $signed(src_a) % $signed(src_b);
 
+  // xor
+  wire [63:0] xor_result = src_a ^ src_b;
+
   // and
   wire [63:0] and_result = src_a & src_b;
 
@@ -95,7 +98,7 @@ module ysyx_22050710_exu (
   // sll
   wire [63:0] sll_result = src_a << src_b;
 
-  MuxKey #(.NR_KEY(10), .KEY_LEN(4), .DATA_LEN(64)) u_mux3 (
+  MuxKey #(.NR_KEY(11), .KEY_LEN(4), .DATA_LEN(64)) u_mux3 (
     .out(aluresult),
     .key(i_ALUctr),
     .lut({
@@ -103,6 +106,7 @@ module ysyx_22050710_exu (
       4'b0000, adder_result,
       4'b1010, sub_result[63] == 1 ? 64'b1 : 64'b0, // sltu
       4'b1000, sub_result,
+      4'b0100, xor_result,
       4'b0111, and_result,
       4'b0110, or_result,
       4'b0001, sll_result,
