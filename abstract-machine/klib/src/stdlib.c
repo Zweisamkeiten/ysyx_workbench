3520a085043f8bb98ce7d9ca947b514d57e650ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-03 22:46:45 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  22:46:45 up 13:11,  9 users,  load average: 2.53, 1.90, 1.86

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -35,7 +35,7 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  if (addr == NULL) addr = (void *)ROUNDUP(heap.start , 8);
+  if (addr == NULL) addr = heap.start;
   // 向上对齐8字节
   size = (size_t)ROUNDUP(size, 8);
   addr += size;
