760c017a19b514893bf792a39907433e1d53776a
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-03 20:34:28 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  20:34:28 up 10:58,  7 users,  load average: 1.48, 1.34, 1.34

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
 
 static uint64_t boot_time = 0;
 
@@ -8,6 +7,7 @@ static uint64_t read_time() {
   uint32_t lo = inl(RTC_ADDR + 0);
   uint32_t hi = inl(RTC_ADDR + 4);
   uint64_t time = ((uint64_t)hi << 32) | lo;
+  // nemu框架中timer offset bug修复前, 如下行可以正确得到结果, 而上述lo, hi的获得顺序不行, 必须调换
   // uint64_t time = (((uint64_t)inl(RTC_ADDR + 4)) << 32) | inl(RTC_ADDR + 0);
   return time;
 }
