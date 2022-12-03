88de3b42b80eead2d95a1d6ceadffc09e679923c
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-03 19:49:51 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  19:49:51 up 10:14,  7 users,  load average: 1.36, 1.25, 1.27

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -21,14 +21,14 @@ static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  printf("before: rtc0 %u\n", rtc_port_base[0]);
-  printf("before: rtc1 %u\n", rtc_port_base[1]);
+  // printf("before: rtc0 %u\n", rtc_port_base[0]);
+  // printf("before: rtc1 %u\n", rtc_port_base[1]);
   if (!is_write && offset == 4) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
-    printf("after: rtc0 %u\n", rtc_port_base[0]);
-    printf("after: rtc1 %u\n", rtc_port_base[1]);
+    // printf("after: rtc0 %u\n", rtc_port_base[0]);
+    // printf("after: rtc1 %u\n", rtc_port_base[1]);
   }
 }
 
