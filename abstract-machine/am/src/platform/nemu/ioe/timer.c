37f8cdac2dd504272570a2c028fe3c5c44018dfc
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-03 17:07:55 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  17:07:55 up  7:32,  6 users,  load average: 1.45, 1.38, 1.36

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,21 +2,21 @@
 #include <nemu.h>
 #include <klib.h>
 
-static uint64_t boot_time = 0;
+// static uint64_t boot_time = 0;
 
-static uint64_t read_time() {
-  uint32_t lo = inl(RTC_ADDR + 0);
-  uint32_t hi = inl(RTC_ADDR + 4);
-  uint64_t time = ((uint64_t)hi << 32) | lo;
-  return time;
-}
+// static uint64_t read_time() {
+//   uint32_t lo = inl(RTC_ADDR + 0);
+//   uint32_t hi = inl(RTC_ADDR + 4);
+//   uint64_t time = ((uint64_t)hi << 32) | lo;
+//   return time;
+// }
 
 void __am_timer_init() {
-  boot_time = read_time();
+  // boot_time = read_time();
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = read_time() - boot_time;
+  uptime->us = (((uint64_t)inl(RTC_ADDR + 4)) << 32) | inl(RTC_ADDR);
   // printf("boot_time: %lu\n", boot_time);
 }
 
