e057c4adecd0890d21b0da83337945c4b1b91f07
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 14:50:23 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  14:50:23 up  5:29,  7 users,  load average: 1.85, 1.41, 1.26

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -33,7 +33,6 @@ static uint32_t screen_size() {
 
 static void *vmem = NULL;
 static uint32_t *vgactl_port_base = NULL;
-static uint32_t *vgasync_port_base = NULL;
 
 #ifdef CONFIG_VGA_SHOW_SCREEN
 #ifndef CONFIG_TARGET_AM
@@ -74,22 +73,20 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  if (vgasync_port_base[0] != 0) {
+  if (vgactl_port_base[1] != 0) {
     update_screen();
-    vgasync_port_base[0] = 0;
+    vgactl_port_base[1] = 0;
   }
 }
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
-  vgasync_port_base = (uint32_t *)new_space(4);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
-  vgasync_port_base[0] = 0;
+  vgactl_port_base[1] = 0; // sync reg
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
   add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
-  add_mmio_map("vgasync", CONFIG_VGA_CTL_MMIO + 4, vgasync_port_base, 4, NULL);
 #endif
 
   vmem = new_space(screen_size());
