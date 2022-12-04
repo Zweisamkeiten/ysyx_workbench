af93f253a193616f43160db4ea8e8a348f3be296
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 12:32:50 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  12:32:50 up  3:12,  7 users,  load average: 1.30, 1.34, 1.16

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t scancode = inw(KBD_ADDR);
-  // printf("scancode: %d\n", scancode);
   kbd->keydown = (scancode & KEYDOWN_MASK ? true : false);
-  kbd->keycode = scancode ^ KEYDOWN_MASK;
+  kbd->keycode = scancode & ~KEYDOWN_MASK;
 }
