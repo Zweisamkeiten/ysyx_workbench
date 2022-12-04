e8fab97895911f25909994c28ccbf9e7bef9f5dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 16:19:30 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  16:19:30 up  6:59,  7 users,  load average: 1.20, 1.49, 1.56

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -24,13 +23,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int w = io_read(AM_GPU_CONFIG).width;
-  if (ctl->w == 0 || ctl->h == 0) return;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;
   for (int row = ctl->y; row < ctl->y + ctl->h; row++) {
     for (int column = ctl->x; column < ctl->x + ctl->w; column++) {
-      fb[row * w + column] = *(uint32_t *)(ctl->pixels);
-      printf("pixels: %x\n", fb[row*w + column]);
-      ctl->pixels++;
+      fb[row * w + column] = *pixels++;
     }
   }
   if (ctl->sync) {
