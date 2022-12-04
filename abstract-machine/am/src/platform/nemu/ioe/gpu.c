fe21ddb808a4338cce4b81ff89416a58fb66211e
22050710-Einsam <snitxmhm@gmail.com>
2022-12-04 15:36:03 +0800

    feat(am, nemu): am: AM_GPU_CONFIG 使用屏幕大小寄存器 nemu: 添加同步寄存器支持

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,31 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  // int i;
+  // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t size = inl(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = size >> 16, .height = size & 0xFFFF,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int w = io_read(AM_GPU_CONFIG).width;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (int row = ctl->y; row < ctl->y + ctl->h; row++) {
+    for (int column = ctl->x; column < ctl->x + ctl->w; column++) {
+      fb[row * w + column] = *(uint32_t *)((ctl->pixels)++);
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
