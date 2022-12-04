c68e6b60b52cfe3f94010e505592fbfd2308c8b3
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 15:28:14 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  15:28:14 up  6:07,  7 users,  load average: 1.51, 1.36, 1.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
   int i;
-  int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
-  int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
