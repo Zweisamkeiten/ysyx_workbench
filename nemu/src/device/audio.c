115a18aecef8a81e1d356b29cea734b0975676c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-05 17:38:57 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  17:38:57 up  8:21,  7 users,  load average: 1.83, 1.74, 1.64

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -60,6 +60,7 @@ static void init_sound() {
   s.format = AUDIO_S16SYS; // 假设系统中音频数据的格式总是使用16位有符号数来表示
   s.callback = audio_play;
   s.userdata = NULL; // 不使用
+  printf("%d", s.freq);
 
   count = 0;
   int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
