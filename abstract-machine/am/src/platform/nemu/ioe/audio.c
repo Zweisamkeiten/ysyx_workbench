024e1c075e4192758b3a97a9570db1c46b140b1a
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-05 20:22:46 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  20:22:46 up 11:05,  8 users,  load average: 1.55, 1.48, 1.46

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -37,13 +37,19 @@ static void audio_write(uint8_t *buf, int len) {
     int free = sbufsize - count;
     int size = (free > len) ? len : free - len;
     if (free > len) {
-      memcpy((void *)(uint64_t)(AUDIO_SBUF_ADDR + write_point), buf, size);
-      write_point += size;
+      int free_to_end = sbufsize - write_point; // 写入点距离缓冲区末尾的空闲空间
+      if (free_to_end >= len) {
+        memcpy((void *)(uint64_t)(AUDIO_SBUF_ADDR + write_point), buf, size);
+        write_point += size;
+      } else {
+        memcpy((void *)(uint64_t)(AUDIO_SBUF_ADDR + write_point), buf, free_to_end);
+        write_point = 0;
+        memcpy((void *)(uint64_t)(AUDIO_SBUF_ADDR + write_point), buf + free_to_end, len - free_to_end);
+        write_point = len - free_to_end;
+      }
       count += size;
       outl(AUDIO_COUNT_ADDR, count);
       nwrite += size;
-    } else {
-      write_point = 0;
     }
   }
 }
