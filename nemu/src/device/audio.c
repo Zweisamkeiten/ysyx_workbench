2817a944c2771900b5a08a3fce7f2c892a6b224d
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-05 18:18:34 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:18:34 up  9:01,  7 users,  load average: 2.01, 1.86, 1.74

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -44,7 +44,7 @@ static void audio_play(void *userdata, uint8_t *stream, int len) {
   while (b < nread) {
     // int size = (count - b < nread) ? count - b : nread;
     // if (size > 0) {
-      memcpy(stream, sbuf, nread);
+      memcpy(stream, sbuf, nread + 1);
       b += nread;
     // }
   }
