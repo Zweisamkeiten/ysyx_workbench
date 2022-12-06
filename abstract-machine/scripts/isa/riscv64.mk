c94fa9b92d16b8d7fd05b6ec37ee276283d7ee19
22050710-Einsam <snitxmhm@gmail.com>
2022-12-06 17:06:16 +0800

    chore(am & nemu): build option -O2 to -O3 to run faster

--- abstract-machine/scripts/isa/riscv64.mk
+++ abstract-machine/scripts/isa/riscv64.mk
@@ -2,4 +2,4 @@ CROSS_COMPILE := riscv64-linux-gnu-
 COMMON_FLAGS  := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
-LDFLAGS       += -melf64lriscv
+LDFLAGS       += -flto -melf64lriscv
