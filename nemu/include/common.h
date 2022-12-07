8f162909379a990d4b9a9aec8e0136b7ed608142
22050710-Einsam <snitxmhm@gmail.com>
2022-12-07 22:29:03 +0800

    fix(nemu): header about elf ftrace

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -47,13 +47,3 @@ typedef uint16_t ioaddr_t;
 #include <debug.h>
 
 #endif
-
-#ifdef CONFIG_FTRACE
-#include <elf.h>
-typedef MUXDEF(CONFIG_ISA64, Elf64_Ehdr, Elf32_Ehdr) Elf_Ehdr;
-typedef MUXDEF(CONFIG_ISA64, Elf64_Shdr, Elf64_Shdr) Elf_Shdr;
-typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) Elf_Addr;
-typedef MUXDEF(CONFIG_ISA64, Elf64_Sym, Elf32_Sym) Elf_Sym;
-#define ELF_ST_TYPE(val) MUXDEF(CONFIG_ISA64, ELF64_ST_TYPE(val), ELF32_ST_TYPE)
-
-#endif
