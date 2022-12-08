84de33ec2dfa04b5de1f3da11644464e4cf6dbb7
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-08 16:14:29 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  16:14:29 up  1:15,  8 users,  load average: 2.18, 1.85, 1.78

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,8 +22,8 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
-LDFLAGS := $(LDFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -34,7 +34,6 @@ static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
 #ifdef CONFIG_FTRACE
-#include <trace.h>
 extern uint8_t * elf_mem_p;
 extern Elf_Ehdr *ehdr;
 
