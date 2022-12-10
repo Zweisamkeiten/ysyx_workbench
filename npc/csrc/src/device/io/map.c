db552f64a6594a8b244151837632ec295f810938
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-10 18:55:44 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:55:44 up  9:25,  9 users,  load average: 2.61, 2.82, 1.85

--- npc/csrc/src/device/io/map.c
+++ npc/csrc/src/device/io/map.c
@@ -60,7 +60,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   // if (check_bound(map, addr) == false) {
   //   return 0;
   // }
-  check_bound(map, addr);
+  // check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
