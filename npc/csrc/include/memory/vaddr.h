16c69aa196b6f25a667d695528c5d3ea56c914c0
22050710-Einsam <snitxmhm@gmail.com>
2022-12-10 19:15:28 +0800

    feat(npc): init_device() adapted from nemu

--- npc/csrc/include/memory/vaddr.h
+++ npc/csrc/include/memory/vaddr.h
@@ -13,38 +13,17 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#ifndef __COMMON_H__
-#define __COMMON_H__
+#ifndef __MEMORY_VADDR_H__
+#define __MEMORY_VADDR_H__
 
-#include <stdint.h>
-#include <inttypes.h>
-#include <stdbool.h>
-#include <string.h>
-#include <macro.h>
-#include <conf.h>
-#include <generated/autoconf.h>
+#include <common.h>
 
-#include <assert.h>
-#include <stdlib.h>
-typedef uint64_t word_t;
-typedef int64_t  sword_t;
-#define FMT_WORD "0x%016lx"
+word_t vaddr_ifetch(vaddr_t addr, int len);
+word_t vaddr_read(vaddr_t addr, int len);
+void vaddr_write(vaddr_t addr, int len, word_t data);
 
-typedef word_t vaddr_t;
-typedef uint64_t paddr_t;
-#define FMT_PADDR "0x%016lx"
-typedef uint16_t ioaddr_t;
-
-#include <debug.h>
-
-#ifdef CONFIG_FTRACE
-#include <elf.h>
-typedef Elf64_Ehdr Elf_Ehdr;
-typedef Elf64_Shdr Elf_Shdr;
-typedef Elf64_Addr Elf_Addr;
-typedef Elf64_Sym Elf_Sym;
-#define ELF_ST_TYPE(val) ELF64_ST_TYPE(val)
-
-#endif
+#define PAGE_SHIFT        12
+#define PAGE_SIZE         (1ul << PAGE_SHIFT)
+#define PAGE_MASK         (PAGE_SIZE - 1)
 
 #endif
