9474611fc0d74a3c6b80e526e3ca5a21e7e426f0
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-10 19:07:58 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  19:07:58 up  9:38,  9 users,  load average: 1.23, 1.46, 1.61

--- npc/csrc/src/device/serial.c
+++ npc/csrc/src/device/serial.c
@@ -30,13 +30,15 @@ static void serial_putc(char ch) {
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   // assert(len == 1);
-  switch (offset) {
-    /* We bind the serial port with the host stderr in NPC. */
-    case CH_OFFSET:
-      if (is_write) serial_putc(serial_base[0]);
-      else panic("do not support read");
-      break;
-    default: panic("do not support offset = %d", offset);
+  if (len == 1) {
+    switch (offset) {
+      /* We bind the serial port with the host stderr in NPC. */
+      case CH_OFFSET:
+        if (is_write) serial_putc(serial_base[0]);
+        // else panic("do not support read");
+        break;
+      default: panic("do not support offset = %d", offset);
+    }
   }
 }
 
