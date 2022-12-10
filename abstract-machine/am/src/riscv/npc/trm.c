1919dd10e23eb3eeec9695531debdf974d55ce81
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-10 18:46:23 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:46:23 up  9:16,  9 users,  load average: 1.37, 1.07, 0.89

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,8 +1,16 @@
+#include ISA_H
 #include <am.h>
-#include <npc.h>
+#include <klib-macros.h>
+
+# define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+#define SERIAL_PORT     (0xa0000000 + 0x00003f8)
 
 extern char _heap_start;
 int main(const char *args);
+
+extern char _pmem_start;
+#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
