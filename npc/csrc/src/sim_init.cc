e3a6422da2dad05c6b417795e482215c8abdc555
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-10 11:48:31 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  11:48:31 up  2:18,  8 users,  load average: 1.42, 1.43, 1.47

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -1,16 +1,13 @@
 #include <sim.hpp>
 #include <common.h>
 #include <isa.h>
-#include <memory/host.h>
 extern "C" {
   #include <memory/paddr.h>
 }
 
 Vtop *top;
-#ifdef CONFIG_VCD_TRACE
 VerilatedContext *contextp = NULL;
 VerilatedVcdC *tfp = NULL;
-#endif
 uint64_t * npcpc;
 
 void set_state_end() {
@@ -25,38 +22,7 @@ extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu.gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
-static uint64_t boot_time = 0;
-
-static uint64_t get_time_internal() {
-  struct timespec now;
-  clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
-  return us;
-}
-
-uint64_t get_time() {
-  if (boot_time == 0) boot_time = get_time_internal();
-  uint64_t now = get_time_internal();
-  return now - boot_time;
-}
-
-static uint32_t rtc_port_base[2];
-static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 0) {
-    uint64_t us = get_time();
-    rtc_port_base[0] = (uint32_t)us;
-    rtc_port_base[1] = us >> 32;
-  }
-}
-
 extern "C" void npc_pmem_read(long long raddr, long long *rdata) {
-  switch (raddr) {
-  case 0xa0000048: rtc_io_handler(0, 1, false); *rdata = rtc_port_base[0]; break;
-  case 0xa000004c: rtc_io_handler(4, 1, false); *rdata = rtc_port_base[1]; break;
-  default: *rdata = paddr_read(raddr, 8); break;
-  }
-  return;
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   *rdata = paddr_read(raddr, 8);
 }
@@ -83,16 +49,12 @@ extern "C" void npc_pmem_write(long long waddr, long long wdata, char wmask) {
 extern "C" void single_cycle() {
   top->i_clk = 0;
   top->eval();
-#ifdef CONFIG_VCD_TRACE
   contextp->timeInc(1);
   tfp->dump(contextp->time());
-#endif
   top->i_clk = 1;
   top->eval();
-#ifdef CONFIG_VCD_TRACE
   contextp->timeInc(1);
   tfp->dump(contextp->time());
-#endif
 }
 
 static void reset(int n) {
@@ -104,17 +66,13 @@ static void reset(int n) {
 }
 
 extern "C" void init_sim() {
-#ifdef CONFIG_VCD_TRACE
   contextp = new VerilatedContext;
   tfp = new VerilatedVcdC;
-#endif
   top = new Vtop;
 
-#ifdef CONFIG_VCD_TRACE
   contextp->traceEverOn(true);
   top->trace(tfp, 0);
   tfp->open("dump.vcd");
-#endif
 
   reset(10);
 
@@ -128,7 +86,5 @@ extern "C" void init_sim() {
 extern "C" void end_sim() {
   top->final();
   delete top;
-#ifdef CONFIG_VCD_TRACE
   tfp->close();
-#endif
 }
--- npc/vsrc/ex_stage.v
+++ npc/vsrc/ex_stage.v
@@ -123,8 +123,8 @@ module ysyx_22050710_exu (
 
   // sra
   wire signed [63:0] sra_result = i_word_cut
-                                  ? {{32{src_a[31]}}, $signed(src_a[31:0]) >>> src_b[5:0]}
-                                  : $signed(src_a[63:0]) >>> src_b[5:0];
+                                  ? {{32{src_a[31]}}, $signed(src_a[31:0]) >>> $signed(src_b[5:0])}
+                                  : $signed(src_a) >>> $signed(src_b[5:0]);
 
   MuxKey #(.NR_KEY(16), .KEY_LEN(5), .DATA_LEN(64)) u_mux4 (
     .out(aluresult),
