299c539517bb2c7de474e0648b148b8335eb7b97
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-10 09:55:47 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  09:55:47 up 25 min,  2 users,  load average: 1.48, 1.61, 1.41

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -32,7 +32,7 @@ static uint64_t get_time_internal() {
   uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
 #else
   struct timespec now;
-  clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
+  clock_gettime(CLOCK_MONOTONIC, &now);
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
 #endif
   return us;
