c2241b09028555d594c439a84608a85092fbaa1b
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-11 18:14:05 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  18:14:05 up  9:36,  8 users,  load average: 1.28, 1.38, 1.44

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -21,7 +21,7 @@
 typedef struct {
   word_t gpr[32];
   vaddr_t pc;
-  word_t csr[4];
+  word_t csr[5];
 } riscv64_CPU_state;
 
 // decode
--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -30,7 +30,7 @@ const char *regs[] = {
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
   // csrs
-  "mstatus", "mtvec", "mepc", "mcause"
+  "mstatus", "mtvec", "mepc", "mcause", "test"
 };
 
 #define NR_REGS ARRLEN(regs)
