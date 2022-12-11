2fc7cbd434e7d7bfac33efdd8cbf8f6cf0bdaac4
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-11 16:16:35 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  16:16:35 up  7:38,  8 users,  load average: 1.25, 1.30, 1.39

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -16,6 +16,18 @@
 #include <isa.h>
 #include "local-include/reg.h"
 
+const char *regs[] = {
+  // gprs
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
+  // csrs
+  "mepc", "mstatus", "mcause"
+};
+
+#define NR_REGS ARRLEN(regs)
+
 void isa_reg_display() {
   printf(ANSI_FMT("GPRS:\n", ANSI_FG_BLUE));
   for (int i = 0; i < 16; ++i) {
