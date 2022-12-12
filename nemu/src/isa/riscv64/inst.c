1f94be24a1192d38cce5cc1ac08247e1f221fc7b
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-12 11:48:41 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.0.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 16 Nov 2022 17:00:58 +0000 x86_64 GNU/Linux  11:48:41 up  2:09,  9 users,  load average: 1.73, 1.50, 1.51

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -133,7 +133,10 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 111 ????? 01110 11", remuw  , R, R(dest) = SEXT((uint32_t)(BITS(src1, 31, 0)) % (uint32_t)(BITS(src2, 31, 0)), 32));
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = NEMUINTR(11, s->pc)); // Environment call from M-mode Expection Code: 11
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+
+  // RV32/RV64 Zicsr
   INSTPAT("??????? ????? ????? 001 00000 11100 11", csrrw  , I, int csr_idx = get_csr_idx(imm); R(dest) = cpu.csr[csr_idx]; cpu.csr[csr_idx] = src1);
+  INSTPAT("??????? ????? ????? 010 00000 11100 11", csrrs  , I, int csr_idx = get_csr_idx(imm); R(dest) = cpu.csr[csr_idx]; cpu.csr[csr_idx] = src1 | cpu.csr[csr_idx]);
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
