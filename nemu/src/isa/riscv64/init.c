73fe3633cd902322dd9149f5b40746d447a49380
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-24 22:22:13 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  22:22:13 up 27 min, 10 users,  load average: 1.08, 1.38, 1.18

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -19,19 +19,19 @@
 
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
-static const uint32_t img [] = {
-  0x00000073,
-  0x02500e93,
-  0x00009117,  // auipc sp, 0x9
-  0x00100513,  // li      a0,1
-  0x00200593,  // li      a1,2
-  0x00200613,  // li      a2,2
-  0x00200693,  // li      a3,2
-  0x00c58933,  // add     s2,a1,a2
-  0x00a13423,  // sd      a0,8(sp)
-  0x00100073,  // ebreak (used as nemu_trap)
-  0xdeadbeef,  // some data
-};
+// static const uint32_t img [] = {
+//   0x00000073,
+//   0x02500e93,
+//   0x00009117,  // auipc sp, 0x9
+//   0x00100513,  // li      a0,1
+//   0x00200593,  // li      a1,2
+//   0x00200613,  // li      a2,2
+//   0x00200693,  // li      a3,2
+//   0x00c58933,  // add     s2,a1,a2
+//   0x00a13423,  // sd      a0,8(sp)
+//   0x00100073,  // ebreak (used as nemu_trap)
+//   0xdeadbeef,  // some data
+// };
 
 static void restart() {
   /* Set the initial program counter. */
