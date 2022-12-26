f923baaee86ea0e70cc4f572ccd1dd781da8a07f
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-26 21:01:01 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  21:01:01 up 11:50, 12 users,  load average: 1.24, 1.16, 1.06

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,11 +9,30 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  extern uint8_t ramdisk_start;
+  Elf_Ehdr *elf = (Elf_Ehdr *)&ramdisk_start;
+
+  // check the magic number.
+  assert(*(uint32_t *)elf->e_ident == 0x7f454c46);
+
+  Elf_Phdr *phdr = (Elf_Phdr *)(&ramdisk_start + elf->e_phoff);
+  printf("\nProgram header list\n\n");
+  for (int i = 0; i < elf->e_phnum; i++) {
+       switch(phdr[i].p_type) {
+         case PT_LOAD:
+           /* * We know that text segment starts * at offset 0.
+            * And only one other *
+            * possible loadable segment exists *
+            * which is the data segment. */
+           if (phdr[i].p_offset == 0) printf("Text segment: 0x%x\n", phdr[i].p_vaddr);
+    }
+  }
+
+  size_t ramdisk_read(void *buf, size_t offset, size_t len);
   ramdisk_read((void *)(uintptr_t)(0x83000000), 0, 0x4f20+0x1038);
-  return 0x83000120;
+  return elf->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
