74f87feb2345bec354ab9a0a5cd435b94836e17d
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-27 15:28:51 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  15:28:51 up  5:27, 11 users,  load average: 1.37, 1.27, 1.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,9 +3,11 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
 
   switch (a[0]) {
     case SYS_yield: sys_yield(); break;
+    case SYS_exit: sys_exit(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
@@ -15,7 +17,7 @@ int sys_yield(void) {
   return 0;
 }
 
-// int sys_exit(int state) {
-//   halt(state);
-//   return 0;
-// }
+int sys_exit(int state) {
+  halt(state);
+  return 0;
+}
