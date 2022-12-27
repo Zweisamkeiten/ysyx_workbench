0851bda56d38c5179e6b29a86b771c1a89283f49
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-27 21:47:21 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  21:47:21 up 11:46, 11 users,  load average: 1.23, 1.25, 1.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -26,30 +26,165 @@ static char *syscall_name_table[] = {
 };
 #endif
 
+// syscall functions
+extern uint64_t sys_exit(void);
+extern uint64_t sys_yield(void);
+extern uint64_t sys_open(void);
+extern uint64_t sys_read(void);
+extern uint64_t sys_write(void);
+extern uint64_t sys_kill(void);
+extern uint64_t sys_getpid(void);
+extern uint64_t sys_close(void);
+extern uint64_t sys_lseek(void);
+extern uint64_t sys_brk(void);
+extern uint64_t sys_fstat(void);
+extern uint64_t sys_time(void);
+extern uint64_t sys_signal(void);
+extern uint64_t sys_execve(void);
+extern uint64_t sys_fork(void);
+extern uint64_t sys_link(void);
+extern uint64_t sys_unlink(void);
+extern uint64_t sys_wait(void);
+extern uint64_t sys_times(void);
+extern uint64_t sys_gettimeofday(void);
+
+// An array mapping syscall numbers from syscall.h
+// to the function that handles the system call.
+static uint64_t (*syscalls[])(void) = {
+  [SYS_exit] = sys_exit,
+  [SYS_yield] = sys_yield,
+  [SYS_open] = sys_open,
+  [SYS_read] = sys_read,
+  [SYS_write] = sys_write,
+  [SYS_kill] = sys_kill,
+  [SYS_getpid] = sys_getpid,
+  [SYS_close] = sys_close,
+  [SYS_lseek] = sys_lseek,
+  [SYS_brk] = sys_brk,
+  [SYS_fstat] = sys_fstat,
+  [SYS_time] = sys_time,
+  [SYS_signal] = sys_signal,
+  [SYS_execve] = sys_execve,
+  [SYS_fork] = sys_fork,
+  [SYS_link] = sys_link,
+  [SYS_unlink] = sys_unlink,
+  [SYS_wait] = sys_wait,
+  [SYS_times] = sys_times,
+  [SYS_gettimeofday] = sys_gettimeofday
+};
+
+static uintptr_t a[4];
+
 void do_syscall(Context *c) {
-  uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
-  long int ret = 0;
+  int num = a[0]; // syscall num
 
-  switch (a[0]) {
-    case SYS_yield: ret = sys_yield(); break;
-    case SYS_exit: ret = sys_exit(a[1]); break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  if (num > 0 && num < NR_SYSCALLS && syscalls[num]) {
+    c->GPRx = syscalls[num](); // return value
+  } else {
+    panic("Unhandled syscall ID = %d", num);
   }
 
   #ifdef CONFIG_STRACE
-  printf("\033[93m" "STACE: %s() = %ld" "\33[0m\n", syscall_name_table[a[0]], ret);
+  printf("\033[93m" "STACE: %s() = %ld" "\33[0m\n", syscall_name_table[num], c->GPRx);
   #endif
 }
 
-long sys_yield(void) {
+uint64_t sys_yield(void) {
   yield();
   return 0;
 }
 
-long sys_exit(int state) {
-  halt(state);
+uint64_t sys_exit(void) {
+  halt(a[1]);
   return 0;
 }
+
+uint64_t sys_open(void) {
+  TODO();
+}
+
+uint64_t sys_read(void) {
+  TODO();
+}
+
+uint64_t sys_write(void) {
+  int fd = a[1];
+  const void *buf = (void *)a[2];
+  size_t count = a[3];
+  uint64_t written = 0;
+  if (fd == 1 || fd == 2) {
+    while (written <= count) {
+      putch(*((char *)(buf) + written));
+      written++;
+    }
+    return written;
+  } else {
+    return -1;
+  }
+}
+
+uint64_t sys_kill(void) {
+  TODO();
+}
+
+uint64_t sys_getpid(void) {
+  TODO();
+}
+
+uint64_t sys_close(void) {
+  TODO();
+}
+
+uint64_t sys_lseek(void) {
+  TODO();
+}
+
+uint64_t sys_brk(void) {
+  TODO();
+}
+
+uint64_t sys_fstat(void) {
+  TODO();
+}
+
+uint64_t sys_time(void) {
+  TODO();
+}
+
+uint64_t sys_signal(void) {
+  TODO();
+}
+
+uint64_t sys_execve(void) {
+  TODO();
+}
+
+uint64_t sys_fork(void) {
+  TODO();
+}
+
+uint64_t sys_link(void) {
+  TODO();
+}
+
+uint64_t sys_unlink(void) {
+  TODO();
+}
+
+uint64_t sys_wait(void) {
+  TODO();
+}
+
+uint64_t sys_times(void) {
+  TODO();
+}
+
+uint64_t sys_gettimeofday(void) {
+  TODO();
+}
+
