47474fce8eed83c95b526db79817974b6d92fece
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-30 15:22:41 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  15:22:41 up  2:43, 11 users,  load average: 1.31, 1.22, 1.19

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -73,7 +73,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int draw_x = x + (screen_w - canvas_w) / 2;
   int draw_y = y + (screen_h - canvas_h) / 2;
   for (int row = 0; row < h; row++) {
-    lseek(fbdev, ((draw_y + row) * screen_w) * 4, SEEK_SET);
+    lseek(fbdev, ((draw_y + row) * screen_w + draw_x) * 4, SEEK_SET);
     write(fbdev, pixels + row * w, w * 4);
   }
 }
