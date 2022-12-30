2843199764545815282021b2b8773c5e2415dc72
22050710-Einsam <snitxmhm@gmail.com>
2022-12-30 15:21:30 +0800

    feat(nanos-lite&navy): os: /dev/fb fb_write() & user: libndl NDL_DrawRect() & pass bmp-test

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -27,12 +27,14 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
 extern size_t events_read(void *buf, size_t offset, size_t len);
 extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
+extern size_t fb_write(const void *buf, size_t offset, size_t len);
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, invalid_read, fb_write},
 
   {"/dev/events", 0, 0, events_read, invalid_write},
   {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
@@ -44,7 +46,10 @@ static Finfo file_table[] __attribute__((used)) = {
 #define NR_FILES ARRLEN(file_table)
 
 void init_fs() {
-  // TODO: initialize the size of /dev/fb
+  // initialize the size of /dev/fb
+  int w = io_read(AM_GPU_CONFIG).width;
+  int h = io_read(AM_GPU_CONFIG).height;
+  file_table[FD_FB].size = w * h * 4; // 00RRGGBB 4byte
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
@@ -74,7 +79,9 @@ size_t fs_read(int fd, void *buf, size_t len) {
     return read_n;
   } else {
     // vfs api
-    return file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    size_t nread = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset += nread;
+    return nread;
   }
 
 }
@@ -94,7 +101,9 @@ size_t fs_write(int fd, const void *buf, size_t len) {
     return written_n;
   } else {
     // vfs api
-    return file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    size_t nwrite = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset += nwrite;
+    return nwrite;
   }
 }
 
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -65,6 +65,13 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  assert(canvas_w != 0 && canvas_h != 0);
+
+  assert(fbdev != -1);
+  for (int row = 0; row < h; row++) {
+    lseek(fbdev, ((y + row) * screen_w + x) * 4, SEEK_SET);
+    write(fbdev, pixels + row * w, w * 4);
+  }
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -97,6 +104,8 @@ int NDL_Init(uint32_t flags) {
     sscanf(buf, "WIDTH : %d\nHEIGHT : %d\n", &screen_w, &screen_h);
     printf("\033[32mNDL_INIT: WIDTH: %d, HEIGHT: %d\n\33[0m", screen_w, screen_h);
   }
+
+  fbdev = open("/dev/fb", 0, 0);
 
   return 0;
 }
