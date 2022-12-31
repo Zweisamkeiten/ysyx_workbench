ca055721501e9d8b6bd6e32a4f86cb2f50cd9947
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-31 20:11:29 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  20:11:29 up 11:00, 13 users,  load average: 1.94, 2.35, 2.50

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,7 +18,30 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64];
+  event->key.keysym.sym = SDLK_NONE;
+  if (NDL_PollEvent(buf, sizeof(buf))) {
+    if (strncmp(buf, "ku", 2) == 0) {
+      event->type = SDL_KEYUP;
+      event->key.type = SDL_KEYUP;
+    } else if (strncmp(buf, "kd", 2) == 0) {
+      printf("%s", buf);
+      event->type = SDL_KEYDOWN;
+      event->key.type = SDL_KEYDOWN;
+    }
+
+    char *blank = strchr(buf, ' ');
+    char *end = strchr(blank + 1, '\n');
+    for (int i = 0; i < SDLK_NR; i++) {
+      if (strncmp(blank + 1, keyname[i], end - blank - 1) == 0) {
+        event->key.keysym.sym = i;
+      }
+    }
+
+    // return 1 on success
+    return 1;
+  }
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
