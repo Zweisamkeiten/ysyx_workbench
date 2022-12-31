ab02526e7ff2c7de27991d94ae4ad063c14a4c63
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-31 22:03:49 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  22:03:49 up 12:52, 13 users,  load average: 1.33, 1.45, 1.55

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,26 +14,25 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
-  event->key.keysym.sym = SDLK_NONE;
+  ev->key.keysym.sym = SDLK_NONE;
+
   if (NDL_PollEvent(buf, sizeof(buf))) {
     if (strncmp(buf, "ku", 2) == 0) {
-      event->type = SDL_KEYUP;
-      event->key.type = SDL_KEYUP;
+      ev->type = SDL_KEYUP;
+      ev->key.type = SDL_KEYUP;
     } else if (strncmp(buf, "kd", 2) == 0) {
-      event->type = SDL_KEYDOWN;
-      event->key.type = SDL_KEYDOWN;
+      ev->type = SDL_KEYDOWN;
+      ev->key.type = SDL_KEYDOWN;
     }
 
     char *blank = strchr(buf, ' ');
     char *end = strchr(blank + 1, '\n');
+    *end = '\0';
     for (int i = 0; i < SDLK_NR; i++) {
-      if (strncmp(blank + 1, keyname[i], end - blank - 1) == 0) {
-        event->key.keysym.sym = i;
+      if (strcmp(blank + 1, keyname[i]) == 0) {
+        printf("%d\n", i);
+        ev->key.keysym.sym = i;
       }
     }
 
@@ -41,6 +40,33 @@ int SDL_WaitEvent(SDL_Event *event) {
     return 1;
   }
   return 0;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  char buf[64];
+  event->key.keysym.sym = SDLK_NONE;
+
+  while (NDL_PollEvent(buf, sizeof(buf)) == 0);
+
+  if (strncmp(buf, "ku", 2) == 0) {
+    event->type = SDL_KEYUP;
+    event->key.type = SDL_KEYUP;
+  } else if (strncmp(buf, "kd", 2) == 0) {
+    event->type = SDL_KEYDOWN;
+    event->key.type = SDL_KEYDOWN;
+  }
+
+  char *blank = strchr(buf, ' ');
+  char *end = strchr(blank + 1, '\n');
+  *end = '\0';
+  for (int i = 0; i < SDLK_NR; i++) {
+    if (strcmp(blank + 1, keyname[i]) == 0) {
+      event->key.keysym.sym = i;
+    }
+  }
+
+  // return 1 on success
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
