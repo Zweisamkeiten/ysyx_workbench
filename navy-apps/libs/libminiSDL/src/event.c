ffb391c57d4a438eb753d23ff75b12198c80ba8b
22050710-Einsam <snitxmhm@gmail.com>
2022-12-31 20:08:44 +0800

    feat(navy: libminiSDL: event.c): SDL_WaitEvent() & pass App NSlider

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,6 +18,28 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  char buf[64];
+  event->key.keysym.sym = SDLK_NONE;
+
+  while (NDL_PollEvent(buf, sizeof(buf)) == 0);
+
+  if (strncmp(buf, "ku", 2) == 0) {
+    event->type = SDL_KEYUP;
+    event->key.type = SDL_KEYUP;
+  } else if (strncmp(buf, "kd", 2) == 0) {
+    event->type = SDL_KEYDOWN;
+    event->key.type = SDL_KEYDOWN;
+  }
+
+  char *blank = strchr(buf, ' ');
+  char *end = strchr(blank + 1, '\n');
+  for (int i = 0; i < SDLK_NR; i++) {
+    if (strncmp(blank + 1, keyname[i], end - blank - 1) == 0) {
+      event->key.keysym.sym = i;
+    }
+  }
+
+  // return 1 on success
   return 1;
 }
 
