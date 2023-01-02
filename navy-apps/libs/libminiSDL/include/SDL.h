20485d8303581d811bc6552f25fa7fe3707cf55b
22050710-Einsam <snitxmhm@gmail.com>
2023-01-02 16:41:29 +0800

    debug(libminiSDL): TODO()

--- navy-apps/libs/libminiSDL/include/SDL.h
+++ navy-apps/libs/libminiSDL/include/SDL.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <debug.h>
 
 #ifdef __cplusplus
 extern "C" {
--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -10,6 +10,7 @@ static const char *keyname[] = {
 };
 
 int SDL_PushEvent(SDL_Event *ev) {
+  TODO();
   return 0;
 }
 
@@ -69,9 +70,11 @@ int SDL_WaitEvent(SDL_Event *event) {
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+  TODO();
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
+  TODO();
   return NULL;
 }
