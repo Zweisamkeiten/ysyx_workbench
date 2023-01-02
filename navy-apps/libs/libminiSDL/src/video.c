e60c4c39bc01aa5e248ede43fbe370cd7dee3ac6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-02 11:24:58 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.1-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 21 Dec 2022 22:27:59 +0000 x86_64 GNU/Linux  11:24:58 up  3:03, 14 users,  load average: 1.53, 1.31, 1.32

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -9,11 +9,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
 
-  uint32_t *src_pixels = (uint32_t *)src->pixels;
-  uint32_t *dst_pixels = (uint32_t *)dst->pixels;
   int src_w = src->w;
   int src_h = src->h;
-  int dst_w = dst->w;
 
   int srcrect_x, srcrect_y;
   int srcrect_w, srcrect_h;
@@ -34,42 +31,73 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
   int rows; // has copid rows
   for (rows = 0; rows < srcrect_h; rows++) {
-    memcpy(dst->pixels + (rows + dstrect_y) * dst->pitch + dstrect_x * dst->format->BytesPerPixel, // pointer to dst rect current coping row first pixel
-           src->pixels + (rows + srcrect_y) * src->pitch + srcrect_x * src->format->BytesPerPixel, // pointer to src rect current coping row first pixel
-           srcrect_w * src->format->BytesPerPixel); // size
+    if (dst->format->BytesPerPixel == 1) {
+      for (int column = 0; column < srcrect_w; column++) {
+        // 像素阵列存放的是8位的调色板下标,
+        // 用这个下标在调色板中进行索引, 得到的才是32位的颜色信息
+        uint8_t src_color_xy_idx = *(src->pixels + (rows + srcrect_y) * src->pitch + (column + srcrect_x));
+        uint8_t dst_color_xy_idx = *(dst->pixels + (rows + dstrect_y) * dst->pitch + (column + dstrect_x));
+        dst->format->palette->colors[dst_color_xy_idx] = src->format->palette->colors[src_color_xy_idx];
+      }
+    } else {
+      memcpy(dst->pixels + (rows + dstrect_y) * dst->pitch + dstrect_x * dst->format->BytesPerPixel, // pointer to dst rect current coping row first pixel
+             src->pixels + (rows + srcrect_y) * src->pitch + srcrect_x * src->format->BytesPerPixel, // pointer to src rect current coping row first pixel
+             srcrect_w * src->format->BytesPerPixel); // size
+
+    }
   }
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  uint32_t *pixels = (uint32_t *)dst->pixels;
-  int sur_w = dst->w;
-  int sur_h = dst->h;
-
   int rectx, recty;
   int rectw, recth;
   if (dstrect == NULL) {
     rectx = 0, recty = 0;
-    rectw = sur_w, recth = sur_h;
+    rectw = dst->w, recth = dst->h;
   } else {
     rectx = dstrect->x, recty = dstrect->y;
     rectw = dstrect->w, recth = dstrect->h;
   }
 
-  assert(rectx + rectw <= sur_w && recty + recth <= sur_h);
-  for (int row = recty; row < recty + recth; row++) {
-    for (int column = rectx; column < rectx + rectw; column++) {
-      pixels[row*sur_w + column] = color;
+  assert(rectx + rectw <= dst->w && recty + recth <= dst->h);
+  int rows; // has copid rows
+  for (rows = 0; rows < recth; rows++) {
+    if (dst->format->BytesPerPixel == 1) {
+      for (int column = 0; column < rectw; column++) {
+        // 像素阵列存放的是8位的调色板下标,
+        // 用这个下标在调色板中进行索引, 得到的才是32位的颜色信息
+        uint8_t dst_color_xy_idx = *(dst->pixels + (rows + recty) + (column + rectx));
+        dst->format->palette->colors[dst_color_xy_idx] = (SDL_Color)color;
+      }
+    } else {
+      for (int column = 0; column < rectw; column++) {
+        memcpy(dst->pixels + (rows + recty) * dst->pitch + (column + rectx) * dst->format->BytesPerPixel,
+               &color,
+               sizeof(color));
+      }
     }
   }
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   if ((x & y & w & h) == 0) {
-    NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);
+    x = 0, y = 0, w = s->w, h = s->h;
   }
-  else {
-    NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);
+
+  if (s->format->BytesPerPixel == 1) {
+    for (int row = 0; row < h; row++) {
+      for (int column = 0; column < w; column++) {
+        // 像素阵列存放的是8位的调色板下标,
+        // 用这个下标在调色板中进行索引, 得到的才是32位的颜色信息
+        uint8_t color_xy_idx = *(s->pixels + (row + y) * s->pitch + (column + x));
+        uint32_t color = *(uint32_t *)(s->format->palette->colors + color_xy_idx);
+        NDL_DrawRect(&color, row, column, 1, 1);
+      }
+      return;
+    }
   }
+
+  NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);
 }
 
 // APIs below are already implemented.
