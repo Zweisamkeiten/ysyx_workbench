ab2f445f298962fb77a706568ed8cef5d91e370f
22050710-Einsam <snitxmhm@gmail.com>
2023-01-12 16:10:45 +0800

    feat(libos): crt0.c make c runtime support c++ global object init

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -3,10 +3,12 @@
 #include <assert.h>
 
 int main(int argc, char *argv[], char *envp[]);
+void __libc_init_array(void);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
+  __libc_init_array();
   exit(main(0, empty, empty));
   assert(0);
 }
