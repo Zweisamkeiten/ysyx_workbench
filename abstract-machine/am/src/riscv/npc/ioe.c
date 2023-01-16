076c69010faa422af822f7b18b784b47e37a4978
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 11:11:07 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  11:11:07 up  2:00, 13 users,  load average: 1.03, 1.13, 1.13

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -6,13 +6,10 @@ void __am_gpu_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
+void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_gpu_config(AM_GPU_CONFIG_T *);
 void __am_gpu_status(AM_GPU_STATUS_T *);
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *);
-void __am_audio_config(AM_AUDIO_CONFIG_T *);
-void __am_audio_ctrl(AM_AUDIO_CTRL_T *);
-void __am_audio_status(AM_AUDIO_STATUS_T *);
-void __am_audio_play(AM_AUDIO_PLAY_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
@@ -29,10 +26,6 @@ static void *lut[128] = {
   [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
   [AM_GPU_STATUS  ] = __am_gpu_status,
   [AM_UART_CONFIG ] = __am_uart_config,
-  [AM_AUDIO_CONFIG] = __am_audio_config,
-  [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
-  [AM_AUDIO_STATUS] = __am_audio_status,
-  [AM_AUDIO_PLAY  ] = __am_audio_play,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
