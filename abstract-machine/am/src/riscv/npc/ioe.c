9aad4067e952335902d96d281ddb7e78fc20b89f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 10:13:54 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  10:13:54 up  1:03, 13 users,  load average: 1.19, 1.14, 1.18

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -37,5 +37,5 @@ bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { putch(reg); ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) { putch(reg + '0'); ((handler_t)lut[reg])(buf); }
 void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
