3834f922b8e3563bf2bb55e680e350c30449b5b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 10:08:43 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  10:08:43 up 58 min, 13 users,  load average: 1.07, 1.12, 1.20

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -47,8 +47,12 @@ static void *lut[128] = {
 static void fail(void *buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
-  for (int i = 0; i < LENGTH(lut); i++)
+  // for (int i = 0; i < LENGTH(lut); i++)
+  //   if (!lut[i]) lut[i] = fail;
+  for (int i = 0; i < LENGTH(lut); i++) {
+    putch(42);
     if (!lut[i]) lut[i] = fail;
+  }
   __am_gpu_init();
   __am_timer_init();
   __am_audio_init();
