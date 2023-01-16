165e2f44e46dcf8fe6ad1f7a87258543350a295a
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 11:08:13 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  11:08:13 up  1:57, 13 users,  load average: 1.31, 1.22, 1.16

--- npc/csrc/src/cpu/cpu-exec.cc
+++ npc/csrc/src/cpu/cpu-exec.cc
@@ -261,7 +261,7 @@ void cpu_exec(uint64_t n) {
              : (npc_state.halt_ret == 0
                     ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN)
                     : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
-        npc_state.halt_pc);
+        snpc);
     // fall through
   case NPC_QUIT: break;
   }
