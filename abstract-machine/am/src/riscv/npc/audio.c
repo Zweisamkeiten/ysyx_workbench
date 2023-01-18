3b08e478c3a856d209ea594742255909f7a7b6c2
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 11:16:14 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  11:16:14 up  2:14, 11 users,  load average: 1.28, 1.72, 1.78

--- abstract-machine/am/src/riscv/npc/audio.c
+++ abstract-machine/am/src/riscv/npc/audio.c
@@ -24,8 +24,8 @@ void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   // outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  // outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  // outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
