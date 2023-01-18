fc935c76ef0a899c357c56e22891311b644dc11c
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 10:23:58 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  10:23:58 up  1:22,  9 users,  load average: 1.11, 1.21, 1.19

--- npc/vsrc/ex_stage.v
+++ npc/vsrc/ex_stage.v
@@ -111,8 +111,10 @@ module ysyx_22050710_exu (
   wire [63:0] unsigned_div_result = src_a / src_b;
 
   // signed rem
-  /* wire signed [63:0] signed_rem_result = $signed(src_a) % $signed(src_b); */
-  wire signed [63:0] signed_rem_result = i_word_cut ? $signed({{32{src_a[32]}}, src_a[31:0]}) % $signed({{32{src_b[32]}}, src_b[31:0]}) : $signed(src_a) % $signed(src_b);
+  wire signed [63:0] signed_rem_result = $signed(src_a) % $signed(src_b);
+  always @(*) begin
+    $display("%x", signed_rem_result);
+  end
 
   // unsigned rem
   wire [63:0] unsigned_rem_result = src_a % src_b;
