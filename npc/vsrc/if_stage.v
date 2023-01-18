f71b10b71242ded4bc934bae553edff1ca77ce96
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 16:16:33 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  16:16:33 up  7:15, 11 users,  load average: 1.11, 1.11, 1.09

--- npc/vsrc/if_stage.v
+++ npc/vsrc/if_stage.v
@@ -9,7 +9,7 @@ module ysyx_22050710_ifu (
 
   wire [63:0] rdata;
   /* assign o_inst   = i_pc[2] == 1'b0 ? rdata[31:0] : rdata[63:32]; */
-  assign o_inst   = rdata[31:0]
+  assign o_inst   = rdata[31:0];
   assign o_unused = rdata[63:32];
 
   always @(posedge i_clk) begin
