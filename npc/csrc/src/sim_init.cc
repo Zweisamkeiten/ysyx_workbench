a065e93f98a57e4fb778442a45a7b9131671f9ad
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 13:40:40 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  13:40:40 up  4:48, 12 users,  load average: 1.39, 1.37, 1.30

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -45,14 +45,7 @@ extern "C" void set_csr_ptr(const svOpenArrayHandle r) {
 
 extern "C" void npc_pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
-  word_t addr;
-  if (likely(in_pmem(raddr))) {
-    addr = raddr & ~0x7ull;
-  } else {
-    printf("123\n");
-    addr = raddr;
-  }
-
+  word_t addr = raddr & ~0x7ull;
   *rdata = paddr_read(addr, 8);
 }
 
@@ -60,24 +53,13 @@ extern "C" void npc_pmem_write(long long waddr, long long wdata, char wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  word_t addr;
-  if (likely(in_pmem(waddr))) {
-    addr = waddr & ~0x7ull;
-    uint8_t *p = (uint8_t *)&wdata;
-    for (int i = 0; i < 8; i++) {
-      if ((wmask & 0x1) == 0x1) {
-        paddr_write(addr + i, 1, *(p + i));
-      }
-      wmask = wmask >> 1;
+  word_t addr = waddr & ~0x7ull;
+  uint8_t *p = (uint8_t *)&wdata;
+  for (int i = 0; i < 8; i++) {
+    if ((wmask & 0x1) == 0x1) {
+      paddr_write(addr + i, 1, *(p + i));
     }
-  } else {
-    addr = waddr;
-    switch((unsigned char)wmask) {
-      case 0x1: paddr_write(addr, 1, wdata); break;
-      case 0x3: paddr_write(addr, 2, wdata); break;
-      case 0xf: paddr_write(addr, 4, wdata); break;
-      default: paddr_write(addr, 8, wdata); break;
-    }
+    wmask = wmask >> 1;
   }
 }
 
