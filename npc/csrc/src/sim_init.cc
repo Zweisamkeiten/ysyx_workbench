ebf81c3da19257cb243fc1c8704a9b868cbe1882
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 09:37:19 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  09:37:19 up 45 min, 12 users,  load average: 1.43, 1.35, 1.16

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -56,10 +56,10 @@ extern "C" void npc_pmem_write(long long waddr, long long wdata, char wmask) {
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
   word_t addr = waddr & ~0x7ull;
   uint8_t *p = (uint8_t *)&wdata;
-  printf("write: ");
+  // printf("write: ");
   for (int i = 7; i >= 0; i--, addr++) {
-    printf("%02x: ", *(p-i+7));
-    printf("%01x, ", (wmask & 0x1));
+    // printf("%02x: ", *(p-i+7));
+    // printf("%01x, ", (wmask & 0x1));
     if ((wmask & 0x1) == 0x1) {
       paddr_write(addr, 1, *(p - i + 7));
     }
