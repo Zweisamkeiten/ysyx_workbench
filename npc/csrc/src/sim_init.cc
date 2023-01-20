d248db5260879880faa3350345d08befe935e6ba
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 10:27:47 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  10:27:47 up  1:35, 12 users,  load average: 1.33, 1.28, 1.22

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -20,6 +20,7 @@ void set_state_end() {
 }
 
 void set_state_abort() {
+  printf("123\n");
   if (npc_state.state != NPC_STOP) {
     printf("There are two cases which will trigger this unexpected exception:\n"
         "1. The instruction at PC = " FMT_WORD " is not implemented.\n"
