4bdc4ca1e16eb4810227c466c2738e57efec67e3
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 11:08:26 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  11:08:26 up  2:16, 12 users,  load average: 1.32, 1.23, 1.28

--- npc/csrc/src/device/audio.c
+++ npc/csrc/src/device/audio.c
@@ -96,7 +96,7 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
       case reg_sbuf_size: audio_base[reg_sbuf_size] = CONFIG_SB_SIZE; break;
       case reg_init: audio_base[reg_init] = true; break;
       case reg_count: audio_base[reg_count] = count; break;
-      default: panic("offset illegal");
+      // default: panic("offset illegal");
     }
   }
 }
