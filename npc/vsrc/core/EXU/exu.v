09f91687df868c2042999907baa8c3d29e0f51e8
22050710-Einsam <snitxmhm@gmail.com>
2023-02-02 11:20:37 +0800

    refactor(npc): pc jumper split independent

--- npc/vsrc/core/EXU/exu.v
+++ npc/vsrc/core/EXU/exu.v
@@ -47,38 +47,16 @@ module ysyx_22050710_exu (
     .o_ALUresult(o_ALUresult)
   );
 
-  wire PCAsrc, PCBsrc;
-  assign o_nextpc = (PCBsrc ? i_rs1 : i_pc) + (PCAsrc ? i_imm : 64'd4);
-  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(1)) u_mux1 (
-    .out(PCAsrc),
-    .key(i_Branch),
-    .lut({
-      3'b000, 1'b0,
-      3'b001, 1'b1,
-      3'b010, 1'b1,
-      3'b100, Zero == 1 ? 1'b1 : 1'b0,
-      3'b101, Zero == 1 ? 1'b0 : 1'b1,
-      3'b110, Less == 1 ? 1'b1 : 1'b0,
-      3'b111, Less == 1 ? 1'b0 : 1'b1
-    })
-  );
-  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(1)) u_mux2 (
-    .out(PCBsrc),
-    .key(i_Branch),
-    .lut({
-      3'b000, 1'b0,
-      3'b001, 1'b0,
-      3'b010, 1'b1,
-      3'b100, 1'b0,
-      3'b101, 1'b0,
-      3'b110, 1'b0,
-      3'b111, 1'b0
-    })
+  ysyx_22050710_pc_jumper u_pc_jumper (
+    .i_rs1(i_rs1), .i_pc(i_pc), .i_imm(i_imm),
+    .i_Branch(i_Branch),
+    .i_zero(Zero), .i_less(Less),
+    .o_nextpc(o_nextpc)
   );
 
   wire [63:0] rdata;
   assign o_GPRbusW = i_MemtoReg ? rdata : (i_sel_csr ? i_rs2 : o_ALUresult);
-  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux3 (
+  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux1 (
     .out(rdata),
     .key(i_MemOP),
     .lut({
---
+++ npc/vsrc/core/EXU/pc_jumper.v
@@ -0,0 +1,42 @@
+// ysyx_22050710 pc arithmetic unit
+
+module ysyx_22050710_pc_jumper (
+  input  [63:0] i_rs1, i_pc, i_imm,
+  input  [2:0]  i_Branch,
+  input  i_zero,
+  input  i_less,
+  output [63:0] o_nextpc
+);
+
+  wire PCAsrc, PCBsrc;
+  assign o_nextpc = (PCBsrc ? i_rs1 : i_pc) + (PCAsrc ? i_imm : 64'd4);
+
+  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(1)) u_mux0 (
+    .out(PCAsrc),
+    .key(i_Branch),
+    .lut({
+      3'b000, 1'b0,
+      3'b001, 1'b1,
+      3'b010, 1'b1,
+      3'b100, i_zero == 1 ? 1'b1 : 1'b0,
+      3'b101, i_zero == 1 ? 1'b0 : 1'b1,
+      3'b110, i_less == 1 ? 1'b1 : 1'b0,
+      3'b111, i_less == 1 ? 1'b0 : 1'b1
+    })
+  );
+
+  MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(1)) u_mux1 (
+    .out(PCBsrc),
+    .key(i_Branch),
+    .lut({
+      3'b000, 1'b0,
+      3'b001, 1'b0,
+      3'b010, 1'b1,
+      3'b100, 1'b0,
+      3'b101, 1'b0,
+      3'b110, 1'b0,
+      3'b111, 1'b0
+    })
+  );
+
+endmodule
