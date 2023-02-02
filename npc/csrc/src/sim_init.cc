1b9898bd965956d8e70260803c083356b86684e7
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 09:58:15 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  09:58:15 up  1:38,  9 users,  load average: 1.30, 1.33, 1.26

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -108,7 +108,7 @@ extern "C" void init_sim() {
 
   npc_state.state = NPC_RUNNING;
 
-  npcpc = &(top->rootp->ysyx_22050710_npc__DOT__pc);
+  npcpc = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__pc);
   cpu.pc = *npcpc;
 }
 
