3be34340d5382a076bd437dff2e56553de77b654
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 22:40:07 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  22:40:07 up 14:19,  9 users,  load average: 2.39, 1.95, 1.09

--- npc/csrc/src/isa/local-include/reg.h
+++ npc/csrc/src/isa/local-include/reg.h
@@ -29,7 +29,7 @@ static inline int check_csr_addr(int idx) {
 }
 
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
-#define csr(idx) (cpu.csr[check_csr_addr(idx)])
+#define csr(idx) (*(cpu.csr[check_csr_addr(idx)]))
 
 typedef enum {
   MSTATUS,
