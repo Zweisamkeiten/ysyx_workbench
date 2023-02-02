479b19d42cfc22a83ebbb57b72986603946b5a8c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 12:28:42 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  12:28:42 up  4:08,  9 users,  load average: 1.21, 1.20, 1.17

--- npc/vsrc/core/csregs.v
+++ npc/vsrc/core/csregs.v
@@ -18,7 +18,7 @@ module ysyx_22050710_csr #(ADDR_WIDTH = 12, DATA_WIDTH = 64) (
   output  reg [63:0] o_nextpc, output reg o_sys_change_pc
 );
 
-  reg [DATA_WIDTH-1:0] rf [1:0];
+  reg [DATA_WIDTH-1:0] rf [`NRCSR:0];
   initial set_csr_ptr(rf);
 
   always @(*) begin
@@ -60,7 +60,7 @@ module ysyx_22050710_csr #(ADDR_WIDTH = 12, DATA_WIDTH = 64) (
   );
 
   wire [ADDR_WIDTH-1:0] waddr;
-  MuxKey #(.NR_KEY(`NRCSR), .KEY_LEN(12), .DATA_LEN(12)) u_mux1 (
+  MuxKey #(.NR_KEY(`NRCSR), .KEY_LEN(2), .DATA_LEN(12)) u_mux1 (
     .out(waddr),
     .key(i_waddr),
     .lut({
