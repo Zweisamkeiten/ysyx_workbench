bf7894af7f7c27a944b4f9ec388f56f5503bde24
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 15:55:07 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  15:55:07 up  7:34,  9 users,  load average: 1.30, 1.37, 1.29

--- npc/vsrc/core/csregs.v
+++ npc/vsrc/core/csregs.v
@@ -24,7 +24,7 @@ module ysyx_22050710_csr #(ADDR_WIDTH = 12, DATA_WIDTH = 64) (
 
     if (i_raise_intr) begin
       o_nextpc = mtvec;
-      $display(mtvec);
+      $display("%x", mtvec);
       o_sys_change_pc = 1'b1;
     end
     else if(i_intr_ret) begin // mret
