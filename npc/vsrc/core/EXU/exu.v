e7991fc13b6b33a2234783eba76601279ea49571
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 10:52:32 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  10:52:32 up  2:32,  9 users,  load average: 1.26, 1.21, 1.18

--- npc/vsrc/core/EXU/exu.v
+++ npc/vsrc/core/EXU/exu.v
@@ -25,7 +25,7 @@ wire Zero, Less;
     .i_rs1(i_rs1), .i_rs2(i_rs2), .i_imm(i_imm), .i_pc(i_pc),
     .i_ALUAsrc(i_ALUAsrc), .i_ALUBsrc(i_ALUBsrc), .i_ALUctr(i_ALUctr),
     .i_word_cut(i_word_cut),
-    .o_zero(Zero), .o_less(Less)
+    .o_zero(Zero), .o_less(Less),
     .o_ALUresult(o_ALUresult)
   );
 
