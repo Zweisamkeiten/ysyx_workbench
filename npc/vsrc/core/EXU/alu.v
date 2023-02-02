e4e84d24277fce0a487c91544cb20aead5742391
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 10:56:53 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 04 Jan 2023 16:28:17 +0000 x86_64 GNU/Linux  10:56:53 up  2:36,  9 users,  load average: 1.27, 1.21, 1.19

--- npc/vsrc/core/EXU/alu.v
+++ npc/vsrc/core/EXU/alu.v
@@ -1,7 +1,7 @@
 // ysyx_22050710 alu
 
 module ysyx_22050710_alu (
-  input  [63:0] i_src_a, i_src_b,
+  input  [63:0] i_i_src_a, i_i_src_b,
   input  [4:0] i_ALUctr,
   input  i_word_cut,
   output o_zero,
@@ -21,7 +21,7 @@ module ysyx_22050710_alu (
   wire signed_Less = overflow == 0
                    ? (sub_result[63] == 1 ? 1'b1 : 1'b0)
                    : (sub_result[63] == 0 ? 1'b1 : 1'b0);
-  wire unsigned_Less = (1'b1 ^ cout) & ~(|src_b == 1'b0); // CF = cin ^ cout
+  wire unsigned_Less = (1'b1 ^ cout) & ~(|i_src_b == 1'b0); // CF = cin ^ cout
 
   // if operand has been cut, the aluresult need signed extend to 64bits from
   // [32:0]
@@ -29,54 +29,54 @@ module ysyx_22050710_alu (
   assign o_ALUresult = i_word_cut ? {{32{aluresult[31]}}, aluresult[31:0]} : aluresult;
 
   // adder
-  wire[63:0] adder_result = src_a + src_b;
+  wire[63:0] adder_result = i_src_a + i_src_b;
   wire [63:0] sub_result; wire cout;
-  wire overflow = ~(src_a[63] ^ src_b[63]) ^ ~(src_a[62] ^ src_b[62]);
-  assign {cout, sub_result}   = {1'b0, src_a} + {1'b0, (({64{1'b1}}^(src_b)) + 1)};
+  wire overflow = ~(i_src_a[63] ^ i_src_b[63]) ^ ~(i_src_a[62] ^ i_src_b[62]);
+  assign {cout, sub_result}   = {1'b0, i_src_a} + {1'b0, (({64{1'b1}}^(i_src_b)) + 1)};
 
   // copy imm
   wire [63:0] copy_result = i_imm;
 
   // signed mul
-  wire signed [63:0] signed_mul_result = $signed(src_a) * $signed(src_b);
+  wire signed [63:0] signed_mul_result = $signed(i_src_a) * $signed(i_src_b);
 
-  wire signed [63:0] signed_mulh_result = {$signed({{64{1'b0}}, src_a}) * $signed({{64{1'b0}}, src_b}) >> 64}[63:0];
+  wire signed [63:0] signed_mulh_result = {$signed({{64{1'b0}}, i_src_a}) * $signed({{64{1'b0}}, i_src_b}) >> 64}[63:0];
 
-  wire signed [63:0] su_mulh_result = {$signed({{64{1'b0}}, src_a}) * {{64{1'b0}}, src_b} >> 64}[63:0];
+  wire signed [63:0] su_mulh_result = {$signed({{64{1'b0}}, i_src_a}) * {{64{1'b0}}, i_src_b} >> 64}[63:0];
 
-  wire signed [63:0] unsigned_mulh_result = {{{64{1'b0}}, src_a} * {{64{1'b0}}, src_b} >> 64}[63:0];
+  wire signed [63:0] unsigned_mulh_result = {{{64{1'b0}}, i_src_a} * {{64{1'b0}}, i_src_b} >> 64}[63:0];
 
   // signed div
-  wire signed [63:0] signed_div_result = i_word_cut ? ($signed({{32{src_a[31]}}, src_a[31:0]}) / $signed({{32{src_b[31]}}, src_b[31:0]})) : $signed(src_a) / $signed(src_b);
+  wire signed [63:0] signed_div_result = i_word_cut ? ($signed({{32{i_src_a[31]}}, i_src_a[31:0]}) / $signed({{32{i_src_b[31]}}, i_src_b[31:0]})) : $signed(i_src_a) / $signed(i_src_b);
 
   // unsigned div
-  wire [63:0] unsigned_div_result = src_a / src_b;
+  wire [63:0] unsigned_div_result = i_src_a / i_src_b;
 
   // signed rem
-  wire signed [63:0] signed_rem_result = i_word_cut ? ($signed({{32{src_a[31]}}, src_a[31:0]}) % $signed({{32{src_b[31]}}, src_b[31:0]})) : $signed(src_a) % $signed(src_b);
+  wire signed [63:0] signed_rem_result = i_word_cut ? ($signed({{32{i_src_a[31]}}, i_src_a[31:0]}) % $signed({{32{i_src_b[31]}}, i_src_b[31:0]})) : $signed(i_src_a) % $signed(i_src_b);
 
   // unsigned rem
-  wire [63:0] unsigned_rem_result = src_a % src_b;
+  wire [63:0] unsigned_rem_result = i_src_a % i_src_b;
 
   // xor
-  wire [63:0] xor_result = src_a ^ src_b;
+  wire [63:0] xor_result = i_src_a ^ i_src_b;
 
   // and
-  wire [63:0] and_result = src_a & src_b;
+  wire [63:0] and_result = i_src_a & i_src_b;
 
   // or
-  wire [63:0] or_result = src_a | src_b;
+  wire [63:0] or_result = i_src_a | i_src_b;
 
   // sll
-  wire [63:0] sll_result = src_a << (i_word_cut ? {1'b0, src_b[4:0]} : src_b[5:0]);
+  wire [63:0] sll_result = i_src_a << (i_word_cut ? {1'b0, i_src_b[4:0]} : i_src_b[5:0]);
 
   // srl
-  wire [63:0] srl_result = src_a >> (i_word_cut ? {1'b0, src_b[4:0]} : src_b[5:0]);
+  wire [63:0] srl_result = i_src_a >> (i_word_cut ? {1'b0, i_src_b[4:0]} : i_src_b[5:0]);
 
   // sra
   wire signed [63:0] sra_result = i_word_cut
-                                  ? $signed({{32{src_a[31]}}, $signed(src_a[31:0]) >>> $signed((i_word_cut ? {1'b0, src_b[4:0]} : src_b[5:0]))})
-                                  : $signed(src_a) >>> $signed((i_word_cut ? {1'b0, src_b[4:0]} : src_b[5:0]));
+                                  ? $signed({{32{i_src_a[31]}}, $signed(i_src_a[31:0]) >>> $signed((i_word_cut ? {1'b0, i_src_b[4:0]} : i_src_b[5:0]))})
+                                  : $signed(i_src_a) >>> $signed((i_word_cut ? {1'b0, i_src_b[4:0]} : i_src_b[5:0]));
 
   MuxKey #(.NR_KEY(19), .KEY_LEN(5), .DATA_LEN(64)) u_mux2 (
     .out(aluresult),
