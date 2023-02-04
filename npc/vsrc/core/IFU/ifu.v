6cab813e283b7d7d7dff1ae42d79e67a125dd5e1
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 13:40:32 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  13:40:32 up  6:25,  2 users,  load average: 1.37, 1.25, 1.15

--- npc/vsrc/core/IFU/ifu.v
+++ npc/vsrc/core/IFU/ifu.v
@@ -9,7 +9,7 @@ module ysyx_22050710_ifu (
   reg [63:0] rdata;
   assign o_inst = i_pc[2] == 1'b0 ? rdata[31:0] : rdata[63:32];
 
-  always @(posedge i_clk or posedge i_rst) begin
+  always @(posedge i_clk) begin
     if (!i_rst) begin
       npc_pmem_read(i_pc, rdata);
     end
