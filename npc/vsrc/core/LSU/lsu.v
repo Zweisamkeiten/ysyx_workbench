830d6ecee9cf73e977c0584e0a928cb98f0c1da1
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 11:22:45 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  11:22:45 up  4:07,  2 users,  load average: 1.20, 1.13, 1.10

--- npc/vsrc/core/LSU/lsu.v
+++ npc/vsrc/core/LSU/lsu.v
@@ -74,7 +74,11 @@ module ysyx_22050710_lsu (
   );
 
   always @(*) begin
-    if (!i_rst & i_ReEn & i_MemOP != 3'b111) begin
+    if (i_rst) begin
+      rdata = 64'b0;
+    end
+    else begin
+      if (i_ReEn & i_MemOP != 3'b111) begin
       npc_pmem_read(raddr, rdata);
       case (raddr[2:0])
         3'h1: rdata = {{ 8{1'b0}}, rdata[63:8 ]};
@@ -86,9 +90,6 @@ module ysyx_22050710_lsu (
         3'h7: rdata = {{56{1'b0}}, rdata[63:56]};
         default: rdata = rdata;
       endcase
-    end
-    else begin
-      rdata = 64'b0;
     end
   end
 
