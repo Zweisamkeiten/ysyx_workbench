0f441bf6ae3c65125c5e1c90a71675f62f580e2d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 12:34:52 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  12:34:52 up  5:19,  2 users,  load average: 1.40, 0.93, 0.52

--- npc/vsrc/core/IDU/idu.v
+++ npc/vsrc/core/IDU/idu.v
@@ -15,9 +15,6 @@ module ysyx_22050710_idu (
   output  o_raise_intr, o_intr_ret
 );
 
-  always @(*) begin
-    $display("%x", i_inst);
-  end
   wire [6:0] opcode;
   wire [2:0] funct3; wire [6:0] funct7;
 
@@ -311,5 +308,8 @@ module ysyx_22050710_idu (
   );
 
   assign o_is_invalid_inst = (o_ALUctr == 5'b11111) && (o_EXctr == 4'b1111) && (i_inst != 32'b0);
+  always @(*) begin
+    $display("%x", o_is_invalid_inst);
+  end
 
 endmodule
