61435dd864a6705a4bcf1f43465dff848eca5521
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 12:32:37 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  12:32:37 up  5:17,  2 users,  load average: 1.17, 0.65, 0.37

--- npc/vsrc/core/pc.v
+++ npc/vsrc/core/pc.v
@@ -9,6 +9,6 @@ module ysyx_22050710_pc (
 );
   
   // 位宽为64bits, 复位值为64'h80000000, 写使能为i_load;
-  Reg #(64, 64'h80000000) u_0 (i_clk, i_rst, i_in, o_pc, i_load);
+  Reg #(64, 64'h80000000) u_0 (i_clk, ~i_rst, i_in, o_pc, i_load);
 
 endmodule
