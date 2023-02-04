5bc9203fb9fcfc657156003a3cb57bbaae36a1bf
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 20:17:11 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  20:17:11 up 13:02,  2 users,  load average: 1.21, 1.15, 1.13

--- npc/vsrc/core/WBU/wbu.v
+++ npc/vsrc/core/WBU/wbu.v
@@ -1,11 +1,11 @@
 // ysyx_22050710 Write back unit
 module ysyx_22050710_wbu #(GPRADDR_WIDTH = 5, DATA_WIDTH = 64) (
-  input i_rf_wen;
-  input [GPRADDR_WIDTH-1:0] i_rf_waddr;
-  input [DATA_WIDTH-1:0] i_rf_wdata;
-  output o_rf_wen;
-  output [GPRADDR_WIDTH-1:0] o_rf_waddr;
-  output [DATA_WIDTH-1:0] o_rf_wdata;
+  input i_rf_wen,
+  input [GPRADDR_WIDTH-1:0] i_rf_waddr,
+  input [DATA_WIDTH-1:0] i_rf_wdata,
+  output o_rf_wen,
+  output [GPRADDR_WIDTH-1:0] o_rf_waddr,
+  output [DATA_WIDTH-1:0] o_rf_wdata
 );
 
 
