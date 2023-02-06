3c798e48f500ac6f838547cd61e589418825f013
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 22:36:59 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  22:36:59 up 12:56,  2 users,  load average: 1.66, 1.72, 1.79

--- npc/vsrc/core/core.v
+++ npc/vsrc/core/core.v
@@ -1,12 +1,19 @@
 // ysyx_22050710 NPC CORE
-`include "defines.v"
 
 module ysyx_22050710_core #(
+  parameter WORD_WD                                          ,
   parameter PC_RESETVAL                                      ,
   parameter PC_WD                                            ,
+  parameter GPR_WD                                           ,
+  parameter GPR_ADDR_WD                                      ,
+  parameter IMM_WD                                           ,
   parameter INST_WD                                          ,
 
   parameter FS_TO_DS_BUS_WD  = `ysyx_22050710_FS_TO_DS_BUS_WD,
+  parameter DS_TO_ES_BUS_WD  = `ysyx_22050710_DS_TO_ES_BUS_WD,
+  /* parameter ES_TO_MS_BUS_WD = `ysyx_22050710_ES_TO_MS_BUS_WD , */
+  /* parameter MS_TO_WS_BUS_WD = `ysyx_22050710_MS_TO_WS_BUS_WD , */
+  parameter WS_TO_RF_BUS_WD  = `ysyx_22050710_WS_TO_RF_BUS_WD,
   parameter BR_BUS_WD        = `ysyx_22050710_BR_BUS_WD      ,
 
   parameter SRAM_ADDR_WD                                     ,
@@ -20,8 +27,19 @@ module ysyx_22050710_core #(
   input  [SRAM_DATA_WD-1:0   ] i_inst_sram_rdata
 );
 
+  /* wire         ds_allowin; */
+  /* wire         es_allowin; */
+  /* wire         ms_allowin; */
+  /* wire         ws_allowin; */
   wire                         fs_to_ds_valid                ;
+  /* wire         ds_to_es_valid; */
+  /* wire         es_to_ms_valid; */
+  /* wire         ms_to_ws_valid; */
   wire [FS_TO_DS_BUS_WD-1:0  ] fs_to_ds_bus                  ;
+  wire [DS_TO_ES_BUS_WD -1:0 ] ds_to_es_bus                  ;
+  /* wire [ES_TO_MS_BUS_WD -1:0] es_to_ms_bus; */
+  /* wire [MS_TO_WS_BUS_WD -1:0] ms_to_ws_bus; */
+  wire [WS_TO_RF_BUS_WD -1:0 ] ws_to_rf_bus                  ;
   wire [BR_BUS_WD-1:0        ] br_bus                        ;
 
 
@@ -48,9 +66,32 @@ module ysyx_22050710_core #(
     .i_inst_sram_rdata        (i_inst_sram_rdata            )
   );
 
-  wire [31:0] inst; wire [63:0] pc;
-  assign {inst, pc} = fs_to_ds_bus;
   wire [63:0] nextpc = sys_change_pc ? sysctr_pc : brtarget;
+
+  ysyx_22050710_id_stage #(
+    .WORD_WD                  (WORD_WD                      ),
+    .INST_WD                  (INST_WD                      ),
+    .PC_WD                    (PC_WD                        ),
+    .GPR_WD                   (GPR_WD                       ),
+    .GPR_ADDR_WD              (GPR_ADDR_WD                  ),
+    .IMM_WD                   (IMM_WD                       ),
+    .FS_TO_DS_BUS_WD          (FS_TO_DS_BUS_WD              ),
+    .DS_TO_ES_BUS_WD          (DS_TO_ES_BUS_WD              ),
+    .BR_BUS_WD                (BR_BUS_WD                    ),
+    .WS_TO_RF_BUS_WD          (WS_TO_RF_BUS_WD              )
+  ) u_id_stage (
+    .i_clk                    (i_clk                        ),
+    .i_rst                    (i_rst                        ),
+    // from fs
+    .i_fs_to_ds_valid         (fs_to_ds_valid               ),
+    .i_fs_to_ds_bus           (fs_to_ds_bus                 ),
+    // to es
+    .o_ds_to_es_bus           (ds_to_es_bus                 ),
+    // to fs
+    .o_br_bus                 (br_bus                       ),
+    // from ws to rf: for write back
+    .i_ws_to_rf_bus           (ws_to_rf_bus                 )
+  );
 
   wire [63:0] rs1data, rs2data;
   wire [63:0] GPRbusW;
