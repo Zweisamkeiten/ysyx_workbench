f78cf431a57ccfa6cd719aff2c5fa340110e3913
22050710-Einsam <snitxmhm@gmail.com>
2023-02-06 10:18:32 +0800

    refactor(npc): 修改csr写数据通路

--- npc/vsrc/core/WBU/wbu.v
+++ npc/vsrc/core/WBU/wbu.v
@@ -1,15 +1,26 @@
 // ysyx_22050710 Write back unit
-module ysyx_22050710_wbu #(GPRADDR_WIDTH = 5, DATA_WIDTH = 64) (
+module ysyx_22050710_wbu #(GPRADDR_WIDTH = 5, CSRADDR_WIDTH = 12, DATA_WIDTH = 64) (
   input i_rf_wen,
   input [GPRADDR_WIDTH-1:0] i_rf_waddr,
   input [DATA_WIDTH-1:0] i_rf_wdata,
   output o_rf_wen,
   output [GPRADDR_WIDTH-1:0] o_rf_waddr,
-  output [DATA_WIDTH-1:0] o_rf_wdata
+  output [DATA_WIDTH-1:0] o_rf_wdata,
+
+  input i_csrrf_wen,
+  input [CSRADDR_WIDTH-1:0] i_csrrf_waddr,
+  input [DATA_WIDTH-1:0] i_csrrf_wdata,
+  output o_csrrf_wen,
+  output [CSRADDR_WIDTH-1:0] o_csrrf_waddr,
+  output [DATA_WIDTH-1:0] o_csrrf_wdata
 );
 
   assign o_rf_wen = i_rf_wen;
   assign o_rf_waddr = i_rf_waddr;
   assign o_rf_wdata = i_rf_wdata;
+
+  assign o_csrrf_wen = i_csrrf_wen;
+  assign o_csrrf_waddr = i_csrrf_waddr;
+  assign o_csrrf_wdata = i_csrrf_wdata;
 
 endmodule
--- npc/vsrc/core/core.v
+++ npc/vsrc/core/core.v
@@ -32,14 +32,15 @@ module ysyx_22050710_core (
   wire [2:0] brfunc;
   wire ALUAsrc; wire [1:0] ALUBsrc; wire [4:0] ALUctr;
   wire word_cut;
-  wire ws_rf_wen, RegWr, MemtoReg, MemWr, MemRe; wire [2:0] MemOP;
+  wire ws_rf_wen, ws_csrrf_wen, RegWr, MemtoReg, MemWr, MemRe; wire [2:0] MemOP;
   wire [3:0] EXctr;
   wire is_invalid_inst;
-  wire sel_csr, sel_zimm/* , CsrWr */;
+  wire sel_csr, sel_zimm, CsrWr;
   wire [63:0] csrrdata;
   wire [63:0] CSRbusW;
   wire [63:0] sysctr_pc; wire sys_change_pc;
   wire [4:0] rd;
+  wire [11:0] csrwaddr;
   ysyx_22050710_idu u_idu (
     .i_clk(i_clk),
     .i_pc(pc),
@@ -49,7 +50,10 @@ module ysyx_22050710_core (
     .i_CSRbusW(CSRbusW),
     .i_ws_rf_wen(ws_rf_wen),
     .i_ws_rf_waddr(ws_rf_waddr),
+    .i_ws_csrrf_wen(ws_csrrf_wen),
+    .i_ws_csrrf_waddr(ws_csrrf_waddr),
     .o_rd(rd),
+    .o_csrwaddr(csrwaddr),
     .o_rs1data(rs1data), .o_rs2data(rs2data),
     .o_imm(imm),
     .o_bren(bren),
@@ -59,7 +63,7 @@ module ysyx_22050710_core (
     .o_RegWr(RegWr), .o_MemtoReg(MemtoReg), .o_MemWr(MemWr), .o_MemRe(MemRe), .o_MemOP(MemOP),
     .o_EXctr(EXctr),
     .o_is_invalid_inst(is_invalid_inst),
-    .o_sel_csr(sel_csr), .o_sel_zimm(sel_zimm),/*  .o_CsrWr(CsrWr), */
+    .o_sel_csr(sel_csr), .o_sel_zimm(sel_zimm), .o_CsrWr(CsrWr),
     .o_zimm(zimm),
     .o_csrrdata(csrrdata),
     .o_sys_change_pc(sys_change_pc), .o_sysctr_pc(sysctr_pc)
@@ -73,7 +77,7 @@ module ysyx_22050710_core (
     .o_dnpc(brtarget)
   );
 
-  wire [63:0] ALUresult;
+  wire [63:0] ALUresult, CSRresult;
   ysyx_22050710_exu u_exu (
     .i_rs1(rs1data), .i_rs2(rs2data),
     .i_imm(imm), .i_pc(pc),
@@ -84,34 +88,44 @@ module ysyx_22050710_core (
     .i_sel_zimm(sel_zimm),
     .i_csrrdata(csrrdata), .i_zimm(zimm),
     .o_ALUresult(ALUresult),
-    .o_CSRbusW(CSRbusW)
+    .o_CSRresult(CSRresult)
   );
 
   wire [63:0] lsu_addr = ALUresult; // x[rs1] + imm
-  wire [63:0] ms_rf_wdata;
+  wire [63:0] ms_rf_wdata, ms_csrrf_wdata;
   ysyx_22050710_lsu u_lsu (
     .i_clk(i_clk),
     .i_rst(i_rst),
     .i_addr(lsu_addr),
     .i_data(rs2data),
     .i_ALUresult(ALUresult),
+    .i_CSRresult(CSRresult),
     .i_csrrdata(csrrdata),
     .i_MemOP(MemOP),
     .i_MemtoReg(MemtoReg),
     .i_WrEn(MemWr),
     .i_ReEn(MemRe),
     .i_sel_csr(sel_csr), 
-    .o_w_rf_data(ms_rf_wdata)
+    .o_w_rf_data(ms_rf_wdata),
+    .o_w_csrrf_data(ms_csrrf_wdata)
   );
 
   wire [4:0] ws_rf_waddr;
+  wire [11:0] ws_csrrf_waddr;
   ysyx_22050710_wbu u_wbu (
     .i_rf_wen(RegWr),
     .i_rf_waddr(rd),
     .i_rf_wdata(ms_rf_wdata),
     .o_rf_wen(ws_rf_wen),
     .o_rf_waddr(ws_rf_waddr),
-    .o_rf_wdata(GPRbusW)
+    .o_rf_wdata(GPRbusW),
+
+    .i_csrrf_wen(CsrWr),
+    .i_csrrf_waddr(csrwaddr),
+    .i_csrrf_wdata(ms_csrrf_wdata),
+    .o_csrrf_wen(ws_csrrf_wen),
+    .o_csrrf_waddr(ws_csrrf_waddr),
+    .o_csrrf_wdata(CSRbusW)
   );
 
 endmodule
