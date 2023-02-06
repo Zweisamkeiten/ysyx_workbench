4bf775f73bb18794ffea96f9a3a3ad62d82e48fa
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 22:44:09 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  22:44:09 up 13:03,  2 users,  load average: 1.82, 1.77, 1.79

--- npc/vsrc/defines.v
+++ npc/vsrc/defines.v
@@ -1,10 +1,24 @@
-// sram addr width
-`define ysyx_22050710_SRAM_ADDR_WD                32
-`define ysyx_22050710_SRAM_DATA_WD                64
+`ifndef YSYX_22050710_MYCPU_TOP_H
+  `define YSYX_22050710_MYCPU_TOP_H
+  // sram addr width
+  `define ysyx_22050710_SRAM_ADDR_WD                32
+  `define ysyx_22050710_SRAM_DATA_WD                64
 
-// pc
-`define ysyx_22050710_PC_RESETVAL                 64'h80000000
-`define ysyx_22050710_PC_WD                       64
+  // pc
+  `define ysyx_22050710_PC_RESETVAL                 64'h80000000
+  `define ysyx_22050710_PC_WD                       64
 
-// inst width
-`define ysyx_22050710_INST_WD                     32
+  // inst width
+  `define ysyx_22050710_INST_WD                     32
+`endif
+
+`ifndef YSYX_22050710_MYCPU_STAGE_BUS_H
+  `define YSYX_22050710_MYCPU_STAGE_BUS_H
+  // Bus width
+  `define ysyx_22050710_FS_TO_DS_BUS_WD             96  // {fs_inst[31:0], fs_pc[63:0]} 32 + 64
+  `define ysyx_22050710_DS_TO_ES_BUS_WD             32
+  `define ysyx_22050710_ES_TO_MS_BUS_WD             32
+  `define ysyx_22050710_MS_TO_WS_BUS_WD             32
+  `define ysyx_22050710_WS_TO_RF_BUS_WD             32
+  `define ysyx_22050710_BR_BUS_WD                   65  // {br_sel[0:0], br_target[63:0]} 1 + 64
+`endif
