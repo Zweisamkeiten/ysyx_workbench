62d8e5a76c8fc8fe5ecb795e053692710d98a64f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 10:17:18 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  10:17:18 up 37 min,  2 users,  load average: 1.20, 1.21, 1.11

--- npc/vsrc/core/LSU/lsu.v
+++ npc/vsrc/core/LSU/lsu.v
@@ -21,7 +21,7 @@ module ysyx_22050710_lsu (
   wire [63:0] rdata;
   wire [63:0] memrdata;
   assign o_w_rf_data = i_MemtoReg ? memrdata : (i_sel_csr ? i_csrrdata : i_ALUresult);
-  assign o_w_csrf_data = i_CSRresult;
+  assign o_w_csrrf_data = i_CSRresult;
   MuxKey #(.NR_KEY(7), .KEY_LEN(3), .DATA_LEN(64)) u_mux0 (
     .out(memrdata),
     .key(i_MemOP),
