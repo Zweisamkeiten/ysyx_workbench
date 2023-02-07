1056b9a1a1a0771a1ea0655cc340d22206312e77
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 20:43:08 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  20:43:08 up  9:38,  2 users,  load average: 1.36, 1.37, 1.30

--- npc/csrc/src/sim_init.cc
+++ npc/csrc/src/sim_init.cc
@@ -107,10 +107,10 @@ extern "C" void init_sim() {
   npcpc = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_if_stage__DOT__u_pc__DOT__pc);
 
   QData ** csr = (QData **)malloc(NR_CSREGS * sizeof(uint64_t *));
-  csr[MSTATUS] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_idu__DOT__u_csrs__DOT__mstatus);
-  csr[MTVEC] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_idu__DOT__u_csrs__DOT__mtvec);
-  csr[MEPC] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_idu__DOT__u_csrs__DOT__mepc);
-  csr[MCAUSE] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_idu__DOT__u_csrs__DOT__mcause);
+  csr[MSTATUS] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_id_stage__DOT__u_csrs__DOT__mstatus);
+  csr[MTVEC] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_id_stage__DOT__u_csrs__DOT__mtvec);
+  csr[MEPC] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_id_stage__DOT__u_csrs__DOT__mepc);
+  csr[MCAUSE] = &(top->rootp->ysyx_22050710_top__DOT__u_core__DOT__u_id_stage__DOT__u_csrs__DOT__mcause);
 
   cpu.csr = csr;
 
