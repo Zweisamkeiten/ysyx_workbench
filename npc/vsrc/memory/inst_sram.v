805f8da83ac57ccfb29931c9e5800aea2a335806
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-08 13:36:03 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  13:36:03 up  4:20,  2 users,  load average: 1.69, 1.68, 1.59

--- npc/vsrc/memory/inst_sram.v
+++ npc/vsrc/memory/inst_sram.v
@@ -4,7 +4,7 @@ module ysyx_22050710_inst_sram #(
 ) (
   input                        i_clk                         ,
 
-  input                        i_en                          ,
+  input                        i_ren                         ,
   input  [SRAM_ADDR_WD   -1:0] i_addr                        ,
   output [SRAM_DATA_WD   -1:0] o_rdata
 );
@@ -13,8 +13,11 @@ module ysyx_22050710_inst_sram #(
   assign o_rdata             = rdata                         ;
 
   always @(posedge i_clk) begin
-    if (i_en) begin
+    if (i_ren) begin
       npc_pmem_read({32'b0, i_addr}, rdata);
+    end
+    else begin
+      rdata <= 0;
     end
   end
 
