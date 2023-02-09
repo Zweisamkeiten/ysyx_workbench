c58c1f4300e2a08beb71f04f2590e148028122dd
22050710-Einsam <snitxmhm@gmail.com>
2023-02-10 00:45:47 +0800

    style(npc): top.v

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -36,33 +36,33 @@ module ysyx_22050710_top #(
   wire [SRAM_DATA_WD-1:0     ] cpu_data_wdata                ;
 
   ysyx_22050710_core #( 
-    .WORD_WD                  (WORD_WD                      ),
-    .PC_RESETVAL              (PC_RESETVAL                  ),
-    .PC_WD                    (PC_WD                        ),
-    .GPR_WD                   (GPR_WD                       ),
-    .GPR_ADDR_WD              (GPR_ADDR_WD                  ),
-    .IMM_WD                   (IMM_WD                       ),
-    .CSR_WD                   (CSR_WD                       ),
-    .CSR_ADDR_WD              (CSR_ADDR_WD                  ),
-    .INST_WD                  (INST_WD                      ),
-    .SRAM_ADDR_WD             (SRAM_ADDR_WD                 ),
-    .SRAM_WMASK_WD            (SRAM_WMASK_WD                ),
-    .SRAM_DATA_WD             (SRAM_DATA_WD                 )
+    .WORD_WD                 (WORD_WD                       ),
+    .PC_RESETVAL             (PC_RESETVAL                   ),
+    .PC_WD                   (PC_WD                         ),
+    .GPR_WD                  (GPR_WD                        ),
+    .GPR_ADDR_WD             (GPR_ADDR_WD                   ),
+    .IMM_WD                  (IMM_WD                        ),
+    .CSR_WD                  (CSR_WD                        ),
+    .CSR_ADDR_WD             (CSR_ADDR_WD                   ),
+    .INST_WD                 (INST_WD                       ),
+    .SRAM_ADDR_WD            (SRAM_ADDR_WD                  ),
+    .SRAM_WMASK_WD           (SRAM_WMASK_WD                 ),
+    .SRAM_DATA_WD            (SRAM_DATA_WD                  )
   ) u_core (
-    .i_clk                    (i_clk                        ),
-    .i_rst                    (i_rst                        ),
+    .i_clk                   (i_clk                         ),
+    .i_rst                   (i_rst                         ),
 
-    .o_inst_sram_ren          (cpu_inst_ren                 ),
-    .o_inst_sram_addr         (cpu_inst_addr                ),
-    .i_inst_sram_rdata        (cpu_inst_rdata               ),
+    .o_inst_sram_ren         (cpu_inst_ren                  ),
+    .o_inst_sram_addr        (cpu_inst_addr                 ),
+    .i_inst_sram_rdata       (cpu_inst_rdata                ),
 
   // data sram interface
-    .o_data_sram_addr         (cpu_data_addr                ),
-    .o_data_sram_ren          (cpu_data_ren                 ),
-    .i_data_sram_rdata        (cpu_data_rdata               ),
-    .o_data_sram_wen          (cpu_data_wen                 ),
-    .o_data_sram_wmask        (cpu_data_wmask               ),
-    .o_data_sram_wdata        (cpu_data_wdata               )
+    .o_data_sram_addr        (cpu_data_addr                 ),
+    .o_data_sram_ren         (cpu_data_ren                  ),
+    .i_data_sram_rdata       (cpu_data_rdata                ),
+    .o_data_sram_wen         (cpu_data_wen                  ),
+    .o_data_sram_wmask       (cpu_data_wmask                ),
+    .o_data_sram_wdata       (cpu_data_wdata                )
   );
 
   // inst ram
