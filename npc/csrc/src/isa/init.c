6b583eff2dcc4f82c0a362bc30bd67cfec95095d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-09 12:19:29 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  12:19:29 up  1:22,  2 users,  load average: 1.50, 1.42, 1.44

--- npc/csrc/src/isa/init.c
+++ npc/csrc/src/isa/init.c
@@ -9,6 +9,10 @@ static const uint32_t img [] = {
   0x00200593,  // li      a1,2
   0x00200613,  // li      a2,2
   0x00200693,  // li      a3,2
+  0x00200693,  // li      a3,2
+  0x00200693,  // li      a3,2
+  0x00200693,  // li      a3,2
+  0x00200693,  // li      a3,2
   0x00c58933,  // add     s2,a1,a2
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
