eb75a3f770545ff74f1c2109be88e383b21c1e2c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-09 22:37:54 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  22:37:54 up 11:41,  2 users,  load average: 2.47, 2.47, 2.48

--- npc/vsrc/core/0_if_stage/if_stage.v
+++ npc/vsrc/core/0_if_stage/if_stage.v
@@ -32,7 +32,7 @@ module ysyx_22050710_if_stage #(
   assign {br_sel, br_target} = i_br_bus                      ;
 
   // if stage
-  reg                         fs_valid                      ;
+  wire                         fs_valid                      ;
   wire                         fs_ready_go                   ;
   wire                         fs_allowin                    ;
 
@@ -44,70 +44,43 @@ module ysyx_22050710_if_stage #(
   assign o_fs_to_ds_valid    = fs_valid && fs_ready_go       ;
 
   wire [INST_WD-1:0          ] fs_inst                       ;
-  reg [PC_WD-1:0            ] fs_pc                         ;
+  wire [PC_WD-1:0            ] fs_pc                         ;
   assign o_fs_to_ds_bus      = {fs_inst, fs_pc}              ;
 
-  /* Reg #( */
-  /*   .WIDTH                    (1                            ), */
-  /*   .RESET_VAL                (1'b0                         ) */
-  /* ) u_fs_valid ( */
-  /*   .clk                      (i_clk                        ), */
-  /*   .rst                      (i_rst                        ), */
-  /*   .din                      (to_fs_valid                  ), // ~reset */
-  /*   .dout                     (fs_valid                     ), */
-  /*   .wen                      (fs_allowin                   ) */
-  /* ); */
+  Reg #(
+    .WIDTH                    (1                            ),
+    .RESET_VAL                (1'b0                         )
+  ) u_fs_valid (
+    .clk                      (i_clk                        ),
+    .rst                      (i_rst                        ),
+    .din                      (to_fs_valid                  ), // ~reset
+    .dout                     (fs_valid                     ),
+    .wen                      (fs_allowin                   )
+  );
 
-  wire [63:0] seq_pc;
-  wire [63:0] nextpc;
-  assign seq_pc       = fs_pc + 4;
-  assign nextpc       = br_sel ? br_target : seq_pc; 
+  ysyx_22050710_pc #(
+    .PC_RESETVAL              (PC_RESETVAL                  ),
+    .PC_WD                    (PC_WD                        ),
+    .SRAM_ADDR_WD             (SRAM_ADDR_WD                 )
+  ) u_pc (
+    .i_clk                    (i_clk                        ),
+    .i_rst                    (i_rst                        ),
+    .i_load                   (to_fs_valid && fs_allowin    ), // if stage 无数据 ds stage 允许写入 准备下一条指令取指
+    .i_br_sel                 (br_sel                       ), // bru 控制指令的跳转在 id stage 完成 直接回到此处改变 pc
+    .i_br_target              (br_target                    ), // 避免控制指令冲突问题
+    .o_pc                     (fs_pc                        ),
+    .o_inst_sram_ren          (o_inst_sram_ren              ),
+    .o_inst_sram_addr         (o_inst_sram_addr             )
+  );
 
-  always @(posedge i_clk) begin
-      if (i_rst) begin
-          fs_valid <= 1'b0;
-      end
-      else if (fs_allowin) begin
-          fs_valid <= to_fs_valid;
-      end
-
-      if (i_rst) begin
-          fs_pc <= PC_RESETVAL;
-      end
-      else if (to_fs_valid && fs_allowin) begin
-          fs_pc <= nextpc;
-      end
-  end
-
-  assign o_inst_sram_ren    = to_fs_valid && fs_allowin;
-  assign o_inst_sram_addr  = nextpc[31:0];
-  assign fs_inst = fs_pc[2] == 1'b0
-                 ? i_inst_sram_rdata[31:0]
-                 : i_inst_sram_rdata[63:32]      ;
-
-  /* ysyx_22050710_pc #( */
-  /*   .PC_RESETVAL              (PC_RESETVAL                  ), */
-  /*   .PC_WD                    (PC_WD                        ), */
-  /*   .SRAM_ADDR_WD             (SRAM_ADDR_WD                 ) */
-  /* ) u_pc ( */
-  /*   .i_clk                    (i_clk                        ), */
-  /*   .i_rst                    (i_rst                        ), */
-  /*   .i_load                   (to_fs_valid && fs_allowin    ), // if stage 无数据 ds stage 允许写入 准备下一条指令取指 */
-  /*   .i_br_sel                 (br_sel                       ), // bru 控制指令的跳转在 id stage 完成 直接回到此处改变 pc */
-  /*   .i_br_target              (br_target                    ), // 避免控制指令冲突问题 */
-  /*   .o_pc                     (fs_pc                        ), */
-  /*   .o_inst_sram_ren          (o_inst_sram_ren              ), */
-  /*   .o_inst_sram_addr         (o_inst_sram_addr             ) */
-  /* ); */
-
-  /* ysyx_22050710_ifu #( */
-  /*   .INST_WD                  (INST_WD                      ), */
-  /*   .SRAM_DATA_WD             (SRAM_DATA_WD                 ) */
-  /* ) u_ifu ( */
-  /*   .i_pc_align               (fs_pc[2]                     ), // 取指访问指令sram 64位对齐 根据 pc[2] 选择前32bits还是后32bits */
-  /*   .o_inst                   (fs_inst                      ), */
-  /*   // inst sram interface */
-  /*   .i_inst_sram_rdata        (i_inst_sram_rdata           ) */
-  /* ); */
+  ysyx_22050710_ifu #(
+    .INST_WD                  (INST_WD                      ),
+    .SRAM_DATA_WD             (SRAM_DATA_WD                 )
+  ) u_ifu (
+    .i_pc_align               (fs_pc[2]                     ), // 取指访问指令sram 64位对齐 根据 pc[2] 选择前32bits还是后32bits
+    .o_inst                   (fs_inst                      ),
+    // inst sram interface
+    .i_inst_sram_rdata        (i_inst_sram_rdata           )
+  );
 
 endmodule
