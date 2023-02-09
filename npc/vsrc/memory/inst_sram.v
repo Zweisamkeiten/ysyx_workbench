edb4a2bf3533d6c1ebee3425cfade9fd223f9062
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-10 00:20:03 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  00:20:03 up 13:23,  2 users,  load average: 2.55, 2.53, 2.51

--- npc/vsrc/memory/inst_sram.v
+++ npc/vsrc/memory/inst_sram.v
@@ -8,23 +8,19 @@ module ysyx_22050710_inst_sram #(
 
   input                        i_ren                         ,
   input  [SRAM_ADDR_WD-1:0   ] i_addr                        ,
-  output [SRAM_DATA_WD-1:0   ] o_rdata
+  output reg [SRAM_DATA_WD-1:0] o_rdata
 );
 
-  reg  [SRAM_DATA_WD-1:0     ] rdata                         ;  // address register for pmem read.
+  wire  [SRAM_DATA_WD-1:0     ] rdata                         ;  // address register for pmem read.
+
+  always @(*) begin
+    npc_pmem_read({32'b0, i_addr}, rdata);
+  end
 
   always @(posedge i_clk) begin
     if (i_ren) begin
-      /* npc_pmem_read({32'b0, i_addr}, rdata); */
-      if (i_addr == 32'h80000000 || i_addr == 32'h80000004) begin
-        rdata <= 64'h0000911700000413;
-      end
-      else if (i_addr == 32'h80000008 || i_addr == 32'h8000000c) begin
-        rdata <= 64'h000c00efffc10113;
-      end
+      o_rdata <= rdata;
     end
   end
-
-  assign o_rdata             = i_ren ? rdata : 0             ;
 
 endmodule
