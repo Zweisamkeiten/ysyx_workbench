837d04bf877b4e1e846159dcc5b977d8de0cff87
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-10 12:59:54 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  12:59:54 up  3:26,  2 users,  load average: 1.48, 1.47, 1.35

--- npc/vsrc/defines.v
+++ npc/vsrc/defines.v
@@ -34,7 +34,7 @@
   `define ysyx_22050710_DS_TO_ES_BUS_WD             391
   `define ysyx_22050710_ES_TO_MS_BUS_WD             312
   `define ysyx_22050710_MS_TO_WS_BUS_WD             312
-  `define ysyx_22050710_WS_TO_RF_BUS_WD             312  // {gpr_rf_wen[0:0], gpr_rf_waddr[4:0], gpr_rf_wdata[63:0]
+  `define ysyx_22050710_WS_TO_RF_BUS_WD             147  // {gpr_rf_wen[0:0], gpr_rf_waddr[4:0], gpr_rf_wdata[63:0]
                                                          //  csr_rf_wen[0:0], csr_rf_waddr[11:0], csr_rf_wdata[63:0]}
   `define ysyx_22050710_BR_BUS_WD                   65  // {br_sel[0:0], br_target[63:0]} 1 + 64
 `endif
