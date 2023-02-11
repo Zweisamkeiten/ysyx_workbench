d96f600f1cadd0d64d8043e9088a2325d8b44587
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-11 23:03:02 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  23:03:02 up 11:48,  2 users,  load average: 1.58, 1.48, 1.46

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -67,6 +67,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
+  assert(0);
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
