e83c94fb197a9a6b1ea09352618328c289fd6907
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-11 13:31:39 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  13:31:39 up  2:17,  2 users,  load average: 1.20, 1.14, 0.98

--- npc/vsrc/core/0_if_stage/if_stage.v
+++ npc/vsrc/core/0_if_stage/if_stage.v
@@ -26,10 +26,14 @@ module ysyx_22050710_if_stage #(
 
   // pre if stage
   wire                         to_fs_valid                   ;
-  assign to_fs_valid         = ~i_rst && ~br_sel              ;
+  assign to_fs_valid         = ~i_rst && ~br_sel             ;
+  wire                         br_stall                      ;
   wire                         br_sel                        ;
   wire [PC_WD-1:0            ] br_target                     ;
-  assign {br_sel, br_target} = i_br_bus                      ;
+  assign {br_stall,
+          br_sel,
+          br_target
+         }                   = i_br_bus                      ;
 
   // if stage
   wire                         fs_valid                      ;
