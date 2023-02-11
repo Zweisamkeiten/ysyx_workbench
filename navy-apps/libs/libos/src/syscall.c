908fd6585649eeeea42731b36a44d7f205573f19
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-11 23:01:36 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  23:01:36 up 11:47,  2 users,  load average: 1.28, 1.36, 1.42

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,10 +68,12 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
+  assert(0);
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
 void *_sbrk(intptr_t increment) {
+  assert(0);
   void *old_program_break = program_break;
   void *new_program_break = program_break + increment;
 
@@ -84,14 +86,17 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
+  assert(0);
   return _syscall_(SYS_read, fd, (intptr_t)buf, count);
 }
 
 int _close(int fd) {
+  assert(0);
   return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
+  assert(0);
   return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
@@ -100,6 +105,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
+  assert(0);
   return _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
 }
 
