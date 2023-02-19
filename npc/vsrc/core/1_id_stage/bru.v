2551a3fac647e6464510534c41d6d44880874a4c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 18:29:25 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  18:29:25 up  3:41,  2 users,  load average: 1.46, 1.17, 1.04

--- npc/vsrc/core/1_id_stage/bru.v
+++ npc/vsrc/core/1_id_stage/bru.v
@@ -84,7 +84,7 @@ module ysyx_22050710_bru #(
     })
   );
 
-  assign o_br_taken          = (i_bren & PCAsrc) | i_ep_sel  ;
+  assign o_br_taken          = i_bren | i_ep_sel             ;
   assign o_br_target         = i_ep_sel
                              ? i_epnpc
                              : (PCBsrc ? i_rs1data : i_pc) + (PCAsrc ? i_imm : 4);
