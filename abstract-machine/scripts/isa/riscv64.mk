31b4349418fd2c4704229f329a4828005367f17d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-28 08:24:56 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  08:24:56 up 19 min,  2 users,  load average: 2.93, 4.12, 2.61

--- abstract-machine/scripts/isa/riscv64.mk
+++ abstract-machine/scripts/isa/riscv64.mk
@@ -1,5 +1,5 @@
 CROSS_COMPILE := riscv64-linux-gnu-
-COMMON_FLAGS  := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
+COMMON_FLAGS  := -march=rv64g -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
 LDFLAGS       += -flto -melf64lriscv
