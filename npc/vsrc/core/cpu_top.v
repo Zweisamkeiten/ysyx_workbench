ca60d950aaec3e37e7508f0a486290bf316f8bc5
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-04 10:31:53 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  10:31:53 up  1:20,  9 users,  load average: 2.01, 2.07, 1.88

--- npc/vsrc/core/cpu_top.v
+++ npc/vsrc/core/cpu_top.v
@@ -93,123 +93,123 @@ module ysyx_22050710_cpu_top #(
   wire                         cpu_data_data_ok              ;
 
   ysyx_22050710_core #( 
-    .WORD_WD                 (WORD_WD                       ),
-    .PC_RESETVAL             (PC_RESETVAL                   ),
-    .PC_WD                   (PC_WD                         ),
-    .GPR_WD                  (GPR_WD                        ),
-    .GPR_ADDR_WD             (GPR_ADDR_WD                   ),
-    .IMM_WD                  (IMM_WD                        ),
-    .CSR_WD                  (CSR_WD                        ),
-    .CSR_ADDR_WD             (CSR_ADDR_WD                   ),
-    .INST_WD                 (INST_WD                       ),
-    .SRAM_ADDR_WD            (SRAM_ADDR_WD                  ),
-    .SRAM_WMASK_WD           (SRAM_WMASK_WD                 ),
-    .SRAM_DATA_WD            (SRAM_DATA_WD                  )
+    .WORD_WD                  (WORD_WD                       ),
+    .PC_RESETVAL              (PC_RESETVAL                   ),
+    .PC_WD                    (PC_WD                         ),
+    .GPR_WD                   (GPR_WD                        ),
+    .GPR_ADDR_WD              (GPR_ADDR_WD                   ),
+    .IMM_WD                   (IMM_WD                        ),
+    .CSR_WD                   (CSR_WD                        ),
+    .CSR_ADDR_WD              (CSR_ADDR_WD                   ),
+    .INST_WD                  (INST_WD                       ),
+    .SRAM_ADDR_WD             (SRAM_ADDR_WD                  ),
+    .SRAM_WMASK_WD            (SRAM_WMASK_WD                 ),
+    .SRAM_DATA_WD             (SRAM_DATA_WD                  )
   ) u_core (
-    .i_clk                   (i_aclk                        ),
-    .i_rst                   (~i_arsetn                     ),
+    .i_clk                    (i_aclk                        ),
+    .i_rst                    (~i_arsetn                     ),
 
-    .o_inst_sram_addr        (cpu_inst_addr                 ),
-    .o_inst_sram_ren         (cpu_inst_ren                  ),
-    .i_inst_sram_rdata       (cpu_inst_rdata                ),
-    .i_inst_sram_addr_ok     (cpu_inst_addr_ok              ),
-    .i_inst_sram_data_ok     (cpu_inst_data_ok              ),
+    .o_inst_sram_addr         (cpu_inst_addr                 ),
+    .o_inst_sram_ren          (cpu_inst_ren                  ),
+    .i_inst_sram_rdata        (cpu_inst_rdata                ),
+    .i_inst_sram_addr_ok      (cpu_inst_addr_ok              ),
+    .i_inst_sram_data_ok      (cpu_inst_data_ok              ),
 
     // data sram interface
-    .o_data_sram_addr        (cpu_data_addr                 ),
-    .o_data_sram_ren         (cpu_data_ren                  ),
-    .i_data_sram_rdata       (cpu_data_rdata                ),
-    .o_data_sram_wen         (cpu_data_wen                  ),
-    .o_data_sram_wmask       (cpu_data_wmask                ),
-    .o_data_sram_wdata       (cpu_data_wdata                )
+    .o_data_sram_addr         (cpu_data_addr                 ),
+    .o_data_sram_ren          (cpu_data_ren                  ),
+    .i_data_sram_rdata        (cpu_data_rdata                ),
+    .o_data_sram_wen          (cpu_data_wen                  ),
+    .o_data_sram_wmask        (cpu_data_wmask                ),
+    .o_data_sram_wdata        (cpu_data_wdata                )
   );
 
   ysyx_22050710_axil_master_wrap u_ifu_axi_wrap (
-    .i_rw_valid                (cpu_inst_ren                ),  //IF&MEM输入信号
-    .o_rw_addr_ok              (cpu_inst_addr_ok            ),  //IF&MEM输入信号
-    .o_rw_data_ok              (cpu_inst_data_ok            ),  //IF&MEM输入信号
-    .i_rw_ren                  (cpu_inst_ren                ),  //IF&MEM输入信号
-    .i_rw_wen                  (0                           ),  //IF&MEM输入信号
-    .i_rw_addr                 (cpu_inst_addr               ),  //IF&MEM输入信号
-    .o_data_read               (cpu_inst_rdata              ),  //IF&MEM输入信号
-    .i_rw_w_data               (64'b0                       ),  //IF&MEM输入信号
-    .i_rw_size                 (8'b0                        ),  //IF&MEM输入信号
+    .i_rw_valid               (cpu_inst_ren                ),  //IF&MEM输入信号
+    .o_rw_addr_ok             (cpu_inst_addr_ok            ),  //IF&MEM输入信号
+    .o_rw_data_ok             (cpu_inst_data_ok            ),  //IF&MEM输入信号
+    .i_rw_ren                 (cpu_inst_ren                ),  //IF&MEM输入信号
+    .i_rw_wen                 (0                           ),  //IF&MEM输入信号
+    .i_rw_addr                (cpu_inst_addr               ),  //IF&MEM输入信号
+    .o_data_read              (cpu_inst_rdata              ),  //IF&MEM输入信号
+    .i_rw_w_data              (64'b0                       ),  //IF&MEM输入信号
+    .i_rw_size                (8'b0                        ),  //IF&MEM输入信号
 
-    .i_aclk                    (i_aclk                      ),
-    .i_arsetn                  (i_arsetn                    ),
+    .i_aclk                   (i_aclk                      ),
+    .i_arsetn                 (i_arsetn                    ),
 
     // Wirte address channel
-    .o_awvalid                 (o_ifu_awvalid               ),
-    .i_awready                 (i_ifu_awready               ),
-    .o_awaddr                  (o_ifu_awaddr                ),
-    .o_awprot                  (o_ifu_awprot                ), // define the access permission for write accesses.
+    .o_awvalid                (o_ifu_awvalid               ),
+    .i_awready                (i_ifu_awready               ),
+    .o_awaddr                 (o_ifu_awaddr                ),
+    .o_awprot                 (o_ifu_awprot                ), // define the access permission for write accesses.
 
     // Write data channel
-    .o_wvalid                  (o_ifu_wvalid                ),
-    .i_wready                  (i_ifu_wready                ),
-    .o_wdata                   (o_ifu_wdata                 ),
-    .o_wstrb                   (o_ifu_wstrb                 ),
+    .o_wvalid                 (o_ifu_wvalid                ),
+    .i_wready                 (i_ifu_wready                ),
+    .o_wdata                  (o_ifu_wdata                 ),
+    .o_wstrb                  (o_ifu_wstrb                 ),
 
     // Write response channel
-    .i_bvalid                  (i_ifu_bvalid                ),
-    .o_bready                  (o_ifu_bready                ),
-    .i_bresp                   (i_ifu_bresp                 ),
+    .i_bvalid                 (i_ifu_bvalid                ),
+    .o_bready                 (o_ifu_bready                ),
+    .i_bresp                  (i_ifu_bresp                 ),
 
     // Read address channel
-    .o_arvalid                 (o_ifu_arvalid               ),
-    .i_arready                 (i_ifu_arready               ),
-    .o_araddr                  (o_ifu_araddr                ),
-    .o_arprot                  (o_ifu_arprot                ),
+    .o_arvalid                (o_ifu_arvalid               ),
+    .i_arready                (i_ifu_arready               ),
+    .o_araddr                 (o_ifu_araddr                ),
+    .o_arprot                 (o_ifu_arprot                ),
 
     // Read data channel
-    .i_rvalid                  (i_ifu_rvalid                ),
-    .o_rready                  (o_ifu_rready                ),
-    .i_rdata                   (i_ifu_rdata                 ),
-    .i_rresp                   (i_ifu_rresp                 )
+    .i_rvalid                 (i_ifu_rvalid                ),
+    .o_rready                 (o_ifu_rready                ),
+    .i_rdata                  (i_ifu_rdata                 ),
+    .i_rresp                  (i_ifu_rresp                 )
   );
 
   ysyx_22050710_axil_master_wrap u_lsu_axi_wrap (
-    .i_rw_valid                (cpu_data_ren | cpu_data_wen ),  //IF&MEM输入信号
-    .o_rw_addr_ok              (cpu_data_addr_ok            ),  //IF&MEM输入信号
-    .o_rw_data_ok              (cpu_data_data_ok            ),  //IF&MEM输入信号
-    .i_rw_ren                  (cpu_data_ren                ),  //IF&MEM输入信号
-    .i_rw_wen                  (cpu_data_wen                ),  //IF&MEM输入信号
-    .i_rw_addr                 (cpu_data_addr               ),  //IF&MEM输入信号
-    .o_data_read               (cpu_data_rdata              ),  //IF&MEM输入信号
-    .i_rw_w_data               (cpu_data_wdata              ),  //IF&MEM输入信号
-    .i_rw_size                 (cpu_data_wmask              ),  //IF&MEM输入信号
+    .i_rw_valid               (cpu_data_ren | cpu_data_wen ),  //IF&MEM输入信号
+    .o_rw_addr_ok             (cpu_data_addr_ok            ),  //IF&MEM输入信号
+    .o_rw_data_ok             (cpu_data_data_ok            ),  //IF&MEM输入信号
+    .i_rw_ren                 (cpu_data_ren                ),  //IF&MEM输入信号
+    .i_rw_wen                 (cpu_data_wen                ),  //IF&MEM输入信号
+    .i_rw_addr                (cpu_data_addr               ),  //IF&MEM输入信号
+    .o_data_read              (cpu_data_rdata              ),  //IF&MEM输入信号
+    .i_rw_w_data              (cpu_data_wdata              ),  //IF&MEM输入信号
+    .i_rw_size                (cpu_data_wmask              ),  //IF&MEM输入信号
 
-    .i_aclk                    (i_aclk                      ),
-    .i_arsetn                  (i_arsetn                    ),
+    .i_aclk                   (i_aclk                      ),
+    .i_arsetn                 (i_arsetn                    ),
 
     // Wirte address channel
-    .o_awvalid                 (o_lsu_awvalid               ),
-    .i_awready                 (i_lsu_awready               ),
-    .o_awaddr                  (o_lsu_awaddr                ),
-    .o_awprot                  (o_lsu_awprot                ), // define the access permission for write accesses.
+    .o_awvalid                (o_lsu_awvalid               ),
+    .i_awready                (i_lsu_awready               ),
+    .o_awaddr                 (o_lsu_awaddr                ),
+    .o_awprot                 (o_lsu_awprot                ), // define the access permission for write accesses.
 
     // Write data channel
-    .o_wvalid                  (o_lsu_wvalid                ),
-    .i_wready                  (i_lsu_wready                ),
-    .o_wdata                   (o_lsu_wdata                 ),
-    .o_wstrb                   (o_lsu_wstrb                 ),
+    .o_wvalid                 (o_lsu_wvalid                ),
+    .i_wready                 (i_lsu_wready                ),
+    .o_wdata                  (o_lsu_wdata                 ),
+    .o_wstrb                  (o_lsu_wstrb                 ),
 
     // Write response channel
-    .i_bvalid                  (i_lsu_bvalid                ),
-    .o_bready                  (o_lsu_bready                ),
-    .i_bresp                   (i_lsu_bresp                 ),
+    .i_bvalid                 (i_lsu_bvalid                ),
+    .o_bready                 (o_lsu_bready                ),
+    .i_bresp                  (i_lsu_bresp                 ),
 
     // Read address channel
-    .o_arvalid                 (o_lsu_arvalid               ),
-    .i_arready                 (i_lsu_arready               ),
-    .o_araddr                  (o_lsu_araddr                ),
-    .o_arprot                  (o_lsu_arprot                ),
+    .o_arvalid                (o_lsu_arvalid               ),
+    .i_arready                (i_lsu_arready               ),
+    .o_araddr                 (o_lsu_araddr                ),
+    .o_arprot                 (o_lsu_arprot                ),
 
     // Read data channel
-    .i_rvalid                  (i_lsu_rvalid                ),
-    .o_rready                  (o_lsu_rready                ),
-    .i_rdata                   (i_lsu_rdata                 ),
-    .i_rresp                   (i_lsu_rresp                 )
+    .i_rvalid                 (i_lsu_rvalid                ),
+    .o_rready                 (o_lsu_rready                ),
+    .i_rdata                  (i_lsu_rdata                 ),
+    .i_rresp                  (i_lsu_rresp                 )
   );
 
 endmodule
