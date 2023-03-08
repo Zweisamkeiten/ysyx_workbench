1ff6db8f521dba096e702d082ee55394efb9c0a5
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 10:04:03 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  10:04:03 up 49 min,  2 users,  load average: 2.08, 2.03, 1.94

--- npc/vsrc/core/1_id_stage/id_stage.v
+++ npc/vsrc/core/1_id_stage/id_stage.v
@@ -246,7 +246,7 @@ module ysyx_22050710_id_stage #(
     .rst                      (i_rst                        ),
     .din                      (i_debug_ws_to_rf_bus         ),
     .dout                     (debug_ws_to_rf_bus_r         ),
-    .wen                      (1'b1                         )
+    .wen                      (o_ds_to_es_valid             )
   );
 
   wire                         rf_debug_valid                ;
