ab12583f929a858e98c305d6ba024ed7af051e70
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 20:42:40 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  20:42:40 up 11:34,  4 users,  load average: 2.53, 2.32, 2.28

--- npc/vsrc/memory/axil_inst_sram_wrap.v
+++ npc/vsrc/memory/axil_inst_sram_wrap.v
@@ -83,7 +83,7 @@ module ysyx_22050710_axil_inst_sram_wrap #(
 
   reg [DATA_WIDTH-1:0] rdata;
   always @(*) begin
-    if (r_state_read) begin
+    if (ar_fire) begin
       npc_pmem_read({32'b0, i_araddr}, rdata);
     end
     else begin
