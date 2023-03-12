ff3db398f273a2df94fcbd1b7b82de74b0557158
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-12 21:51:18 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  21:51:18 up 1 day, 12:35,  2 users,  load average: 2.19, 2.26, 2.19

---
+++ npc/vsrc/memory/data_sram.v
@@ -0,0 +1,50 @@
+// ysyx_22050710 Data Sram
+
+module ysyx_22050710_data_sram #(
+  parameter SRAM_ADDR_WD                                     ,
+  parameter SRAM_WMASK_WD                                    ,
+  parameter SRAM_DATA_WD
+) (
+  input                        i_clk                         ,
+
+  input  [SRAM_ADDR_WD-1:0   ] i_addr                        ,  // 单端口用于读写地址
+  // read port
+  input                        i_ren                         ,
+  output reg [SRAM_DATA_WD-1:0   ] o_rdata                       ,
+  // write port
+  input                        i_wen                         ,
+  input  [SRAM_WMASK_WD-1:0  ] i_wmask                       ,
+  input  [SRAM_DATA_WD-1:0   ] i_wdata
+);
+
+  wire [SRAM_ADDR_WD-1:0     ] raddr;
+  wire [SRAM_ADDR_WD-1:0     ] waddr;
+  assign raddr               = i_addr;
+  assign waddr               = i_addr;
+
+  reg  [SRAM_DATA_WD-1:0     ] rdata                         ;  // read register for pmem read.
+
+  always @(*) begin
+    if (i_ren) begin
+      npc_pmem_read({32'b0, raddr}, rdata);
+    end
+    else begin
+      rdata = 0;
+    end
+  end
+
+  // read port
+  always @(posedge i_clk) begin                                 // 改为 同步接口 延迟一周期返回
+    if (i_ren) begin
+      o_rdata <= rdata;
+    end
+  end
+
+  // write port
+  always @(posedge i_clk) begin
+    if (i_wen) begin
+      npc_pmem_write({32'b0, waddr}, i_wdata, i_wmask);
+    end
+  end
+
+endmodule
