8abb76c6588c7e5aef8204ee16ef512b2bb303c3
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 10:28:23 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  10:28:23 up 1 day,  1:13, 13 users,  load average: 2.28, 2.29, 2.28

--- npc/csrc/src/filelist.mk
+++ npc/csrc/src/filelist.mk
@@ -2,7 +2,7 @@ CXXSRCS += csrc/src/sim_init.cc
 CXXFLAGS += -faligned-new
 DIRS-y += csrc/src/cpu csrc/src/utils csrc/src/engine
 DIRS-y += csrc/src/memory
-ifeq ($(VERILATOR_INC_ROOT),)
+ifeq ("$(VERILATOR_INC_ROOT)","/usr/share/verilator")
 INC_PATH += /usr/share/verilator/include
 INC_PATH += /usr/share/verilator/include/vltstd
 else
