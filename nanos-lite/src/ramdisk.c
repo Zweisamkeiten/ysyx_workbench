17bc5a73630c4d30aabc5a9ad08da468e4a01efa
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-20 21:42:20 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  21:42:20 up 12:27, 11 users,  load average: 3.12, 2.73, 2.48

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -18,6 +18,7 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len) {
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
 size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
+  printf("%u\n", offset);
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(&ramdisk_start + offset, buf, len);
   return len;
