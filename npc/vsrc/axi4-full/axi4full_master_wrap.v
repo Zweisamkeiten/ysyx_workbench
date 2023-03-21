421ff923b20cab7fd865e4b57b5e89ed87642b58
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 16:41:27 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  16:41:27 up 1 day,  7:26, 11 users,  load average: 2.35, 2.36, 2.35

--- npc/vsrc/axi4-full/axi4full_master_wrap.v
+++ npc/vsrc/axi4-full/axi4full_master_wrap.v
@@ -206,10 +206,10 @@ module ysyx_22050710_axi4full_master_wrap #(
 
   assign o_rw_addr_ok        = ar_fire | w_fire              ;
   assign o_rw_data_ok        = r_fire  | b_fire              ;
-  assign o_rw_rdata          = i_rdata                       ;
+
   wire [DATA_WIDTH-1:0] rdata;
   Reg #(
-    .WIDTH                    (ADDR_WIDTH                   ),
+    .WIDTH                    (DATA_WIDTH                   ),
     .RESET_VAL                (0                            )
   ) u_r_data_r (
     .clk                      (i_aclk                       ),
