1b469e548468abff43881ec23807149bddb091d3
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 14:40:33 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  14:40:33 up  1:53, 11 users,  load average: 1.86, 2.14, 2.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -239,9 +239,11 @@ static void statistic() {
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
+#ifdef CONFIG_MULTI_COUNT
   extern uint64_t multi_count;
   Log("total multi instructions = " NUMBERIC_FMT, multi_count);
-  IFDEF(CONFIG_MULTI_COUNT, Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);)
+#endif
+  Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
 }
