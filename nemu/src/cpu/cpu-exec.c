fd3e61aa76cec2fc1c9e370aa764fce7091ba578
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 15:57:13 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  15:57:13 up  3:10, 11 users,  load average: 2.15, 2.27, 2.31

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -134,7 +134,7 @@ void disassemble_inst_to_buf(char *logbuf, size_t bufsize, uint8_t * inst_val, v
   char * q = ftrace_buf;
   if (strncmp(p, "ret", 3) == 0) {
     char *func_str = NULL;
-    if((func_str = check_is_func_call(pc)) != NULL && strcmp(func_str, "memcpy") == 0) {
+    if((func_str = check_is_func_call(pc)) != NULL) {
       q += snprintf(q, 128, FMT_WORD ":", pc);
       stack_depth--;
       for (int i = 0; i < stack_depth; i++) {
@@ -146,7 +146,7 @@ void disassemble_inst_to_buf(char *logbuf, size_t bufsize, uint8_t * inst_val, v
   }
   else if (strncmp(p, "jal", 3) == 0) {
     char *func_str = NULL;
-    if((func_str = check_is_func_call(cpu.pc)) != NULL && strcmp(func_str, "memcpy") == 0) {
+    if((func_str = check_is_func_call(cpu.pc)) != NULL) {
       q += snprintf(q, 128, FMT_WORD ":", pc);
       for (int i = 0; i < stack_depth; i++) {
         q += snprintf(q, 128, "  ");
