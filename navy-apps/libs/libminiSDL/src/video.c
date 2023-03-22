402cb9413d495ba8fa5167843c35a9bb69ddbdb3
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 16:47:50 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.1.9-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Wed, 01 Feb 2023 17:07:37 +0000 x86_64 GNU/Linux  16:47:50 up  4:00, 11 users,  load average: 3.63, 2.95, 2.55

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -38,6 +38,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
+  printf("123\n");
   int rectx, recty;
   int rectw, recth;
   if (dstrect == NULL) {
