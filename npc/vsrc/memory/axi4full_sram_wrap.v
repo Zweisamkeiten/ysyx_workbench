7b7ec301c2dd510c65b780cec0f1ed280e1dc5a6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 09:45:10 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.2.10-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Fri, 07 Apr 2023 02:10:16 +0000 x86_64 GNU/Linux  09:45:10 up 23 min, 10 users,  load average: 2.26, 2.10, 1.68

--- npc/vsrc/memory/axi4full_sram_wrap.v
+++ npc/vsrc/memory/axi4full_sram_wrap.v
@@ -90,7 +90,7 @@ module ysyx_22050710_axi4full_sram_wrap #(
     else begin
       case (read_state_reg)
         READ_STATE_IDLE : if (ar_fire) read_state_reg <= READ_STATE_READ;
-        READ_STATE_READ : if ((|i_arlen == 1'b0) ? r_fire : o_rlast) read_state_reg <= READ_STATE_IDLE;
+        READ_STATE_READ : if ((|arlen == 1'b0) ? r_fire : o_rlast) read_state_reg <= READ_STATE_IDLE;
         default         :              read_state_reg <= read_state_reg ;
       endcase
     end
@@ -132,6 +132,18 @@ module ysyx_22050710_axi4full_sram_wrap #(
     .din                      ((i_arvalid ? i_araddr : araddr) + 32'd8),
     .dout                     (araddr                       ),
     .wen                      (i_arvalid || r_state_read      )
+  );
+
+  wire [7:0] arlen;
+  Reg #(
+    .WIDTH                    (8                            ),
+    .RESET_VAL                (0                            )
+  ) u_ar_addr_r (
+    .clk                      (i_aclk                       ),
+    .rst                      (!i_arsetn                    ),
+    .din                      (i_arlen                      ),
+    .dout                     (arlen                        ),
+    .wen                      (ar_fire                      )
   );
 
   always @(posedge i_aclk) begin
