a737ae4470e0a5eaf46b63e02acc10c94155eb38
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 17:21:07 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.2.10-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Fri, 07 Apr 2023 02:10:16 +0000 x86_64 GNU/Linux  17:21:07 up  7:59, 10 users,  load average: 2.33, 2.31, 2.53

--- npc/vsrc/memory/axi4full_sram_wrap.v
+++ npc/vsrc/memory/axi4full_sram_wrap.v
@@ -90,7 +90,7 @@ module ysyx_22050710_axi4full_sram_wrap #(
     else begin
       case (read_state_reg)
         READ_STATE_IDLE : if (ar_fire) read_state_reg <= READ_STATE_READ;
-        READ_STATE_READ : if ((|arlen == 1'b0) ? r_fire : ((i_arlen == 8'd4) && o_rlast)) read_state_reg <= READ_STATE_IDLE;
+        READ_STATE_READ : if ((|arlen == 1'b0) ? r_fire : o_rlast) read_state_reg <= READ_STATE_IDLE;
         default         :              read_state_reg <= read_state_reg ;
       endcase
     end
@@ -174,7 +174,7 @@ module ysyx_22050710_axi4full_sram_wrap #(
     end
   end
 
-  assign o_arready           = r_state_idle                  ;
+  assign o_arready           = r_state_idle && (nums_have_sent == 7'd0);
   assign o_awready           = w_state_idle                  ;
   assign o_wready            = w_state_write                 ;
   assign o_bresp             = 2'b00                         ;
