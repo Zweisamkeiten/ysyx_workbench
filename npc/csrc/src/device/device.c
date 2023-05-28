7a936c0d3cb041a8babebc4c10ea2832f00ab22d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-28 20:45:11 +0800

    > build RTL ysyx_22050710 杨海帆 Linux hf 6.3.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 11 May 2023 16:40:19 +0000 x86_64 GNU/Linux  20:45:11 up  9:22, 20 users,  load average: 3.85, 3.92, 3.72

--- npc/csrc/src/device/device.c
+++ npc/csrc/src/device/device.c
@@ -19,7 +19,7 @@ void vga_update_screen();
 void device_update() {
   static uint64_t last = 0;
   uint64_t now = get_time();
-  if (now - last < 1000000 / TIMER_HZ) {
+  if (now - last < 10000 / TIMER_HZ) {
     return;
   }
   last = now;
