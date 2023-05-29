e87213b60a423e438c4c10e75e3b067212427ac8
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-29 09:26:48 +0800

    > sim RTL ysyx_22050710 杨海帆 Linux hf 6.3.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 11 May 2023 16:40:19 +0000 x86_64 GNU/Linux  09:26:48 up 16 min, 20 users,  load average: 1.85, 2.25, 1.74

--- npc/csrc/src/utils/timer.c
+++ npc/csrc/src/utils/timer.c
@@ -24,9 +24,8 @@ IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
 static uint64_t boot_time = 0;
 
 static uint64_t get_time_internal() {
-  struct timespec now;
-  clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
+  extern uint64_t cycles;
+  uint64_t us = cycles / 1.5;
   return us;
 }
 
