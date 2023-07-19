174921867deb19ed170890c045f6a375b6ba47d0
22050710-Einsam <snitxmhm@gmail.com>
2023-07-19 23:51:59 +0800

    feat(nanos, am): 实现上下文切换

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -17,5 +17,6 @@ typedef union {
 } PCB;
 
 extern PCB *current;
+extern Context* schedule(Context *prev);
 
 #endif
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -19,17 +19,36 @@ void hello_fun(void *arg) {
   }
 }
 
+/**
+ * @brief 调用 kcontext() 创建上下文, 并将返回的指针记录到 PCB 的 cp 中
+ */
+void context_kload(PCB *pcb, void (*entry)(void *), void * args) {
+  pcb->as.area.end = pcb->stack;
+  pcb->as.area.start = pcb->stack + (STACK_SIZE * sizeof(uint8_t));
+
+  pcb->cp = kcontext(pcb->as.area, entry, args);
+}
+
 void init_proc() {
+  context_kload(&pcb[0], hello_fun, NULL);
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-  void naive_uload(PCB *pcb, const char *filename);
-  naive_uload(NULL, "/bin/nterm");
+  /* void naive_uload(PCB *pcb, const char *filename); */
+  /* naive_uload(NULL, "/bin/nterm"); */
 
 }
 
 Context* schedule(Context *prev) {
-  return NULL;
+  // save the context pointer
+  current->cp = prev;
+
+  // always select pcb[0] as the new process
+  current = &pcb[0];
+
+  Log("%lx", (Context *)current->cp->mepc);
+  // then return the new context
+  return current->cp;
 }
