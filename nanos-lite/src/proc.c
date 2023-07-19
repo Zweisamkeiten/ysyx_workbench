e51af6d5688f7bb734b323ed269cc52ae95e17cd
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-19 23:03:00 +0800

    > compile NEMU ysyx_22050710 杨海帆 Linux hf 6.4.3-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 11 Jul 2023 05:13:16 +0000 x86_64 GNU/Linux  23:03:00 up 13:01, 23 users,  load average: 3.58, 4.83, 4.07

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,8 +26,8 @@ void context_kload(PCB *pcb, void (*entry)(void *), void * args) {
   pcb->as.area.end = pcb->stack;
   pcb->as.area.start = pcb->stack + (STACK_SIZE * sizeof(uint8_t));
 
-  Log("%p", entry);
   pcb->cp = kcontext(pcb->as.area, entry, args);
+  Log("%lx", pcb->cp);
 }
 
 void init_proc() {
